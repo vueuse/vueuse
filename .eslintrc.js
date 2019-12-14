6be7f4d39e7c0a64b9ea8abb5b9b82f399bb86e0
Anthony Fu <anthonyfu117@hotmail.com>
2019-12-14 18:46:17 +0800

    chore: build tools

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true
+  },
+  extends: [
+    '@antfu/eslint-config-vue',
+  ],
+  rules: {
+  }
+}
---
+++ src/index.test.ts
@@ -0,0 +1,5 @@
+import index from './index'
+
+test('export', () => {
+  expect(index).toEqual(2)
+})
