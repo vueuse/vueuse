47c9b7a169da22fb90fda97dc9e5c82c054d57a4
Anthony Fu <anthonyfu117@hotmail.com>
2019-12-26 13:17:03 +0800

    chore: build import

--- build/import.js
+++ build/import.js
@@ -1,8 +1,13 @@
 const path = require('path')
 const fs = require('fs-extra')
-const { version } = require('../package.json')
 
 const srcDir = path.resolve(__dirname, '../src')
+const packageFilepath = path.resolve(__dirname, '../package.json')
+
+async function getVersion () {
+  const raw = await fs.readFile(packageFilepath)
+  return JSON.parse(raw).version
+}
 
 async function updateImport () {
   const files = fs
@@ -11,7 +16,7 @@ async function updateImport () {
     .sort()
 
   let content = ''
-  content += `export const version = '${version}'\n\n`
+  content += `export const version = '${await getVersion()}'\n\n`
   content += files.map(f => `export * from './${f}'\n`).join('')
 
   fs.writeFileSync(path.join(srcDir, 'index.ts'), content)
