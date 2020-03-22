bd3dff7c2d9e14f93a28c1a79cb9660adb63cfd6
Scott Bedard <scottbedard@users.noreply.github.com>
2020-03-21 20:56:43 -0700

    add basic implementaton of useElementVisibility (#61)
    * add basic implementaton of useElementVisibility
    
    * simplify useElementVisibility api
    
    * clean up docs

---
+++ packages/core/useElementVisibility/index.ts
@@ -0,0 +1,29 @@
+import { onMounted, ref, Ref, watch } from '../../api'
+import { useWindowScroll } from '../useWindowScroll'
+
+export function useElementVisibility(element: Ref<Element>) {
+  const { x, y } = useWindowScroll()
+  const elementIsVisible = ref(false)
+
+  const testBoundingClientRect = () => {
+    if (!element.value) {
+      elementIsVisible.value = false
+    }
+    else {
+      const rect = element.value.getBoundingClientRect()
+
+      elementIsVisible.value = (
+        rect.top <= (window.innerHeight || document.documentElement.clientHeight)
+          && rect.left <= (window.innerWidth || document.documentElement.clientWidth)
+          && rect.bottom >= 0
+          && rect.right >= 0
+      )
+    }
+  }
+
+  onMounted(testBoundingClientRect)
+
+  watch([x, y], testBoundingClientRect)
+
+  return elementIsVisible
+}
