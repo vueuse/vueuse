18238ae4ea58825b9a6c4074f89a1166215f8e80
Scott Bedard <scottbedard@users.noreply.github.com>
2020-03-23 23:00:56 -0700

    feat(useDocumentVisibility): new function (#68)
    * add useDocumentVisibility util
    
    * docs
    
    * linting

---
+++ packages/core/useDocumentVisibility/index.stories.tsx
@@ -0,0 +1,44 @@
+import 'vue-tsx-support/enable-check'
+import Vue from 'vue'
+import { storiesOf } from '@storybook/vue'
+import { defineComponent, ref, watch } from '../../api'
+import { ShowDocs } from '../../_docs/showdocs'
+import { useDocumentVisibility } from '.'
+import { useTimeoutFn } from '../useTimeoutFn'
+
+const Demo = defineComponent({
+  setup() {
+    const startMessage = 'Minimize this page and return'
+    const message = ref(startMessage)
+    const visibility = useDocumentVisibility()
+
+    const timeout = useTimeoutFn(() => {
+      message.value = startMessage
+    }, 3000)
+
+    watch(visibility, (current, previous) => {
+      if (current === 'visible' && previous === 'hidden') {
+        message.value = 'Welcome back!'
+        timeout.start()
+      }
+    })
+
+    return { message }
+  },
+  render(this: Vue & any) {
+    // @ts-ignore
+    const Docs: any = <ShowDocs md={require('./index.md')} />
+
+    return (
+      <div>
+        <div id='demo'>
+          <h1>{this.message}</h1>
+        </div>
+        {Docs}
+      </div>
+    )
+  },
+})
+
+storiesOf('Sensors', module)
+  .add('useDocumentVisibility', () => Demo as any)
