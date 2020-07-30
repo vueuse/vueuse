5aea46a394f7ff800d0efff0ea6ed5f2b2655ed3
Konstantin Barabanov <criitch@yandex.ru>
2020-07-30 15:38:44 +0300

    feat: useDevicePixelRatio (#104)

---
+++ packages/core/useDevicePixelRatio/index.ts
@@ -0,0 +1,48 @@
+import { ref, onMounted, onUnmounted } from 'vue-demi'
+import { useEventListener } from '../useEventListener'
+
+// device pixel ratio statistics from https://www.mydevice.io/
+export const DEVICE_PIXEL_RATIO_SCALES = [
+  1,
+  1.325,
+  1.4,
+  1.5,
+  1.8,
+  2,
+  2.4,
+  2.5,
+  2.75,
+  3,
+  3.5,
+  4,
+]
+
+export function useDevicePixelRatio() {
+  const pixelRatio = ref(typeof window === 'undefined' ? 1 : window.devicePixelRatio)
+
+  if (typeof window === 'undefined') return { pixelRatio }
+
+  const handleDevicePixelRatio = () => {
+    pixelRatio.value = window.devicePixelRatio
+  }
+
+  onMounted(handleDevicePixelRatio)
+
+  useEventListener('resize', handleDevicePixelRatio)
+
+  DEVICE_PIXEL_RATIO_SCALES.forEach((dppx) => {
+    // listen mql events in both sides
+    const mqlMin = window.matchMedia(`screen and (min-resolution: ${dppx}dppx)`)
+    const mqlMax = window.matchMedia(`screen and (max-resolution: ${dppx}dppx)`)
+
+    mqlMin.addListener(handleDevicePixelRatio)
+    mqlMax.addListener(handleDevicePixelRatio)
+
+    onUnmounted(() => {
+      mqlMin.removeListener(handleDevicePixelRatio)
+      mqlMax.removeListener(handleDevicePixelRatio)
+    })
+  })
+
+  return { pixelRatio }
+}
