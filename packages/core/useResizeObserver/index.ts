9517189b831783444a6961d2147c4842fa690169
zhong666 <aa900031@gmail.com>
2020-09-17 10:11:44 +0800

    feat: add dep @types/resize-observer-browser

--- packages/core/useResizeObserver/index.ts
+++ packages/core/useResizeObserver/index.ts
@@ -2,16 +2,16 @@ import { Ref, unref, onBeforeUnmount, watchEffect } from 'vue-demi'
 
 export function useResizeObserver(
   el: Ref<HTMLElement | null | undefined>,
-  callback: (entries: any) => void,
+  callback: ResizeObserverCallback,
 ) {
   const stop = watchEffect((onInvalidate) => {
-    // @ts-ignore
     const observer = new ResizeObserver(callback)
     const _el = unref(el)
     _el && observer.observe(_el)
 
     onInvalidate(() => {
-      observer.unobserve(_el)
+      const _el = unref(el)
+      _el && observer.unobserve(_el)
       observer.disconnect()
     })
   })
