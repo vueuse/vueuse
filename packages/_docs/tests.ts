258a4e9607a0e8ddb20b940335fd56725f2a644b
Ivan Demchuk <ivan.demchuk@gmail.com>
2020-09-22 19:15:53 +0300

    test: fix storage tests

--- packages/_docs/tests.ts
+++ packages/_docs/tests.ts
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import { createLocalVue, shallowMount } from '@vue/test-utils'
 import VueCompositionApi from '@vue/composition-api'
-import { defineComponent } from 'vue-demi'
+import { defineComponent, UnwrapRef } from 'vue-demi'
 
 const localVue = createLocalVue()
 localVue.use(VueCompositionApi)
@@ -14,5 +14,5 @@ export function renderHook<V, Props = unknown, Data = unknown>(
     template: '<div ref="app" id="app"></div>',
   })
 
-  return shallowMount<Vue & V>(App as any, { localVue })
+  return shallowMount<Vue & UnwrapRef<V>>(App as any, { localVue })
 }
