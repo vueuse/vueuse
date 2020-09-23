df819454408e04261d704510bcf93c6008a72a38
Ivan Demchuk <ivan.demchuk@gmail.com>
2020-09-23 12:18:51 +0300

    chore: renames

--- packages/core/createGlobalState/index.ts
+++ packages/core/createGlobalState/index.ts
@@ -1,6 +1,6 @@
 import { createApp, reactive } from 'vue-demi'
 
-function createInstance<T extends object>(factory: () => T): T {
+function withScope<T extends object>(factory: () => T): T {
   const container = document.createElement('div')
 
   let state: T = null as any
@@ -18,12 +18,12 @@ function createInstance<T extends object>(factory: () => T): T {
 export function createGlobalState<T extends object>(
   factory: () => T,
 ) {
-  let instance: T
+  let state: T
 
   return () => {
-    if (instance == null)
-      instance = createInstance(factory)
+    if (state == null)
+      state = withScope(factory)
 
-    return instance
+    return state
   }
 }
