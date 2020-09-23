86931a0464f17c8e85b40f7c5549f055935f5e9c
Ivan Demchuk <ivan.demchuk@gmail.com>
2020-09-23 11:18:30 +0300

    test: add test for deep watch of global state

--- packages/core/createGlobalState/index.test.ts
+++ packages/core/createGlobalState/index.test.ts
@@ -100,4 +100,77 @@ describe('createGlobalState', () => {
     expect(vm2.ref).toBe('c')
     expect(localStorage.setItem).toBeCalledWith(KEY, 'c')
   })
+
+  it('watches deeply', async() => {
+    const useGlobalState = createGlobalState(() => useStorage(KEY, { a: 'a', b: 1 }))
+
+    const instance1 = renderHook(() => {
+      const ref = useGlobalState()
+
+      expect(ref.value).toEqual({
+        a: 'a',
+        b: 1,
+      })
+      expect(localStorage.setItem).toBeCalledWith(KEY, '{"a":"a","b":1}')
+      // @ts-ignore
+      localStorage.setItem.mockClear()
+
+      return {
+        ref,
+      }
+    })
+
+    const vm1 = instance1.vm
+
+    vm1.ref.a = 'b'
+    await vm1.$nextTick()
+
+    expect(vm1.ref).toEqual({
+      a: 'b',
+      b: 1,
+    })
+    expect(localStorage.setItem).toBeCalledWith(KEY, '{"a":"b","b":1}')
+    // @ts-ignore
+    localStorage.setItem.mockClear()
+
+    instance1.destroy()
+
+    const instance2 = renderHook(() => {
+      const ref = useGlobalState()
+
+      expect(ref.value).toEqual({
+        a: 'b',
+        b: 1,
+      })
+
+      return {
+        ref,
+      }
+    })
+
+    const vm2 = instance2.vm
+
+    vm2.ref.b = 2
+    await vm2.$nextTick()
+
+    expect(vm2.ref).toEqual({
+      a: 'b',
+      b: 2,
+    })
+    expect(localStorage.setItem).toBeCalledWith(KEY, '{"a":"b","b":2}')
+    // @ts-ignore
+    localStorage.setItem.mockClear()
+
+    vm2.ref = {
+      c: 'c',
+      d: 4,
+    }
+    await vm2.$nextTick()
+
+    expect(vm2.ref).toEqual({
+      c: 'c',
+      d: 4,
+    })
+    expect(localStorage.setItem).toBeCalledWith(KEY, '{"c":"c","d":4}')
+  })
 })
