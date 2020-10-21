b755b5b7ac9f63534b49387f918913e0ede372a8
Anthony Fu <anthonyfu117@hotmail.com>
2020-10-21 11:38:42 +0800

    chore: scripts to update version

--- scripts/update-package.ts
+++ scripts/update-package.ts
@@ -3,14 +3,18 @@ import fs from 'fs-extra'
 import consola from 'consola'
 import { activePackages } from './packages'
 
-const srcDir = path.resolve(__dirname, '../packages')
+const rootDir = path.resolve(__dirname, '..')
+const srcDir = path.resolve(rootDir, 'packages')
 
 export async function updatePackageJSON() {
+  const { version } = await fs.readJSON(path.join(rootDir, 'package.json'))
+
   for (const { name, description, author } of activePackages) {
     const packageDir = path.join(srcDir, name)
     const packageJSONPath = path.join(packageDir, 'package.json')
     const packageJSON = await fs.readJSON(packageJSONPath)
 
+    packageJSON.version = version
     packageJSON.description = description
     packageJSON.author = author || 'Anthony Fu<https://github.com/antfu>'
     packageJSON.bugs = {
@@ -19,6 +23,11 @@ export async function updatePackageJSON() {
     packageJSON.homepage = name === 'core'
       ? 'https://github.com/antfu/vueuse#readme'
       : `https://github.com/antfu/vueuse/tree/master/packages/${name}#readme`
+
+    for (const key of Object.keys(packageJSON.dependencies)) {
+      if (key.startsWith('@vueuse/'))
+        packageJSON.dependencies[key] = version
+    }
 
     await fs.writeJSON(packageJSONPath, packageJSON, { spaces: 2 })
 
