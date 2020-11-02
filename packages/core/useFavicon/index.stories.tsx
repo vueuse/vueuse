12f27f0787e9a21e84f3347a354c4fd5ab67207c
Antério Vieira <anteriovieira@gmail.com>
2020-11-02 10:11:13 -0300

    chore: applies the new style to define the story

--- packages/core/useFavicon/index.stories.tsx
+++ packages/core/useFavicon/index.stories.tsx
@@ -1,20 +1,10 @@
-import 'vue-tsx-support/enable-check'
-import Vue from 'vue'
-import { storiesOf } from '@storybook/vue'
-import { defineComponent } from 'vue-demi'
-import { ShowDocs } from '../../_docs/showdocs'
+import { defineDemo, html } from '../../_docs'
 
-const Demo = defineComponent({
-  render(this: Vue & any) {
-    // @ts-ignore
-    const Docs: any = <ShowDocs md={require('./index.md')} />
-
-    return (
-      <div>
-        {Docs}
-      </div>
-    )
-  },
-})
-storiesOf('Browser', module)
-  .add('useFavicon', () => Demo as any)
+defineDemo(
+  {
+    name: 'useFavicon',
+    category: 'Browser',
+    docs: require('./index.md'),
+    module,
+  }
+)
