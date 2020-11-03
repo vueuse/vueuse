0f8245fef632316b682f3fe16df3e8fca1454041
Antério Vieira <anteriovieira@gmail.com>
2020-11-03 10:36:23 -0300

    docs(core/useEventListener): updates the story template

--- packages/core/useEventListener/index.stories.tsx
+++ packages/core/useEventListener/index.stories.tsx
@@ -1,20 +1,10 @@
-import 'vue-tsx-support/enable-check'
-import Vue from 'vue'
-import { storiesOf } from '@storybook/vue'
-import { defineComponent } from 'vue-demi'
-import { ShowDocs } from '../../_docs/showdocs'
+import { defineDemo } from '../../_docs'
 
-const Demo = defineComponent({
-  render(this: Vue & any) {
-    // @ts-ignore
-    const Docs: any = <ShowDocs md={require('./index.md')} />
-
-    return (
-      <div>
-        {Docs}
-      </div>
-    )
+defineDemo(
+  {
+    name: 'useEventListener',
+    category: 'Browser',
+    docs: require('./index.md'),
+    module,
   },
-})
-storiesOf('Browser', module)
-  .add('useEventListener', () => Demo as any)
+)
