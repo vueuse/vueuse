50ccd88334c00fdb878fe17c87e3634aa26ee005
Anthony Fu <anthonyfu117@hotmail.com>
2020-11-04 19:51:01 +0800

    test

--- packages/core/useMouse/index.ts
+++ packages/core/useMouse/index.ts
@@ -1,5 +1,4 @@
 import { ref } from 'vue-demi'
-import { useEventListener } from '../useEventListener'
 
 export function useMouse(options: {
   touch?: boolean
@@ -7,33 +6,11 @@ export function useMouse(options: {
   initial?: {x: number; y: number}
 } = {}) {
   const {
-    touch = true,
-    resetOnTouchEnds = true,
     initial = { x: 0, y: 0 },
   } = options
 
   const x = ref(initial.x)
   const y = ref(initial.y)
-
-  useEventListener('mousemove', (event) => {
-    x.value = event.pageX
-    y.value = event.pageY
-  })
-
-  if (touch) {
-    useEventListener('touchmove', (event) => {
-      if (event.touches.length > 0) {
-        x.value = event.touches[0].clientX
-        y.value = event.touches[0].clientY
-      }
-    })
-    if (resetOnTouchEnds) {
-      useEventListener('touchend', () => {
-        x.value = initial.x
-        y.value = initial.y
-      })
-    }
-  }
 
   return {
     x,
