d2cc8df4e912502ef5c5a64a0bf8d6cf74261033
Richard Musiol <mail@richard-musiol.de>
2020-11-05 11:11:14 +0100

    fix: remove "const" from WorkerStatus enum
    This is to allow use of the TypeScript compiler option --isolatedModules.
    
    Fixes #172

--- packages/core/useWebWorkerFn/index.ts
+++ packages/core/useWebWorkerFn/index.ts
@@ -4,7 +4,7 @@ import { ref, Ref } from 'vue-demi'
 import createWorkerBlobUrl from './lib/createWorkerBlobUrl'
 import { tryOnMounted, tryOnUnmounted } from '@vueuse/shared'
 
-export const enum WorkerStatus {
+export enum WorkerStatus {
   Pending = 'PENDING',
   Success = 'SUCCESS',
   Runing = 'RUNNING',
