9a9d12da336e34491c06f012ecf988210c9e260d
Antério Vieira <anteriovieira@gmail.com>
2020-11-04 23:50:03 -0300

    docs(core/usePreferredLanguages): updates the story template

--- packages/core/usePreferredLanguages/index.stories.tsx
+++ packages/core/usePreferredLanguages/index.stories.tsx
@@ -1,35 +1,25 @@
-import 'vue-tsx-support/enable-check'
-import Vue from 'vue'
-import { storiesOf } from '@storybook/vue'
+import { defineDemo, html } from '../../_docs'
 import { defineComponent } from 'vue-demi'
-import { ShowDocs } from '../../_docs/showdocs'
 import { usePreferredLanguages } from '.'
 
-const Demo = defineComponent({
-  setup() {
-    return {
-      usePreferredLanguages: usePreferredLanguages(),
-    }
+defineDemo(
+  {
+    name: 'usePreferredLanguages',
+    category: 'Browser',
+    docs: require('./index.md'),
+    module,
   },
+  defineComponent({
+    setup() {
+      return {
+        usePreferredLanguages: usePreferredLanguages(),
+      }
+    },
 
-  render(this: Vue & any) {
-    const {
-      usePreferredLanguages,
-    } = this
-
-    // @ts-ignore
-    const Docs: any = <ShowDocs md={require('./index.md')} />
-
-    return (
+    template: html`
       <div>
-        <div id="demo">
-          <p>Preferred Languages: <code>{usePreferredLanguages.join(', ')}</code></p>
-        </div>
-        {Docs}
+        <p>Preferred Languages: <code>{{usePreferredLanguages.join(', ')}}</code></p>
       </div>
-    )
-  },
-})
-
-storiesOf('Browser', module)
-  .add('usePreferredLanguages', () => Demo as any)
+    `,
+  }),
+)
