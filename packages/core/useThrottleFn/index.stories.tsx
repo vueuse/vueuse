22e249c0b3c5da00008acc1ca47ef7def4b9c9b9
Antério Vieira <anteriovieira@gmail.com>
2020-11-05 21:48:14 -0300

    docs(core/useThrottleFn): updates the story template

--- packages/core/useThrottleFn/index.stories.tsx
+++ packages/core/useThrottleFn/index.stories.tsx
@@ -1,56 +1,43 @@
-import 'vue-tsx-support/enable-check'
-import Vue from 'vue'
-import { storiesOf } from '@storybook/vue'
+import { defineDemo, html } from '../../_docs'
 import { defineComponent, ref } from 'vue-demi'
-import { ShowDocs } from '../../_docs/showdocs'
 import { useThrottleFn } from '.'
 
-type Inject = {
-  clicked: number
-  updated: number
-  clickedFn: Function
-  throttledFn: Function
-}
-
-const Demo = defineComponent({
-  setup() {
-    const updated = ref(0)
-    const clicked = ref(0)
-    const throttledFn = useThrottleFn(() => {
-      updated.value += 1
-    }, 1000)
-
-    const clickedFn = () => {
-      clicked.value += 1
-      throttledFn()
-    }
-
-    return {
-      clicked,
-      clickedFn,
-      throttledFn,
-      updated,
-    }
+defineDemo(
+  {
+    name: 'useThrottleFn',
+    category: 'Utilities',
+    docs: require('./index.md'),
+    module,
   },
+  defineComponent({
+    setup() {
+      const updated = ref(0)
+      const clicked = ref(0)
+      const throttledFn = useThrottleFn(() => {
+        updated.value += 1
+      }, 1000)
 
-  render(this: Vue & Inject) {
-    // @ts-ignore
-    const Docs = <ShowDocs md={require('./index.md')} />
+      const clickedFn = () => {
+        clicked.value += 1
+        throttledFn()
+      }
 
-    return (
+      return {
+        clicked,
+        clickedFn,
+        throttledFn,
+        updated,
+      }
+    },
+
+    template: html`
       <div>
-        <div id="demo">
-          <button onClick={() => this.clickedFn()}>Smash me!</button>
+          <button @click="clickedFn">Smash me!</button>
           <note>Delay is set to 1000ms for this demo.</note>
 
-          <p>Button clicked: {this.clicked}</p>
-          <p>Event handler called: {this.updated}</p>
-        </div>
-        {Docs}
+          <p>Button clicked: {{clicked}}</p>
+          <p>Event handler called: {{updated}}</p>
       </div>
-    )
-  },
-})
-
-storiesOf('Utilities', module)
-  .add('useThrottleFn', () => Demo as any)
+    `,
+  }),
+)
