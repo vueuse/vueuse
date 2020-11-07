5e4f689296b8e15bce3150e2f35d56834db0c31f
Antério Vieira <anteriovieira@gmail.com>
2020-11-07 00:52:28 -0300

    docs(core/useI18n): updates the story template

--- packages/i18n/useI18n/index.stories.tsx
+++ packages/i18n/useI18n/index.stories.tsx
@@ -1,47 +1,36 @@
-import 'vue-tsx-support/enable-check'
-import Vue from 'vue'
-import { storiesOf } from '@storybook/vue'
 import { defineComponent } from 'vue-demi'
-import { ShowDocs } from '../../_docs/showdocs'
 import { useI18n } from '.'
+import { defineDemo, html } from '../../_docs'
 
-const Demo = defineComponent({
-  setup() {
-    return useI18n({
-      locale: 'en',
-      messages: {
-        en: {
-          hello: 'Hello',
-        },
-        zhCN: {
-          hello: '你好',
-        },
-      },
-    })
+defineDemo(
+  {
+    name: 'useI18n',
+    category: '@i18n',
+    docs: require('./index.md'),
+    module,
   },
+  defineComponent({
+    setup() {
+      return useI18n({
+        locale: 'en',
+        messages: {
+          en: {
+            hello: 'Hello',
+          },
+          zhCN: {
+            hello: '你好',
+          },
+        },
+      })
+    },
 
-  render(this: Vue & any) {
-    const {
-      t,
-      locale,
-    } = this
-
-    // @ts-ignore
-    const Docs: any = <ShowDocs md={require('./index.md')} />
-
-    return (
+    template: html`
       <div>
-        <div id="demo">
-          <note>Hello in {locale}</note>
-          <p>{t('hello')}</p>
-          <br></br>
-          <button onClick={() => this.locale = locale === 'en' ? 'zhCN' : 'en'}>Switch</button>
-        </div>
-        {Docs}
+        <note>Hello in {{locale}}</note>
+        <p>{{t('hello')}}</p>
+        <br></br>
+        <button @click="locale = (locale === 'en' ? 'zhCN' : 'en')">Switch</button>
       </div>
-    )
-  },
-})
-
-storiesOf('@i18n', module)
-  .add('useI18n', () => Demo as any)
+    `,
+  }),
+)
