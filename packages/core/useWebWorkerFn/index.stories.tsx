3c5c4e5e6f60b5e57e40f954629e7f5cc8cb31a2
Antério Vieira <anteriovieira@gmail.com>
2020-11-07 02:42:06 -0300

    docs: update stories template (#178)
    * docs(core/useWebWorkerFn): updates the story template
    
    * docs(core/useWindowScroll): updates the story template
    
    * docs(core/useWindowSize): updates the story template
    
    * docs(core/useFirestore): updates the story template
    
    * docs(core/useRTDB): updates the story template
    
    * docs(core/createI18n): updates the story template
    
    * docs(core/useI18n): updates the story template

--- packages/core/useWebWorkerFn/index.stories.tsx
+++ packages/core/useWebWorkerFn/index.stories.tsx
@@ -1,77 +1,59 @@
-import 'vue-tsx-support/enable-check'
-import Vue from 'vue'
 import dayjs from 'dayjs'
-import { storiesOf } from '@storybook/vue'
-import { defineComponent } from 'vue-demi'
-import { ShowDocs } from '../../_docs/showdocs'
-import { useWebWorkerFn, WorkerStatus } from './'
+import { defineDemo, html } from '../../_docs'
+import { defineComponent, computed } from 'vue-demi'
+import { useWebWorkerFn, WorkerStatus } from '.'
 import { useNow } from '../useNow'
-
-type Inject = {
-  time: number
-  baseSort: Function
-  workerSort: Function
-  workerStatus: string
-  workerTerminate: Function
-}
 
 const numbers: number[] = [...Array(5000000)].map(_ => ~~(Math.random() * 1000000))
 const sortNumbers = (nums: number[]): number[] => nums.sort()
 
-const Demo = defineComponent({
-  setup() {
-    const { workerFn, workerStatus, workerTerminate } = useWebWorkerFn(sortNumbers)
-    const time = useNow()
-
-    const baseSort = () => {
-      const data = sortNumbers(numbers)
-      console.log('Normal sort (top 10 of 5000000)', data.slice(0, 10))
-    }
-    const workerSort = async() => {
-      const data = await workerFn(numbers)
-      console.log('Worker sort (top 10 of 5000000)', data.slice(0, 10))
-    }
-
-    return {
-      time,
-      baseSort,
-      workerSort,
-      workerStatus,
-      workerTerminate,
-    }
+defineDemo(
+  {
+    name: 'useWebWorkerFn',
+    category: 'Misc',
+    docs: require('./index.md'),
+    module,
   },
+  defineComponent({
+    setup() {
+      const { workerFn, workerStatus, workerTerminate } = useWebWorkerFn(sortNumbers)
+      const time = useNow()
+      const computedTime = computed(() => dayjs(time.value).format('YYYY-MM-DD HH:mm:ss SSS'))
+      const running = computed(() => workerStatus.value === WorkerStatus.Runing)
 
-  render(this: Vue & Inject) {
-    const { baseSort, workerSort, time, workerStatus, workerTerminate } = this
+      const baseSort = () => {
+        const data = sortNumbers(numbers)
+        console.log('Normal sort (top 10 of 5000000)', data.slice(0, 10))
+      }
 
-    // @ts-ignore
-    const Docs = <ShowDocs md={require('./index.md')} />
+      const workerSort = async() => {
+        const data = await workerFn(numbers)
+        console.log('Worker sort (top 10 of 5000000)', data.slice(0, 10))
+      }
 
-    return (
+      return {
+        running,
+        baseSort,
+        workerSort,
+        computedTime,
+        workerTerminate,
+      }
+    },
+
+    template: html`
       <div>
-        <div id="demo">
-          <p>Current Time: {dayjs(time).format('YYYY-MM-DD HH:mm:ss SSS')}</p>
-          <note>Open console to see the sorted result. Clock stops when UI blocking happends.</note>
-          <button onClick={() => baseSort()}>
-            Normal Sort
-          </button>
-          {
-            workerStatus !== WorkerStatus.Runing ? (
-              <button onClick={() => workerSort()}>
-                Worker Sort
-              </button>
-            ) : (
-              // @ts-ignore
-              <button onClick={() => workerTerminate()} class="orange">
-                Terminate Worker
-              </button>
-            )
-          }
-        </div>
-        {Docs}
+        <p>Current Time: {{computedTime}}</p>
+        <note>Open console to see the sorted result. Clock stops when UI blocking happends.</note>
+        <button @click="baseSort">
+          Normal Sort
+        </button>
+        <button v-if="!running" @click="workerSort">
+          Worker Sort
+        </button>
+        <button v-else @click="workerTerminate" class="orange">
+          Terminate Worker
+        </button>
       </div>
-    )
-  },
-})
-
-storiesOf('Misc', module).add('useWebWorkerFn', () => Demo as any)
+    `,
+  }),
+)
