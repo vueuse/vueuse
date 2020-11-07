5c6944d59ef26c8a0bc0683162dfca059ccc3ab0
Scott Bedard <scottbedard90@gmail.com>
2020-11-07 14:50:27 -0700

    verify transition function support

--- packages/core/useTransition/index.test.ts
+++ packages/core/useTransition/index.test.ts
@@ -59,7 +59,34 @@ describe('useTransition', () => {
 
     setTimeout(() => {
       expect(vm.transitionedValue > 0 && vm.transitionedValue < 1).toBe(true)
-      
+
+      setTimeout(() => {
+        expect(vm.transitionedValue).toBe(1)
+        done()
+      }, 100)
+    }, 50)
+  })
+
+  it('supports custom function transitions', (done) => {
+    const { vm } = renderHook(() => {
+      const baseValue = ref(0)
+
+      const transitionedValue = useTransition(baseValue, {
+        duration: 100,
+        transition: n => n,
+      })
+
+      return {
+        baseValue,
+        transitionedValue,
+      }
+    })
+
+    vm.baseValue = 1;
+
+    setTimeout(() => {
+      expect(vm.transitionedValue > 0 && vm.transitionedValue < 1).toBe(true)
+
       setTimeout(() => {
         expect(vm.transitionedValue).toBe(1)
         done()
