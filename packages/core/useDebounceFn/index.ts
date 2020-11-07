490dd18245c246d245ca44484e6d71b13be5b7b6
maxma241 <marfi241@gmail.com>
2020-11-07 17:07:54 +0800

    fix: format

--- packages/core/useDebounceFn/index.ts
+++ packages/core/useDebounceFn/index.ts
@@ -1,4 +1,4 @@
-export function useDebounceFn<T extends Function, D extends number>(fn: T, delay: D = 200 as any ): D extends 0 ? T : () => void {
+export function useDebounceFn<T extends Function, D extends number>(fn: T, delay: D = 200 as any): D extends 0 ? T : () => void {
   if (delay <= 0)
     return fn as any
 
