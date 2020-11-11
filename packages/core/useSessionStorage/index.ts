b32ff40c58dc933087325b9fc74b3a3315baef2b
Anthony Fu <anthonyfu117@hotmail.com>
2020-11-11 13:17:51 +0800

    fix(useStorage): types, close #197, close #198

--- packages/core/useSessionStorage/index.ts
+++ packages/core/useSessionStorage/index.ts
@@ -1,11 +1,11 @@
 import { useStorage } from '../useStorage'
 import { Ref } from 'vue-demi'
 
-export function useSessionStorage (key: string, defaultValue: string, storage?: Storage): Ref<string>
-export function useSessionStorage (key: string, defaultValue: boolean, storage?: Storage): Ref<boolean>
-export function useSessionStorage(key: string, defaultValue: number, storage?: Storage): Ref<number>
-export function useSessionStorage<T> (key: string, defaultValue: T, storage?: Storage): Ref<T>
-export function useSessionStorage<T = unknown> (key: string, defaultValue: null, storage?: Storage): Ref<T>
+export function useSessionStorage (key: string, defaultValue: string): Ref<string>
+export function useSessionStorage (key: string, defaultValue: boolean): Ref<boolean>
+export function useSessionStorage(key: string, defaultValue: number): Ref<number>
+export function useSessionStorage<T> (key: string, defaultValue: T): Ref<T>
+export function useSessionStorage<T = unknown> (key: string, defaultValue: null): Ref<T>
 export function useSessionStorage<T extends(string|number|boolean|object|null)> (key: string, defaultValue: T): Ref<any> {
   // @ts-ignore
   return useStorage(key, defaultValue, sessionStorage)
