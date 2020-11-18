9394e47a1831f9a47d3b9142e2afef22bd2fee1a
Antério Vieira <anteriovieira@gmail.com>
2020-11-17 22:31:18 -0300

    chore(core/useRTDB): adds jsdoc

--- packages/firebase/useRTDB/index.ts
+++ packages/firebase/useRTDB/index.ts
@@ -2,6 +2,12 @@ import type firebase from 'firebase'
 import { ref } from 'vue-demi'
 import { tryOnUnmounted } from '@vueuse/shared'
 
+/**
+ * Reactive Firebase Realtime Database binding.
+ *
+ * @see   {@link https://vueuse.js.org/useRTDB}
+ * @param docRef
+ */
 export function useRTDB(
   docRef: firebase.database.Reference,
 ) {
