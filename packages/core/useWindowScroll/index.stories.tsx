5fa649a49c02a1ebdb3656a4789e83286e656db0
Nurettin Kaya <sibbngheid@gmail.com>
2020-11-18 18:56:29 +0300

    chore: fix demo styles (#220)

--- packages/core/useWindowScroll/index.stories.tsx
+++ packages/core/useWindowScroll/index.stories.tsx
@@ -17,11 +17,15 @@ defineDemo(
     template: html`
       <div>
         <note>See scroll values in the lower right corner of the screen.</note>
-        <div style="position: absolute; width: 10000px; height: 10000px;"></div>
-        <div id="demo" style="
-          position: fixed;
+        <div style="
+          position: absolute; 
           top: 100%;
           left: 100%;
+          width: 10000px; 
+          height: 10000px;
+        "></div>
+        <div id="demo" style="
+          position: fixed;
           bottom: 0;
           right: 0;
           padding: 1em 5em 1em 1.5em;
