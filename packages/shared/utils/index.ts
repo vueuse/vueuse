5f8663b7b53c84d8adc0d821f79fbe1029ac4dc4
liaoliao666 <1076988944@qq.com>
2020-12-07 14:10:09 +0800

    eslint(shared/utils/index.ts)

--- packages/shared/utils/index.ts
+++ packages/shared/utils/index.ts
@@ -1,40 +1,18 @@
-export * from "./is";
-export * from "./filters";
-export * from "./types";
+export * from './is'
+export * from './filters'
+export * from './types'
 
 export function promiseTimeout(
   ms: number,
   throwOnTimeout = false,
-  reason = "Timeout"
+  reason = 'Timeout',
 ): Promise<void> {
   return new Promise((resolve, reject) => {
-    if (throwOnTimeout) setTimeout(() => reject(reason), ms);
-    else setTimeout(resolve, ms);
-  });
+    if (throwOnTimeout) setTimeout(() => reject(reason), ms)
+    else setTimeout(resolve, ms)
+  })
 }
 
 export function invoke<T>(fn: () => T): T {
-  return fn();
+  return fn()
 }
-
-interface Todo {
-  title: string;
-  description: string;
-  completed: boolean;
-}
-
-const todo: Todo = {
-  title: "string",
-  description: "string",
-  completed: true,
-};
-
-interface A {
-  [key: string]: unknown;
-}
-
-function a(arg: { [key: string]: unknown }) {
-  return undefined;
-}
-
-a(todo);
