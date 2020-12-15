6513b34240f06b4f3e89a2a058daf3e4d603655c
yangger6 <i@yangger.cn>
2020-12-15 18:04:03 +0800

    fix#253

--- packages/core/useVModel/index.ts
+++ packages/core/useVModel/index.ts
@@ -10,7 +10,8 @@ import { computed, getCurrentInstance, isVue3 } from 'vue-demi'
  */
 export function useVModel<P extends object>(props: P, key: keyof P, emit?: (name: string, value: any) => void) {
   // @ts-expect-error mis-alignment with @vue/composition-api
-  const _emit = emit || isVue3 ? getCurrentInstance()?.emit : getCurrentInstance()?.$emit
+  const vueCurrentInstance = getCurrentInstance()
+  const _emit = emit || isVue3 ? vueCurrentInstance?.emit : vueCurrentInstance?.$emit?.bind(vueCurrentInstance)
 
   return computed({
     get() {
