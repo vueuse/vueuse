6acb10d78f7f73e83ea693d5f7879ab5b2005c37
lstoeferle <lukas.stoeferle@share-now.com>
2020-12-21 11:59:03 +0100

    refactor: removed dependecy to useBrowserLocation

--- packages/core/useUrlSearchParams/index.ts
+++ packages/core/useUrlSearchParams/index.ts
@@ -1,37 +1,34 @@
 import { computed, Ref, ref, watch } from 'vue-demi'
-import { useBrowserLocation } from '..'
 import { useEventListener } from '../useEventListener'
 
 export function useUrlSearchParams(method: 'history'|'hash'): Ref<URLSearchParams> {
-  const location = useBrowserLocation()
-
   const hashWithoutParams = computed((): string => {
-    const hash = location.value.hash || ''
+    const hash = window.location.hash || ''
     const paramsIndex = hash.indexOf('?')
     return paramsIndex > 0 ? hash.substring(0, paramsIndex) : hash
   })
 
   const readParams = (): URLSearchParams => {
     if (method === 'hash') {
-      const hash = location.value.hash || ''
+      const hash = window.location.hash || ''
       const iParams = hash.indexOf('?')
       return iParams ? new URLSearchParams(hash.substring(iParams + 1)) : new URLSearchParams('')
     }
     else {
-      return new URLSearchParams(location.value.search || '')
+      return new URLSearchParams(window.location.search || '')
     }
   }
 
   const writeParams = (params: URLSearchParams) => {
     if (method === 'hash') {
       params.entries().next().done
-        ? window.history.replaceState({}, '', `${location.value.pathname}${hashWithoutParams.value}`)
-        : window.history.replaceState({}, '', `${location.value.pathname}${hashWithoutParams.value}?${params}`)
+        ? window.history.replaceState({}, '', `${window.location.pathname}${hashWithoutParams.value}`)
+        : window.history.replaceState({}, '', `${window.location.pathname}${hashWithoutParams.value}?${params}`)
     }
     else {
       params.entries().next().done
-        ? window.history.replaceState({}, '', `${location.value.pathname}${hashWithoutParams.value}`)
-        : window.history.replaceState({}, '', `${location.value.pathname}?${params}${hashWithoutParams.value}`)
+        ? window.history.replaceState({}, '', `${window.location.pathname}${hashWithoutParams.value}`)
+        : window.history.replaceState({}, '', `${window.location.pathname}?${params}${hashWithoutParams.value}`)
     }
   }
 
