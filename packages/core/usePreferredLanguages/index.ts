18af0543e6fba01360afca121cffa9fb63b27cc7
Anthony Fu <anthonyfu117@hotmail.com>
2021-01-05 19:22:30 +0800

    fix(usePreferredLanguages): types

--- packages/core/usePreferredLanguages/index.ts
+++ packages/core/usePreferredLanguages/index.ts
@@ -1,4 +1,4 @@
-import { ref } from 'vue-demi'
+import { ref, Ref } from 'vue-demi'
 import { useEventListener } from '../useEventListener'
 import { ConfigurableWindow, defaultWindow } from '../_configurable'
 
@@ -8,13 +8,13 @@ import { ConfigurableWindow, defaultWindow } from '../_configurable'
  * @see   {@link https://vueuse.js.org/usePreferredLanguages}
  * @param options
  */
-export function usePreferredLanguages(options: ConfigurableWindow = {}) {
+export function usePreferredLanguages(options: ConfigurableWindow = {}): Ref<readonly string[]> {
   const { window = defaultWindow } = options
   if (!window)
-    return ref('en')
+    return ref(['en'])
 
   const navigator = window.navigator
-  const value = ref(navigator.languages)
+  const value = ref<readonly string[]>(navigator.languages)
 
   useEventListener(window, 'languagechange', () => {
     value.value = navigator.languages
