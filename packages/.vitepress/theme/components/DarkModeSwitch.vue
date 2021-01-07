c39814cabd0114bd7a73c3cb5f3d1939f60e0bfb
Anthony Fu <anthonyfu117@hotmail.com>
2021-01-08 04:19:19 +0800

    chore: style changes

--- packages/.vitepress/theme/components/DarkModeSwitch.vue
+++ packages/.vitepress/theme/components/DarkModeSwitch.vue
@@ -9,17 +9,22 @@
 import Moon from './icons/Moon.vue'
 import Sun from './icons/Sun.vue'
 import { isDark } from '../composables/darkmode'
+import { useToggle } from '../../../shared'
 
-const toggle = () => isDark.value = !isDark.value
+const toggle = useToggle(isDark)
 </script>
 
 <style scoped>
 .switch {
-  font-size: 1.1em;
+  display: flex;
+  font-size: 1.2em;
   margin-left: 1rem;
   border: 0;
   outline: none;
   background: none;
   color: var(--c-text);
+}
+.switch svg {
+  margin: auto;
 }
 </style>
