7f30b3a4aee33798a3a681fff3a134a3cdad0d91
zhong666 <aa900031@gmail.com>
2021-01-14 00:47:46 +0800

    fix(templateRef): Vue 3 test

--- packages/core/templateRef/index.test.ts
+++ packages/core/templateRef/index.test.ts
@@ -19,54 +19,54 @@ describe('templateRef', () => {
   it('string ref mount', () => {
     const refKey = 'target'
     const vm = useSetupWithRender(() => {
-      const $target = templateRef<HTMLElement | null>(refKey, null)
-      return { $target }
+      const targetEl = templateRef<HTMLElement | null>(refKey, null)
+      return { targetEl }
     }, () => h('div', { ref: refKey }))
 
-    expect(vm.$target).toBe(vm.$el)
+    expect(vm.targetEl).toBe(vm.$el)
   })
 
   it('string ref update', async() => {
     const vm = useSetupWithRender(() => {
       const refKey = ref('foo')
-      const $foo = templateRef<HTMLElement | null>('foo', null)
-      const $bar = templateRef<HTMLElement | null>('bar', null)
+      const fooEl = templateRef<HTMLElement | null>('foo', null)
+      const barEl = templateRef<HTMLElement | null>('bar', null)
       return {
         refKey,
-        $foo,
-        $bar,
+        fooEl,
+        barEl,
       }
     }, function() {
       return h('div', { ref: this.refKey })
     })
 
-    expect(vm.$foo).toBe(vm.$el)
-    expect(vm.$bar).toBe(null)
+    expect(vm.fooEl).toBe(vm.$el)
+    expect(vm.barEl).toBe(null)
 
     vm.refKey = 'bar'
     await nextTick()
 
-    expect(vm.$foo).toBe(null)
-    expect(vm.$bar).toBe(vm.$el)
+    expect(vm.fooEl).toBe(null)
+    expect(vm.barEl).toBe(vm.$el)
   })
 
   it('string ref unmount', async() => {
     const vm = useSetupWithRender(() => {
       const toggle = ref(true)
-      const $target = templateRef<HTMLElement | null>('target', null)
+      const targetEl = templateRef<HTMLElement | null>('target', null)
       return {
         toggle,
-        $target,
+        targetEl,
       }
     }, function() {
       return this.toggle ? h('div', { ref: 'target' }) : null
     })
 
-    expect(vm.$target).toBe(vm.$el)
+    expect(vm.targetEl).toBe(vm.$el)
 
     vm.toggle = false
     await nextTick()
 
-    expect(vm.$target).toBe(null)
+    expect(vm.targetEl).toBe(null)
   })
 })
