cb8e788bfa237479b8fbdea72cfa8d55f65b6ff4
Homyee King <49113249+HomyeeKing@users.noreply.github.com>
2021-01-22 23:47:56 +0800

    feat: add useVModel default value (#298)
    Co-authored-by: Anthony Fu <anthonyfu117@hotmail.com>

--- packages/core/useVModel/index.ts
+++ packages/core/useVModel/index.ts
@@ -1,25 +1,28 @@
-import { computed, getCurrentInstance } from 'vue-demi'
+import { computed, getCurrentInstance, isVue2 } from 'vue-demi'
 
 /**
  * Shorthand for v-model binding, props + emit -> ref
  *
  * @see   {@link https://vueuse.js.org/useVModel}
  * @param props
- * @param key
+ * @param key (default 'value' in Vue 2 and 'modelValue' in Vue 3)
  * @param emit
  */
 export function useVModel<P extends object, K extends keyof P>(
   props: P,
-  key: K,
+  key?: K,
   emit?: (name: string, value: any) => void,
 ) {
   const vm = getCurrentInstance()
   // @ts-expect-error mis-alignment with @vue/composition-api
   const _emit = emit || vm?.emit || vm?.$emit?.bind(vm)
 
+  if (!key)
+    key = (isVue2 ? 'value' : 'modelValue') as K
+
   return computed<P[K]>({
     get() {
-      return props[key]
+      return props[key!]
     },
     set(value) {
       _emit(`update:${key}`, value)
