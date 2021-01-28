efc3655e42f731e0b4bdeb12d2a95c78252e0756
Jacob Clevenger <jacobrclevenger@gmail.com>
2021-01-28 05:12:22 -0500

    Removed login/logout functions from `useAuth`

--- packages/firebase/useAuth/index.ts
+++ packages/firebase/useAuth/index.ts
@@ -12,17 +12,6 @@ interface UseAuth {
   authenticationStatus: Ref<FirebaseAuthStatus>
   isAuthenticated: ComputedRef<boolean>
   user: Ref<firebase.User | null>
-
-  signInAnonymously(): Promise<firebase.auth.UserCredential>
-  signInWithCredential(credential: firebase.auth.AuthCredential): Promise<firebase.auth.UserCredential>
-  signInWithCustomToken(token: string): Promise<firebase.auth.UserCredential>
-  signInWithEmailAndPassword(email: string, password: string): Promise<firebase.auth.UserCredential>
-  signInWithEmailLink(email: string, emailLink?: string | undefined): Promise<firebase.auth.UserCredential>
-  signInWithPhoneNumber(phoneNumber: string, applicationVerifier: firebase.auth.ApplicationVerifier): Promise<firebase.auth.ConfirmationResult>
-  signInWithRedirect(provider: firebase.auth.AuthProvider): Promise<void>
-  signInWithPopup(provider: firebase.auth.AuthProvider): Promise<firebase.auth.UserCredential>
-  signOut(): Promise<void>
-  createUserWithEmailAndPassword(email: string, password: string): void
 }
 
 export function useAuth(instance: typeof firebase): UseAuth {
@@ -55,16 +44,5 @@ export function useAuth(instance: typeof firebase): UseAuth {
     authenticationStatus,
     isAuthenticated,
     user,
-
-    signInAnonymously() { return auth().signInAnonymously() },
-    signInWithCredential(credential: firebase.auth.AuthCredential) { return auth().signInWithCredential(credential) },
-    signInWithCustomToken(token: string) { return auth().signInWithCustomToken(token) },
-    signInWithEmailAndPassword(email: string, password: string) { return auth().signInWithEmailAndPassword(email, password) },
-    signInWithEmailLink(email: string, emailLink?: string | undefined) { return auth().signInWithEmailLink(email, emailLink) },
-    signInWithPhoneNumber(phoneNumber: string, applicationVerifier: firebase.auth.ApplicationVerifier) { return auth().signInWithPhoneNumber(phoneNumber, applicationVerifier) },
-    signInWithRedirect(provider: firebase.auth.AuthProvider) { return auth().signInWithRedirect(provider) },
-    signInWithPopup(provider: firebase.auth.AuthProvider) { return auth().signInWithPopup(provider) },
-    signOut() { return auth().signOut() },
-    createUserWithEmailAndPassword(email: string, password: string) { return auth().createUserWithEmailAndPassword(email, password) },
   }
 }
