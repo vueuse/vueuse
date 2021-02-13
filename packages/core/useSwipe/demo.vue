b6a7fdbee5621155db04f786b88fb267a29e8ebb
lstoeferle <48953604+lstoeferle@users.noreply.github.com>
2021-02-13 15:41:15 +0100

    chore: started with docs

---
+++ packages/core/useSwipe/demo.vue
@@ -0,0 +1,45 @@
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import { useSwipe } from '.'
+
+const el = ref(null)
+
+const { direction, isSwiping, lengthX, lengthY } = useSwipe(el)
+</script>
+
+<template>
+  <div>
+    <div ref="el" class="container" :class="{highlight: isSwiping}">
+      <p class="notice">
+        Swipe here
+      </p>
+    </div>
+    <p class="status">
+      Direction: {{ direction? direction : '-' }} <br>
+      lengthX: {{ lengthX }} | lengthY: {{ lengthY }}
+    </p>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  border: 2px dashed #ccc;
+  height: 100;
+}
+
+.container.highlight {
+  transition: border-color 0.2s linear;
+  border-color: #3fb983;
+}
+
+.notice {
+  text-align: center;
+  font-style: italic;
+  color: #ccc;
+}
+
+.status {
+  text-align: center;
+}
+</style>
---
+++ packages/core/useSwipe/index.md
@@ -0,0 +1,31 @@
+---
+category: Sensors
+---
+
+
+# useSwipe
+
+Reactive swipe detection based on [`TouchEvents`](https://developer.mozilla.org/en-US/docs/Web/API/TouchEvent).
+
+## Usage
+
+```html {16-20}
+<template>
+  <div ref="el">
+    Swipe here
+  </div>
+</template>
+
+<script>
+  setup() {
+    const el = ref(null)
+    const { direction } = useSwipe(el)
+
+    return { el, direction }
+  } 
+</script>
+```
+
+## Limitations
+
+TODO: add limitations
