d3a1c64d1d24caa10a7b7021e8a7344420e75d3e
Scott Bedard <scottbedard90@gmail.com>
2021-03-16 20:23:31 -0500

    scaffolding specs

---
+++ packages/core/useTransition/next.spec.ts
@@ -0,0 +1,17 @@
+import { useTransition, TransitionPresets } from './next'
+
+describe('useTransition', () => {
+  it.todo('transitions between numbers')
+
+  it.todo('transitions between vectors')
+
+  it.todo('supports cubic bezier curves')
+
+  it.todo('supports custom easing functions')
+
+  it.todo('supports dynamic transitions')
+
+  it.todo('supports dynamic durations')
+
+  it.todo('fires onStarted and onFinished callbacks')
+})
