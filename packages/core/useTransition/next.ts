6c360775cb13d41c3cbe1ebaf9c16a24c99966c9
Scott Bedard <scottbedard90@gmail.com>
2021-03-16 20:07:17 -0500

    improve IDE support of options object

--- packages/core/useTransition/next.ts
+++ packages/core/useTransition/next.ts
@@ -1,20 +1,56 @@
 import { computed, ComputedRef, ref, Ref, unref } from 'vue-demi'
 import { isNumber, MaybeRef } from '@vueuse/shared'
 
+/**
+ * Cubic bezier points
+ */
+type CubicBezierPoints = [number, number, number, number]
+
+/**
+ * Easing function
+ */
+type EasingFunction = (n: number) => number
+
+/**
+ * Transition options
+ */
+export type TransitionOptions = {
+  /**
+   * Transition duration in milliseconds
+   */
+  duration?: MaybeRef<number>
+
+  /**
+   * Callback to execute after transition finishes
+   */
+  onFinished?: () => void
+
+  /**
+   * Callback to execute after transition starts
+   */
+  onStarted?: () => void
+
+  /**
+   * Easing function or cubic bezier points for calculating transition values
+   */
+  transition?: MaybeRef<EasingFunction | CubicBezierPoints>
+}
+
 // option 1: reactive number
-export function useTransition(source: Ref<number>): ComputedRef<number>
+export function useTransition(source: Ref<number>, options?: TransitionOptions): ComputedRef<number>
 
 // option 2: static array of possibly reactive numbers
-export function useTransition<T extends MaybeRef<number>[]>(source: [...T]): ComputedRef<{ [K in keyof T]: number }>
+export function useTransition<T extends MaybeRef<number>[]>(source: [...T], options?: TransitionOptions): ComputedRef<{ [K in keyof T]: number }>
 
 // option 3: reactive array of numbers
-export function useTransition<T extends Ref<number[]>>(source: T): ComputedRef<number[]>
+export function useTransition<T extends Ref<number[]>>(source: T, options?: TransitionOptions): ComputedRef<number[]>
 
 /**
  * Transition between values.
  */
 export function useTransition(
   source: Ref<number> | MaybeRef<number>[] | Ref<number[]>,
+  options: TransitionOptions = {},
 ): ComputedRef<number | { [K in keyof typeof source]: number } | number[]> {
   // raw source value
   const sourceValue = computed(() => {
