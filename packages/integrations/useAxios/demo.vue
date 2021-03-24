f8f295fed0766d491a45bf60698c2e76639f88fa
Marcos Dantas <marcos.adantas@hotmail.com>
2021-03-24 20:02:39 -0300

    Update demo.vue

--- packages/integrations/useAxios/demo.vue
+++ packages/integrations/useAxios/demo.vue
@@ -2,13 +2,13 @@
 import { stringify } from '@vueuse/docs-utils'
 import { useAxios } from '.'
 
-const { data, finished } = useAxios(
+const { data, loading } = useAxios(
   'https://jsonplaceholder.typicode.com/todos/1',
 )
 const text = stringify(data)
 </script>
 
 <template>
-  <note>Finished: {{ finished.toString() }}</note>
+  <note>Finished: {{ loading.toString() }}</note>
   <pre lang="yaml">{{ text }}</pre>
 </template>
