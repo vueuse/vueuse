b39e8839a6044ffba9fc46844ce11005b0fa7149
Fabian <donskelle@googlemail.com>
2021-03-25 07:07:37 +0100

    refactor(useWebWorkerFn): Small doc and type improvements (#382)
    Co-authored-by: Anthony Fu <anthonyfu117@hotmail.com>

--- packages/core/useWebWorkerFn/index.md
+++ packages/core/useWebWorkerFn/index.md
@@ -26,7 +26,7 @@ import { useWebWorkerFn } from '@vueuse/core'
 const { workerFn, workerStatus, workerTerminate } = useWebWorkerFn(
   dates => dates.sort(dateFns.compareAsc), 
   {
-    timeout: 50000, // 5 seconds
+    timeout: 50000,
     dependencies: [
       'https://cdnjs.cloudflare.com/ajax/libs/date-fns/1.30.1/date_fns.js' // dateFns
     ],
@@ -37,31 +37,6 @@ const { workerFn, workerStatus, workerTerminate } = useWebWorkerFn(
 ## Web Worker
 
 Before you start using this function, we suggest you read the [Web Worker](https://developer.mozilla.org/en-US/docs/Web/API/Web_Workers_API/Using_web_workers) documentation.
-
-## API
-
-### Parameters
-
-| Name            | Type             | Description                                                |
-| --------------- | ---------------- | ---------------------------------------------------------- |
-| fn              | Function         | The `pure function` to run with web workers                |
-| options         | Object           | The object containing the options of the worker            |
-
-### Options
-
-| Name         | Type            | Default   | Description                                                               |
-| ------------ | --------------- | --------- | ------------------------------------------------------------------------- |
-| timeout      | Number          | undefined | the number of milliseconds before killing the worker                      |
-| dependencies | Array of String | []        | an array that contains the external dependencies needed to run the worker |
-
-
-### Return Value
-
-| Name            | Type             | Description                                                |
-| --------------- | ---------------- | ---------------------------------------------------------- |
-| workerFn        | Promise          | The `function` that allows you to run `fn` with web worker |
-| workerStatus    | `WebWorkerStatus`   | The status of `workerFn`                                   |
-| workerTerminate | Function         | The function that kills the worker                          |
 
 ## Credit
 
@@ -92,7 +67,7 @@ export interface WebWorkerOptions extends ConfigurableWindow {
  */
 export declare const useWebWorkerFn: <T extends (...fnArgs: any[]) => any>(
   fn: T,
-  { dependencies, timeout, window }?: WebWorkerOptions
+  { dependencies = [], timeout, window = defaultWindow }: Partial<WebWorkerOptions> = {},
 ) => {
   workerFn: (...fnArgs: Parameters<T>) => Promise<ReturnType<T>>
   workerStatus: Ref<WebWorkerStatus>
--- packages/core/useWebWorkerFn/index.ts
+++ packages/core/useWebWorkerFn/index.ts
@@ -1,14 +1,27 @@
 /* this implementation is a vue port of https://github.com/alewin/useWorker by Alessio Koci */
 
-import { ref, Ref } from 'vue-demi'
+import { ref } from 'vue-demi'
 import createWorkerBlobUrl from './lib/createWorkerBlobUrl'
 import { tryOnUnmounted } from '@vueuse/shared'
 import { ConfigurableWindow, defaultWindow } from '../_configurable'
 
-export type WebWorkerStatus = 'PENDING' | 'SUCCESS' | 'RUNNING' | 'ERROR' | 'TIMEOUT_EXPIRED'
+export type WebWorkerStatus =
+  | 'PENDING'
+  | 'SUCCESS'
+  | 'RUNNING'
+  | 'ERROR'
+  | 'TIMEOUT_EXPIRED'
 
 export interface WebWorkerOptions extends ConfigurableWindow {
+  /**
+   * Number of milliseconds before killing the worker
+   *
+   * @default undefined
+   */
   timeout?: number
+  /**
+   * An array that contains the external dependencies needed to run the worker
+   */
   dependencies?: string[]
 }
 
@@ -21,17 +34,18 @@ export interface WebWorkerOptions extends ConfigurableWindow {
  */
 export const useWebWorkerFn = <T extends (...fnArgs: any[]) => any>(
   fn: T,
-  {
+  options: WebWorkerOptions = {},
+) => {
+  const {
     dependencies = [],
     timeout,
     window = defaultWindow,
-  }: WebWorkerOptions = {},
-) => {
-  const worker: Ref<Worker & { _url?: string } | undefined> = ref(undefined)
+  } = options
 
+  const worker = ref<(Worker & { _url?: string }) | undefined>()
   const workerStatus = ref<WebWorkerStatus>('PENDING')
-  const promise: Ref<{ reject?: (result: ReturnType<T> | ErrorEvent) => void;resolve?: (result: ReturnType<T>) => void }> = ref({})
-  const timeoutId: Ref<number | undefined> = ref(undefined)
+  const promise = ref<({ reject?: (result: ReturnType<T> | ErrorEvent) => void;resolve?: (result: ReturnType<T>) => void })>({})
+  const timeoutId = ref<number>()
 
   const workerTerminate = (status: WebWorkerStatus = 'PENDING') => {
     if (worker.value && worker.value._url && window) {
@@ -46,9 +60,7 @@ export const useWebWorkerFn = <T extends (...fnArgs: any[]) => any>(
 
   workerTerminate()
 
-  tryOnUnmounted(() => {
-    workerTerminate()
-  })
+  tryOnUnmounted(workerTerminate)
 
   const generateWorker = () => {
     const blobUrl = createWorkerBlobUrl(fn, dependencies)
@@ -56,10 +68,7 @@ export const useWebWorkerFn = <T extends (...fnArgs: any[]) => any>(
     newWorker._url = blobUrl
 
     newWorker.onmessage = (e: MessageEvent) => {
-      const {
-        resolve = () => {},
-        reject = () => {},
-      } = promise.value
+      const { resolve = () => {}, reject = () => {} } = promise.value
       const [status, result] = e.data as [WebWorkerStatus, ReturnType<T>]
 
       switch (status) {
@@ -75,9 +84,7 @@ export const useWebWorkerFn = <T extends (...fnArgs: any[]) => any>(
     }
 
     newWorker.onerror = (e: ErrorEvent) => {
-      const {
-        reject = () => {},
-      } = promise.value
+      const { reject = () => {} } = promise.value
 
       reject(e)
       workerTerminate('ERROR')
@@ -92,20 +99,23 @@ export const useWebWorkerFn = <T extends (...fnArgs: any[]) => any>(
     return newWorker
   }
 
-  const callWorker = (...fnArgs: Parameters<T>) => new Promise<ReturnType<T>>((resolve, reject) => {
-    promise.value = {
-      resolve,
-      reject,
-    }
-    worker.value && worker.value.postMessage([[...fnArgs]])
+  const callWorker = (...fnArgs: Parameters<T>) =>
+    new Promise<ReturnType<T>>((resolve, reject) => {
+      promise.value = {
+        resolve,
+        reject,
+      }
+      worker.value && worker.value.postMessage([[...fnArgs]])
 
-    workerStatus.value = 'RUNNING'
-  })
+      workerStatus.value = 'RUNNING'
+    })
 
   const workerFn = (...fnArgs: Parameters<T>) => {
     if (workerStatus.value === 'RUNNING') {
       /* eslint-disable-next-line no-console */
-      console.error('[useWebWorkerFn] You can only run one instance of the worker at a time.')
+      console.error(
+        '[useWebWorkerFn] You can only run one instance of the worker at a time.',
+      )
       /* eslint-disable-next-line prefer-promise-reject-errors */
       return Promise.reject()
     }
