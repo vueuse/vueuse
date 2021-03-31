f25ba5236cb1a30069af45548a7761bed2c2a60c
Jacob Clevenger <jacobrclevenger@gmail.com>
2021-03-31 00:25:48 -0400

    fix(useBreakpoints): useBreakpoints samller should < rather than <=. Closes #404

--- packages/core/useBreakpoints/index.ts
+++ packages/core/useBreakpoints/index.ts
@@ -16,8 +16,12 @@ export const breakpointsTailwind = {
  * @param options
  */
 export function useBreakpoints<K extends string>(breakpoints: Record<K, number | string>, options: ConfigurableWindow = {}) {
-  function getValue(k: K) {
+  function getValue(k: K, modifier?: (v: number) => string | number) {
     let v = breakpoints[k]
+
+    if (modifier)
+      v = modifier(parseInt(v.toString()))
+
     if (typeof v === 'number')
       v = `${v}px`
     return v
@@ -35,7 +39,7 @@ export function useBreakpoints<K extends string>(breakpoints: Record<K, number |
       return useMediaQuery(`(min-width: ${getValue(k)})`, options)
     },
     smaller(k: K) {
-      return useMediaQuery(`(max-width: ${getValue(k)})`, options)
+      return useMediaQuery(`(max-width: ${getValue(k, v => v - 1)})`, options)
     },
     between(a: K, b: K) {
       return useMediaQuery(`(min-width: ${getValue(a)}) and (max-width: ${getValue(b)})`, options)
