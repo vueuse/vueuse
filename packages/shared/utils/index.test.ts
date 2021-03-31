30f2c1e84ea9e94feb18a7b847114a5ab704f03d
Anthony Fu <anthonyfu117@hotmail.com>
2021-03-31 12:43:14 +0800

    chore: update

---
+++ packages/shared/utils/index.test.ts
@@ -0,0 +1,14 @@
+import { increaseWithUnit } from '.'
+
+describe('utils', () => {
+  it('increaseWithUnit', () => {
+    expect(increaseWithUnit(100, 1)).toEqual(101)
+    expect(increaseWithUnit('1px', 1)).toEqual('2px')
+    expect(increaseWithUnit('-1em', 1)).toEqual('0em')
+    expect(increaseWithUnit('1em', -1)).toEqual('0em')
+    expect(increaseWithUnit('1em', -5)).toEqual('-4em')
+    expect(increaseWithUnit('0.5vw', 1.5)).toEqual('2vw')
+    expect(increaseWithUnit('100 %', 10)).toEqual('110 %')
+    expect(increaseWithUnit('var(--cool)', -5)).toEqual('var(--cool)')
+  })
+})
--- packages/shared/utils/index.ts
+++ packages/shared/utils/index.ts
@@ -22,3 +22,23 @@ export function invoke<T>(fn: () => T): T {
 export function containsProp(obj: object, ...props: string[]) {
   return props.some(k => k in obj)
 }
+
+/**
+ * Increase string a value with unit
+ *
+ * @example '2px' + 1 = '3px'
+ * @example '15em' + (-2) = '13em'
+ */
+export function increaseWithUnit(target: number, delta: number): number
+export function increaseWithUnit(target: string, delta: number): string
+export function increaseWithUnit(target: string | number, delta: number): string | number
+export function increaseWithUnit(target: string | number, delta: number): string | number {
+  if (typeof target === 'number')
+    return target + delta
+  const value = target.match(/^-?[0-9]+\.?[0-9]*/)?.[0] || ''
+  const unit = target.slice(value.length)
+  const result = (parseFloat(value) + delta)
+  if (Number.isNaN(result))
+    return target
+  return result + unit
+}
