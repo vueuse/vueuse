780d667074c415a6f1958f8cb7f61178ff1e5103
Anthony Fu <anthonyfu117@hotmail.com>
2021-04-05 00:35:12 +0800

    chore: make keys more visuable

--- packages/core/useMagicKeys/Key.vue
+++ packages/core/useMagicKeys/Key.vue
@@ -9,7 +9,10 @@ defineProps<{
 <template>
   <div
     class="font-mono px-4 py-2 rounded "
-    :class="value ? 'opacity-100 text-primary bg-primary bg-opacity-10' : 'opacity-50 bg-gray-400 bg-opacity-5'"
+    :class="value
+      ? 'opacity-100 text-primary bg-primary bg-opacity-15'
+      : 'opacity-50 bg-gray-600 bg-opacity-10 dark:(bg-gray-400 bg-opacity-10)'
+    "
   >
     <slot />
   </div>
