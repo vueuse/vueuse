d88f3f90bd746e2216b1910c8309c8c40f0ef645
Anthony Fu <anthonyfu117@hotmail.com>
2021-04-05 00:03:06 +0800

    feat(useMaigcKeys): new function

---
+++ packages/core/useMagicKeys/Key.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { defineProps } from 'vue'
+
+defineProps<{
+  value: boolean
+}>()
+</script>
+
+<template>
+  <div
+    class="font-mono px-4 py-2 rounded "
+    :class="value ? 'opacity-100 text-primary bg-primary bg-opacity-10' : 'opacity-50 bg-gray-400 bg-opacity-5'"
+  >
+    <slot />
+  </div>
+</template>
---
+++ packages/core/useMagicKeys/demo.vue
@@ -0,0 +1,61 @@
+<script setup lang="ts">
+import Key from './Key.vue'
+import { useMagicKeys } from '.'
+
+const { shift, v, u, e, s, v_u_e, u_s_e } = useMagicKeys()
+</script>
+
+<template>
+  <div class="flex">
+    <img
+      src="/vue.svg"
+      class="h-20 m-auto transform transistion duration-500"
+      :class="{'opacity-0': !v_u_e, 'rotate-180': shift }"
+    />
+
+    <div>
+      <note class="text-center mt-0 mb-5">
+        Press the following keys to test out
+      </note>
+      <div class="flex gap-3 justify-center">
+        <Key :value="v">
+          V
+        </Key>
+        <Key :value="u">
+          u
+        </Key>
+        <Key :value="e">
+          e
+        </Key>
+        <div class="mx-1"></div>
+        <Key :value="u">
+          U
+        </Key>
+        <Key :value="s">
+          s
+        </Key>
+        <Key :value="e">
+          e
+        </Key>
+      </div>
+
+      <div class="flex gap-3 justify-center mt-3">
+        <Key :value="shift">
+          Shift
+        </Key>
+        <Key :value="v_u_e">
+          Vue
+        </Key>
+        <Key :value="u_s_e">
+          Use
+        </Key>
+      </div>
+    </div>
+
+    <img
+      src="/favicon.svg"
+      class="h-20 m-auto transform transistion duration-500"
+      :class="{'opacity-0': !u_s_e, 'rotate-180': shift }"
+    />
+  </div>
+</template>
