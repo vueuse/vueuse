3949584a100e4b44c3d944363b15aab7771e264e
Jacob Clevenger <jacobrclevenger@gmail.com>
2021-05-02 04:15:16 -0400

    feat useMouseInElement component

---
+++ packages/core/useMouseInElement/component.ts
@@ -0,0 +1,38 @@
+import { h, ref, defineComponent } from 'vue-demi'
+import { useMouseInElement, MouseInElementOptions } from '.'
+
+export const UseMouse = defineComponent<{ options: MouseInElementOptions }>({
+  name: 'UseMouse',
+  setup(props, { slots, emit }) {
+    const target = ref()
+    const {
+      x,
+      y,
+      elementX,
+      elementY,
+      elementPositionX,
+      elementPositionY,
+      elementWidth,
+      elementHeight,
+      isOutside,
+      sourceType,
+    } = useMouseInElement(target, props.options)
+
+    return () => {
+      if (slots.default) {
+        return h('div', { ref: target }, slots.default({
+          x: x.value,
+          y: y.value,
+          elementX: elementX.value,
+          elementY: elementY.value,
+          elementPositionX: elementPositionX.value,
+          elementPositionY: elementPositionY.value,
+          elementWidth: elementWidth.value,
+          elementHeight: elementHeight.value,
+          isOutside: isOutside.value,
+          sourceType: sourceType.value,
+        }))
+      }
+    }
+  },
+})
