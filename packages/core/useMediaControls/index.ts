f0d7667c530b8ee2713f698db1b135289d35509f
Alex Kozack <cawa-93@users.noreply.github.com>
2021-05-11 17:54:19 +0000

    feat: Export useMediaControls source types

--- packages/core/useMediaControls/index.ts
+++ packages/core/useMediaControls/index.ts
@@ -8,7 +8,7 @@ import { ConfigurableDocument, defaultDocument } from '../_configurable'
  * documentation from MDN(https://developer.mozilla.org/en-US/docs/Web/API/HTMLMediaElement)
  */
 
-interface UseMediaSource {
+export interface UseMediaSource {
   /**
    * The source url for the media
    */
@@ -20,7 +20,7 @@ interface UseMediaSource {
   type?: string
 }
 
-interface UseMediaTextTrackSource {
+export interface UseMediaTextTrackSource {
   /**
    * Indicates that the track should be enabled unless the user's preferences indicate
    * that another track is more appropriate
