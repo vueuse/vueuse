1d5087a0a9b2311a7743eb2b5319dfae064e666e
Jacob Clevenger <jacobrclevenger@gmail.com>
2021-05-23 21:34:36 -0400

    feat(createEventHook): added interface

--- packages/shared/createEventHook/index.ts
+++ packages/shared/createEventHook/index.ts
@@ -1,14 +1,20 @@
 /**
- * The source code for this function was inspiried by vue-apollo's `useEventHook` util
+ * The source code for this function was inspired by vue-apollo's `useEventHook` util
  * https://github.com/vuejs/vue-apollo/blob/v4/packages/vue-apollo-composable/src/util/useEventHook.ts
  */
+
+export interface EventHook<T = any> {
+  on: (fn: (param: T) => void) => void
+  off: (fn: (param: T) => void) => void
+  trigger: (param: T) => void
+}
 
 /**
  * Utility for creating event hooks
  *
  * @see https://vueuse.org/createEventHook
  */
-export function createEventHook<T = any>() {
+export function createEventHook<T = any>(): EventHook<T> {
   const fns: Array<(param: T) => void> = []
 
   const off = (fn: (param: T) => void) => {
