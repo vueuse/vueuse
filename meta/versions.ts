ff3af15edd09115edfd3394c9801410d527fb59c
Anthony Fu <anthonyfu117@hotmail.com>
2021-06-06 15:48:07 +0800

    chore: version order

--- meta/versions.ts
+++ meta/versions.ts
@@ -4,6 +4,6 @@ export const currentVersion = version
 
 export const versions = [
   { version },
-  { version: '4.0', link: 'https://v4-0-6.vueuse.org/' },
   { version: '4.11', link: 'https://v4-11-2.vueuse.org/' },
+  { version: '4.0', link: 'https://v4-0-6.vueuse.org/' },
 ]
