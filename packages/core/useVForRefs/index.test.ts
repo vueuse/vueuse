8cb9ed736243464b05ae39929bf1e8415b5a1f47
Ernest <iendeavor.github.io@gmail.com>
2021-06-28 10:47:17 +0800

    test: add more test

--- packages/core/useVForRefs/index.test.ts
+++ packages/core/useVForRefs/index.test.ts
@@ -2,45 +2,140 @@ import { defineComponent, nextTick, onMounted, ref } from 'vue'
 import { mount } from '../../.test'
 import { useVForRefs } from '.'
 
+const keyedVFor = `
+<div v-for="item in list" :key="item.id" :ref="setRef">
+  {{ item.value }}
+</div>
+`
+
+const keyedTemplateVFor = `
+<template v-for="item in list" :key="item.id">
+  <div :ref="setRef">
+    {{ item.value }}
+  </div>
+</template>
+`
+
 describe('useCounter', () => {
   it('should be defined', () => {
     expect(useVForRefs).toBeDefined()
   })
 
-  it('should bind refs on mounted', async() => {
-    const vm = mount(defineComponent({
-      setup() {
-        const list = [{ id: 0, value: 'foo' }, { id: 1, value: 'bar' }]
-        const { refs, setRef } = useVForRefs()
+  describe('keyed v-for', () => {
+    it('should bind refs on mounted', async() => {
+      const vm = mount(defineComponent({
+        setup() {
+          const list = [{ id: 0, value: 'foo' }, { id: 1, value: 'bar' }]
+          const { refs, setRef } = useVForRefs()
 
-        return { list, refs, setRef }
-      },
-      template: '<div><template v-for="item in list" :key="item.id"><div :ref="setRef">{{item.value}}</div></template></div>',
-    }))
+          return { list, refs, setRef }
+        },
+        template: keyedVFor,
+      }))
 
-    expect(vm.refs[0].innerHTML).toBe('foo')
-    expect(vm.refs[1].innerHTML).toBe('bar')
+      expect(vm.refs[0].innerHTML).toBe('foo')
+      expect(vm.refs[1].innerHTML).toBe('bar')
+    })
+
+    it('should bind refs on updated', async() => {
+      const vm = mount(defineComponent({
+        setup() {
+          const list = ref([{ id: 0, value: 'foo' }, { id: 1, value: 'bar' }])
+          const { refs, setRef } = useVForRefs()
+
+          onMounted(() => {
+            list.value.push({ id: 2, value: 'baz' }, { id: 3, value: 'qux' })
+          })
+
+          return { list, refs, setRef }
+        },
+        template: keyedVFor,
+      }))
+
+      await nextTick()
+      expect(vm.refs[0].innerHTML).toBe('foo')
+      expect(vm.refs[1].innerHTML).toBe('bar')
+      expect(vm.refs[2].innerHTML).toBe('baz')
+      expect(vm.refs[3].innerHTML).toBe('qux')
+    })
+
+    it('should update order of refs on updated', async() => {
+      const vm = mount(defineComponent({
+        setup() {
+          const list = ref([{ id: 0, value: 'foo' }, { id: 1, value: 'bar' }])
+          const { refs, setRef } = useVForRefs()
+
+          onMounted(() => {
+            [list.value[0], list.value[1]] = [list.value[1], list.value[0]]
+          })
+
+          return { list, refs, setRef }
+        },
+        template: keyedVFor,
+      }))
+
+      await nextTick()
+      expect(vm.refs[0].innerHTML).toBe('bar')
+      expect(vm.refs[1].innerHTML).toBe('foo')
+    })
   })
 
-  it('should bind refs on updated', async() => {
-    const vm = mount(defineComponent({
-      setup() {
-        const list = ref([{ id: 0, value: 'foo' }, { id: 1, value: 'bar' }])
-        const { refs, setRef } = useVForRefs()
+  describe('keyed template v-for', () => {
+    it('should bind refs on mounted', async() => {
+      const vm = mount(defineComponent({
+        setup() {
+          const list = [{ id: 0, value: 'foo' }, { id: 1, value: 'bar' }]
+          const { refs, setRef } = useVForRefs()
 
-        onMounted(() => {
-          list.value.push({ id: 2, value: 'baz' }, { id: 3, value: 'qux' })
-        })
+          return { list, refs, setRef }
+        },
+        template: keyedTemplateVFor,
+      }))
 
-        return { list, refs, setRef }
-      },
-      template: '<div><template v-for="item in list" :key="item.id"><div :ref="setRef">{{item.value}}</div></template></div>',
-    }))
+      expect(vm.refs[0].innerHTML).toBe('foo')
+      expect(vm.refs[1].innerHTML).toBe('bar')
+    })
 
-    await nextTick()
-    expect(vm.refs[0].innerHTML).toBe('foo')
-    expect(vm.refs[1].innerHTML).toBe('bar')
-    expect(vm.refs[2].innerHTML).toBe('baz')
-    expect(vm.refs[3].innerHTML).toBe('qux')
+    it('should bind refs on updated', async() => {
+      const vm = mount(defineComponent({
+        setup() {
+          const list = ref([{ id: 0, value: 'foo' }, { id: 1, value: 'bar' }])
+          const { refs, setRef } = useVForRefs()
+
+          onMounted(() => {
+            list.value.push({ id: 2, value: 'baz' }, { id: 3, value: 'qux' })
+          })
+
+          return { list, refs, setRef }
+        },
+        template: keyedTemplateVFor,
+      }))
+
+      await nextTick()
+      expect(vm.refs[0].innerHTML).toBe('foo')
+      expect(vm.refs[1].innerHTML).toBe('bar')
+      expect(vm.refs[2].innerHTML).toBe('baz')
+      expect(vm.refs[3].innerHTML).toBe('qux')
+    })
+
+    it('should update order of refs on updated', async() => {
+      const vm = mount(defineComponent({
+        setup() {
+          const list = ref([{ id: 0, value: 'foo' }, { id: 1, value: 'bar' }])
+          const { refs, setRef } = useVForRefs()
+
+          onMounted(() => {
+            [list.value[0], list.value[1]] = [list.value[1], list.value[0]]
+          })
+
+          return { list, refs, setRef }
+        },
+        template: keyedTemplateVFor,
+      }))
+
+      await nextTick()
+      expect(vm.refs[0].innerHTML).toBe('bar')
+      expect(vm.refs[1].innerHTML).toBe('foo')
+    })
   })
 })
