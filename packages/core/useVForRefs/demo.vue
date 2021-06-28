34515a14f238581887a51f03890f6cbb4ff84c72
Ernest <iendeavor.github.io@gmail.com>
2021-06-28 10:58:49 +0800

    docs: update demo

--- packages/core/useVForRefs/demo.vue
+++ packages/core/useVForRefs/demo.vue
@@ -1,26 +1,69 @@
 
 <template>
-  <template v-for="item in list" :key="item.id">
+  <button @click="push">
+    Push
+  </button>
+  <button @click="pop">
+    Pop
+  </button>
+  <button @click="shift">
+    Shift
+  </button>
+  <button @click="unshift">
+    Unshift
+  </button>
+  <button @click="shuffle">
+    Shuffle
+  </button>
+  <br>
+
+  <note>Open console to see <code>refs</code></note>
+  <br>
+  <template v-for="item in list" :key="item.timestamp">
     <div :ref="setRef">
       {{ item.value }}
     </div>
   </template>
 </template>
 
-<script lang="ts">
-import { onMounted } from 'vue-demi'
+<script setup lang="ts">
+import { onMounted, reactive, watch, nextTick, toRaw } from 'vue-demi'
 import { useVForRefs } from '@vueuse/core'
 
-export default {
-  setup() {
-    const list = [{ id: 0, value: 'foo' }, { id: 1, value: 'bar' }]
-    const { refs, setRef } = useVForRefs()
+const list = reactive<ReturnType<typeof generateItem>[]>([])
+const { refs, setRef } = useVForRefs()
 
-    onMounted(() => {
-      console.log(refs) // the <div> elements
-    })
-
-    return { list, setRef }
-  },
+function generateItem() {
+  return {
+    timestamp: Date.now(),
+    value: Math.floor(Math.random() * 100),
+  }
 }
+const push = () => list.push(generateItem())
+const pop = () => list.pop()
+const shift = () => list.shift()
+const unshift = () => list.unshift(generateItem())
+const shuffle = () => {
+  const keys = [...list.keys()]
+  for (const i of keys) {
+    const j = Math.floor(Math.random() * keys.length)
+;[list[i], list[j]] = [list[j], list[i]]
+  }
+}
+
+setTimeout(push, 1)
+setTimeout(push, 1)
+setTimeout(push, 1)
+onMounted(() => {
+  console.log(toRaw(refs)) // the <div> elements
+})
+watch(
+  () => list,
+  () => {
+    nextTick(() => {
+      console.log(toRaw(refs))
+    })
+  },
+  { deep: true },
+)
 </script>
