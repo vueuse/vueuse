380edb9fc012d2ca7dba6abb75bd5fb023bc23c9
Jeffrey Li <46302202+jeffrey-dot-li@users.noreply.github.com>
2021-07-04 21:17:46 -0400

    fix(usePointerSwipe): use `style.setProperty` instead of `setAttribute` (#583)
    * fix : replace `setAttribute` with `style.setProperty` in usePointerSwipe
    
    * nullable chaining and single quotes
    
    * Delete package-lock.json
    
    Co-authored-by: Anthony Fu <anthonyfu117@hotmail.com>

--- packages/core/usePointerSwipe/index.ts
+++ packages/core/usePointerSwipe/index.ts
@@ -48,7 +48,7 @@ export interface PointerSwipeReturn {
  * @param options
  */
 export function usePointerSwipe(
-  target: MaybeRef<Element | null | undefined>,
+  target: MaybeRef<HTMLElement | null | undefined>,
   options: PointerSwipeOptions = {},
 ): PointerSwipeReturn {
   const targetRef = ref(target)
@@ -99,7 +99,7 @@ export function usePointerSwipe(
     useEventListener(target, 'pointerdown', (e: PointerEvent) => {
       isPointerDown.value = true
       // Disable scroll on for TouchEvents
-      targetRef.value?.setAttribute('style', 'touch-action: none')
+      targetRef.value?.style?.setProperty('touch-action', 'none')
       // Future pointer events will be retargeted to target until pointerup/cancel
       targetRef.value?.setPointerCapture(e.pointerId)
 
@@ -127,7 +127,7 @@ export function usePointerSwipe(
 
       isPointerDown.value = false
       isSwiping.value = false
-      targetRef.value?.setAttribute('style', 'touch-action: initial')
+      targetRef.value?.style?.setProperty('touch-action', 'initial')
     }),
   ]
 
