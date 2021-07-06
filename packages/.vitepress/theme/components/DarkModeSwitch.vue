85536236b3218428742944594837188f6cfad626
Maxim Vishnevsky <mxmvshnvsk@gmail.com>
2021-07-06 12:19:22 +0400

    Renamed toogle

--- packages/.vitepress/theme/components/DarkModeSwitch.vue
+++ packages/.vitepress/theme/components/DarkModeSwitch.vue
@@ -1,5 +1,5 @@
 <template>
-  <button aria-label="Toggle Theme" class="icon-button" @click="toggle()">
+  <button aria-label="Toggle Theme" class="icon-button" @click="toggleDark()">
     <ri-moon-line v-show="isDark" />
     <ri-sun-line v-show="!isDark" />
   </button>
@@ -9,5 +9,5 @@
 import { useToggle } from '@vueuse/core'
 import { isDark } from '../composables/dark'
 
-const toggle = useToggle(isDark)
+const toggleDark = useToggle(isDark)
 </script>
