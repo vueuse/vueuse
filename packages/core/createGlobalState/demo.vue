b12bd056d08ae7d22a749fe5fe2bf7ab40511018
Anthony Fu <anthonyfu117@hotmail.com>
2021-07-08 20:56:43 +0800

    refactor(createGlobalState): rewrite with effectScope

--- packages/core/createGlobalState/demo.vue
+++ packages/core/createGlobalState/demo.vue
@@ -1,16 +1,15 @@
 <script setup lang="ts">
 import { stringify } from '@vueuse/docs-utils'
-import { reactive } from 'vue-demi'
 import { useStorage } from '../useStorage'
 import { createGlobalState } from '.'
 
-const useState = createGlobalState(() => {
-  return useStorage('vue-use-locale-storage', {
+const useState = createGlobalState(() =>
+  useStorage('vue-use-locale-storage', {
     name: 'Banana',
     color: 'Yellow',
     size: 'Medium',
-  })
-})
+  }),
+)
 
 const state = useState()
 const text = stringify(state)
