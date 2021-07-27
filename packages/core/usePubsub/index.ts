8fc4202e005bca02a1b35ea254ffa1d2e9a74776
TuiMao233 <wwu710632@gmail.com>
2021-07-27 23:37:14 +0800

    feat(usePubsub): Organize the code.

--- packages/core/usePubsub/index.ts
+++ packages/core/usePubsub/index.ts
@@ -22,28 +22,29 @@ export interface UsePubsubReturn<Message> {
 usePubsub.observes = {} as UsePubsubObserves
 usePubsub.watcher = {
   subscribe: <T>(event: string, execute: (message: T) => void) => {
-    const id = Symbol('observeId')
-    if (typeof usePubsub.observes[event] === 'undefined')
-      usePubsub.observes[event] = new Map([[id, execute]])
-    else
+    const id = Symbol('observe__uid')
+    if (usePubsub.observes[event])
       usePubsub.observes[event].set(id, execute)
+    else
+      usePubsub.observes[event] = new Map([[id, execute]])
     return id
   },
   publish: <T>(event: string, message?: T) => {
     if (!usePubsub.observes[event])
       return
-    [...usePubsub.observes[event]?.values()]
-      .forEach(execute => execute(message))
+    usePubsub.observes[event].forEach(execute => execute(message))
   },
   unsubscribe: (token: string) => {
     if (usePubsub.observes[token]) {
       delete usePubsub.observes[token]
       return
     }
-    Object.keys(usePubsub.observes).forEach((key) => {
-      usePubsub.observes[key].has(token) && usePubsub.observes[key].delete(token)
-      !usePubsub.observes[key].size && delete usePubsub.observes[key]
-    })
+    for (const [key, value] of Object.entries(usePubsub.observes)) {
+      if (value.has(token))
+        value.delete(token)
+      if (!value.size)
+        delete usePubsub.observes[key]
+    }
   },
 }
 
@@ -65,14 +66,13 @@ export function usePubsub<Message = any>(event: string | symbol): UsePubsubRetur
   function publish(message?: Message) {
     return usePubsub.watcher.publish(<string>event, message)
   }
-
   function unsubscribe(token?: string | symbol) {
     if (typeof token === 'undefined') {
-      [...unsubTokens].forEach(token => unsubscribe(token))
+      unsubTokens.forEach(token => unsubscribe(token))
       return
     }
-    if (typeof token === 'symbol')
-      unsubTokens.has(token) && unsubTokens.delete(token)
+    if (typeof token === 'symbol' && unsubTokens.has(token))
+      unsubTokens.delete(token)
     usePubsub.watcher.unsubscribe(<string>(token || event))
   }
 
