9dfd24dc8d87e1effe16e1aae48329ac005a8fbf
TuiMao233 <49724027+TuiMao233@users.noreply.github.com>
2021-08-09 22:44:35 +0800

    feat(useEventBus): new function (#647)
    Co-authored-by: Anthony Fu <anthonyfu117@hotmail.com>

---
+++ packages/core/useEventBus/index.test.ts
@@ -0,0 +1,94 @@
+import { nextTick } from 'vue-demi'
+import { useCounter } from '../../shared/useCounter'
+import { useSetup } from '../../.test'
+import { events } from './internal'
+import { useEventBus } from '.'
+
+describe('useEventBus', () => {
+  const emptyMap = new Map()
+  it('on event and off listener', () => {
+    const { on, off } = useEventBus<number>('foo')
+    const { inc } = useCounter(0)
+    on(inc)
+    off(inc)
+    expect(events).toEqual(emptyMap)
+  })
+  it('on event', () => {
+    let event = false
+    const { emit, on, reset } = useEventBus<boolean>('on-event')
+    on((_event) => {
+      event = _event
+    })
+    emit(true)
+    expect(event).toBe(true)
+    reset()
+    expect(events).toEqual(emptyMap)
+  })
+  it('on callback off event', () => {
+    const bus = useEventBus<number>('on-callback-off')
+    const { count, inc } = useCounter(0)
+    const off = bus.on(inc)
+    bus.on(inc)
+    off()
+    bus.emit()
+    bus.reset()
+    expect(count.value).toBe(1)
+    expect(events).toEqual(emptyMap)
+  })
+  it('useEventBus off event', () => {
+    const { emit, on, reset } = useEventBus<number>('useEventBus-off')
+    const { count, inc } = useCounter(0)
+    on(inc)
+    on(inc)
+    on(inc)
+
+    emit()
+    reset()
+
+    on(inc)
+    on(inc)
+
+    emit()
+    reset()
+
+    expect(count.value).toBe(5)
+    expect(events).toEqual(emptyMap)
+  })
+  it('event off event', () => {
+    const event1 = useEventBus<number>('event-off-1')
+    const event2 = useEventBus<number>('event-off-2')
+    const { count, inc } = useCounter(0)
+    event1.on(inc)
+    event2.on(inc)
+    event1.emit() // 1
+    event2.emit() // 2
+
+    event1.reset()
+
+    event1.on(inc)
+    event2.on(inc)
+    event1.emit() // 3
+    event2.emit() // 5
+
+    event1.reset()
+    event2.reset()
+
+    expect(count.value).toBe(5)
+    expect(events).toEqual(emptyMap)
+  })
+
+  it('setup unmount off', async() => {
+    const vm = useSetup(() => {
+      const { on } = useEventBus('setup-unmount')
+      on(() => {})
+      on(() => {})
+      on(() => {})
+      on(() => {})
+      on(() => {})
+    })
+    expect(events).not.toEqual(emptyMap)
+    vm.unmount()
+    await nextTick()
+    expect(events).toEqual(emptyMap)
+  })
+})
---
+++ packages/core/useEventBus/index.ts
@@ -0,0 +1,69 @@
+import { Fn } from '@vueuse/shared'
+import { getCurrentScope } from 'vue-demi'
+import { events } from './internal'
+
+export type EventBusListener<T = unknown> = (event: T) => void
+export type EventBusEvents<T> = EventBusListener<T>[]
+
+export interface EventBusKey<T> extends Symbol { }
+export type EventBusIdentifer<T = unknown> = EventBusKey<T> | string | number
+
+export interface UseEventBusReturn<T> {
+  /**
+   * Subscribe to an event. When calling emit, the listeners will execute.
+   * @param listener watch listener.
+   * @returns a stop function to remove the current callback.
+   */
+  on: (listener: EventBusListener<T>) => Fn
+  /**
+   * Emit an event, the corresponding event listeners will execute.
+   * @param event data sent.
+   */
+  emit: (event?: T) => void
+  /**
+   * Remove the corresponding listener.
+   * @param listener watch listener.
+   */
+  off: (listener: EventBusListener<T>) => void
+  /**
+   * Clear all events
+   */
+  reset: () => void
+}
+
+export function useEventBus<T = unknown>(key: EventBusIdentifer<T>): UseEventBusReturn<T> {
+  const scope = getCurrentScope()
+
+  function on(listener: EventBusListener<T>) {
+    const listeners = events.get(key) || []
+    listeners.push(listener)
+    events.set(key, listeners)
+
+    const _off = () => off(listener)
+    // auto unsubscribe when scope get disposed
+    scope?.cleanups.push(_off)
+    return _off
+  }
+
+  function off(listener: EventBusListener<T>): void {
+    const listeners = events.get(key)
+    if (!listeners)
+      return
+
+    const index = listeners.indexOf(listener)
+    if (index > -1)
+      listeners.splice(index, 1)
+    if (!listeners.length)
+      events.delete(key)
+  }
+
+  function reset() {
+    events.delete(key)
+  }
+
+  function emit(event?: T) {
+    events.get(key)?.forEach(v => v(event))
+  }
+
+  return { on, off, emit, reset }
+}
