712b83fb9d99abc2ac1b4cdf5e88b3f1679d42d8
Anthony Fu <anthonyfu117@hotmail.com>
2021-08-16 06:58:13 +0800

    chore: add 5.3 version in docs

--- meta/versions.ts
+++ meta/versions.ts
@@ -4,6 +4,7 @@ export const currentVersion = version
 
 export const versions = [
   { version },
+  { version: '5.3', link: 'https://v5-3-0.vueuse.org/' },
   { version: '4.11', link: 'https://v4-11-2.vueuse.org/' },
   { version: '4.0', link: 'https://v4-0-6.vueuse.org/' },
 ]
