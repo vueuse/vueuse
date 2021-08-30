95c4f85b1af9aed5252e53d5f5958bacad90df57
JMaylor <60825814+JMaylor@users.noreply.github.com>
2021-08-30 11:40:59 +0100

    add Modifier Type

--- packages/core/useKeyModifierState/index.ts
+++ packages/core/useKeyModifierState/index.ts
@@ -1,7 +1,9 @@
 import { ref } from 'vue-demi'
 import { useEventListener } from '../useEventListener'
 
-export function useKeyModifierState(modifier: string) {
+export type Modifier = 'Alt' | 'AltGraph' | 'CapsLock' | 'Control' | 'Fn' | 'FnLock' | 'Meta' | 'NumLock' | 'ScrollLock' | 'Shift' | 'Symbol' | 'SymbolLock'
+
+export function useKeyModifierState(modifier: Modifier) {
   const state = ref<null | boolean>(null)
   const listenerEvents = ['keydown', 'keyup']
   listenerEvents.forEach((listenerEvent) => {
