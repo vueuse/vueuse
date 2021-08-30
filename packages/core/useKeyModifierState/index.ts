16beb6711a6b3624beec2b0f91219529b6d7210a
JMaylor <60825814+JMaylor@users.noreply.github.com>
2021-08-30 17:18:53 +0100

    set default events and make configurable

--- packages/core/useKeyModifierState/index.ts
+++ packages/core/useKeyModifierState/index.ts
@@ -1,13 +1,25 @@
 import { ref } from 'vue-demi'
-import { useEventListener } from '../useEventListener'
+import { useEventListener, WindowEventName } from '../useEventListener'
 
 export type Modifier = 'Alt' | 'AltGraph' | 'CapsLock' | 'Control' | 'Fn' | 'FnLock' | 'Meta' | 'NumLock' | 'ScrollLock' | 'Shift' | 'Symbol' | 'SymbolLock'
 
-const listenerEvents = ['keydown', 'keyup']
+const defaultEvents: WindowEventName[] = ['mousedown', 'mouseup', 'keydown', 'keyup']
 
-export function useKeyModifierState(modifier: Modifier) {
+export interface ModifierOptions {
+  /**
+   * Event names that will prompt update to modifier states
+   *
+   * @default ['mousedown', 'mouseup', 'keydown', 'keyup']
+   */
+  events?: WindowEventName[]
+}
+
+export function useKeyModifierState(modifier: Modifier, options: ModifierOptions = {}) {
+  const {
+    events = defaultEvents,
+  } = options
   const state = ref<null | boolean>(null)
-  listenerEvents.forEach((listenerEvent) => {
+  events.forEach((listenerEvent) => {
     useEventListener(document, listenerEvent, (evt: KeyboardEvent) => {
       state.value = evt.getModifierState(modifier)
     })
