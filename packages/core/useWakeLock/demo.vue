fd0837153f0a45a1c284c2bd2471f40085a67e58
koheing <koheing.0111@gmail.com>
2021-09-03 05:30:16 +0900

    feat(useWakeLock): new function (#687)

---
+++ packages/core/useWakeLock/demo.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { reactive, computed } from 'vue-demi'
+import { useWakeLock } from '.'
+
+const wakeLock = reactive(useWakeLock())
+const text = computed(() => wakeLock.isActive ? 'OFF' : 'ON')
+const onClick = () => wakeLock.isActive ? wakeLock.request('screen') : wakeLock.release()
+</script>
+
+<template>
+  <button @click="onClick">
+    {{ text }}
+  </button>
+</template>
