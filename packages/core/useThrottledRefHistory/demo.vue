99f20f45f1054fb3a3061c04af61a11ff8040384
harmyderoman <romangarmider@gmail.com>
2021-09-14 16:45:20 +0300

    demo styles fixes

--- packages/core/useThrottledRefHistory/demo.vue
+++ packages/core/useThrottledRefHistory/demo.vue
@@ -9,7 +9,10 @@ const format = (ts: number) => dayjs(ts).format()
 const delay: Ref<number> = ref(1000)
 
 const { count, inc, dec } = useCounter()
-const { history, undo, redo, canUndo, canRedo } = useThrottledRefHistory(count, { deep: true, throttle: delay })
+const { history, undo, redo, canUndo, canRedo } = useThrottledRefHistory(
+  count, {
+    deep: true, throttle: delay, capacity: 10,
+  })
 </script>
 
 <template>
@@ -27,6 +30,7 @@ const { history, undo, redo, canUndo, canRedo } = useThrottledRefHistory(count, 
   <button :disabled="!canRedo" @click="redo()">
     Redo
   </button>
+  <br />
   <span>Delay (in ms):</span>
   <input v-model="delay" type="number" />
   <br>
