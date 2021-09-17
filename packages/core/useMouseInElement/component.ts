e557fa074d7a92f0eaef0de447909c2acbe4624a
wheat <jacobrclevenger@gmail.com>
2021-09-17 02:28:43 -0400

    feat: added 'as' prop to renderable components (#742)

--- packages/core/useMouseInElement/component.ts
+++ packages/core/useMouseInElement/component.ts
@@ -1,16 +1,17 @@
 import { h, ref, defineComponent, reactive } from 'vue-demi'
 import { useMouseInElement, MouseInElementOptions } from '@vueuse/core'
+import { RenderableComponent } from '../types'
 
-export const UseMouseInElement = defineComponent<MouseInElementOptions>({
+export const UseMouseInElement = defineComponent<MouseInElementOptions & RenderableComponent>({
   name: 'UseMouseElement',
-  props: ['handleOutside'] as unknown as undefined,
+  props: ['handleOutside', 'as'] as unknown as undefined,
   setup(props, { slots }) {
     const target = ref()
     const data = reactive(useMouseInElement(target, props))
 
     return () => {
       if (slots.default)
-        return h('div', { ref: target }, slots.default(data))
+        return h(props.as || 'div', { ref: target }, slots.default(data))
     }
   },
 })
