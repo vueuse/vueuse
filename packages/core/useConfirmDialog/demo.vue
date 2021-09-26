dae11293e5fb4a5d54d755b740eedda0a7a1b3ab
harmyderoman <romangarmider@gmail.com>
2021-09-26 13:01:12 +0300

    docs

--- packages/core/useConfirmDialog/demo.vue
+++ packages/core/useConfirmDialog/demo.vue
@@ -1,53 +1,48 @@
-<script lang="ts">
-import { defineComponent, ref } from '@vue-demi'
+<script setup lang="ts">
+import { ref } from 'vue-demi'
 import { useConfirmDialog } from '.'
 
-export default defineComponent({
-  name: 'Demo',
-  setup() {
-    const content = ref('')
-    const show = ref(false)
-    const {
-      showDialog,
-      confirm,
-      cancel,
-      onConfirm,
-      onReject,
-    } = useConfirmDialog(show, () => console.log('Modal is shown!'))
+const content = ref('Confirm or Reject:')
+const message = ref('')
+const show = ref(false)
+const {
+  showDialog,
+  confirm,
+  cancel,
+  onConfirm,
+  onCancel,
+} = useConfirmDialog(show, () => console.log('Modal is shown!'))
 
-    onConfirm(() => {
-      console.log('Confirmed!')
-    })
-
-    return {
-      show,
-      content,
-      showDialog,
-      confirm,
-      cancel,
-    }
-  },
+onConfirm(() => {
+  message.value = 'Confirmed!'
 })
+
+onCancel(() => {
+  message.value = 'Rejected!'
+})
+
 </script>
 
 <template>
-  <h1>My App</h1>
-  <input v-model="content" type="text" />
+  <h2>{{ message }}</h2>
   <button @click="showDialog">
-    Show Content
+    Click to Show Modal Dialog
   </button>
-  <Teleport to="body">
-    <div v-if="show" class="message-box dialog-mask" @click="cancel">
-      <div class="dialog-content">
-        <div class="dialog-body">
-          <p>{{ content }}</p>
-        </div>
-        <footer>
-          <button size="mini" @click="confirm">
-            OK
-          </button>
-        </footer>
+  <!-- <Teleport to="body"> -->
+  <div v-if="show" class="message-box dialog-mask">
+    <div class="dialog-content">
+      <div class="dialog-body">
+        <p>{{ content }}</p>
       </div>
+      <footer>
+        <button size="mini" @click="confirm">
+          OK
+        </button>
+        <button size="mini" @click="cancel">
+          Cancel
+        </button>
+      </footer>
     </div>
-  </Teleport>
+  </div>
+  <!-- </Teleport> -->
 </template>
