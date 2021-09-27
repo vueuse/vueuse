b0123feaf94ca401ef3a94d4b3fdc3baa2575408
harmyderoman <romangarmider@gmail.com>
2021-09-27 23:45:59 +0300

    tests: init

--- packages/core/useConfirmDialog/index.test.ts
+++ packages/core/useConfirmDialog/index.test.ts
@@ -1,41 +1,26 @@
+import { ref } from 'vue-demi'
 import { useSetup } from '../../.test'
-import { useCounter } from '.'
+import { useConfirmDialog } from '.'
 
 describe('useCounter', () => {
   it('should be defined', () => {
-    expect(useCounter).toBeDefined()
+    expect(useConfirmDialog).toBeDefined()
   })
 
-  it('should be update counter', () => {
+  it('should open the dialog and close on confirm', () => {
     useSetup(() => {
-      const { count, inc, dec, get, set, reset } = useCounter()
+      const show = ref(false)
 
-      expect(count.value).toBe(0)
-      expect(get()).toBe(0)
-      inc()
-      expect(count.value).toBe(1)
-      expect(get()).toBe(1)
-      inc(2)
-      expect(count.value).toBe(3)
-      expect(get()).toBe(3)
-      dec()
-      expect(count.value).toBe(2)
-      expect(get()).toBe(2)
-      dec(5)
-      expect(count.value).toBe(-3)
-      expect(get()).toBe(-3)
-      set(100)
-      expect(count.value).toBe(100)
-      expect(get()).toBe(100)
-      reset()
-      expect(count.value).toBe(0)
-      expect(get()).toBe(0)
-      reset(25)
-      expect(count.value).toBe(25)
-      expect(get()).toBe(25)
-      reset()
-      expect(count.value).toBe(25)
-      expect(get()).toBe(25)
+      const {
+        showDialog,
+        confirm,
+      } = useConfirmDialog(show)
+
+      showDialog()
+      expect(show.value).toBe(true)
+
+      confirm()
+      expect(show.value).toBe(false)
     })
   })
 })
