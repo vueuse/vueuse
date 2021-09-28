abdf7903997c06f873ce180e326cc7551a6f3d40
harmyderoman <romangarmider@gmail.com>
2021-09-28 07:51:58 +0300

    chore: tests

--- packages/core/useConfirmDialog/index.test.ts
+++ packages/core/useConfirmDialog/index.test.ts
@@ -2,7 +2,7 @@ import { ref } from 'vue-demi'
 import { useSetup } from '../../.test'
 import { useConfirmDialog } from '.'
 
-describe('useCounter', () => {
+describe('useConfirmDialog', () => {
   it('should be defined', () => {
     expect(useConfirmDialog).toBeDefined()
   })
@@ -21,6 +21,67 @@ describe('useCounter', () => {
 
       confirm()
       expect(show.value).toBe(false)
+    })
+  })
+  it('should close on cancel', () => {
+    useSetup(() => {
+      const show = ref(false)
+
+      const {
+        showDialog,
+        cancel,
+      } = useConfirmDialog(show)
+
+      showDialog()
+      expect(show.value).toBe(true)
+
+      cancel()
+      expect(show.value).toBe(false)
+    })
+  })
+  it('should execute a callback inside `onConfirm` hook only after confirming', () => {
+    useSetup(() => {
+      const show = ref(false)
+      const message = ref('initial')
+
+      const {
+        showDialog,
+        confirm,
+        onConfirm,
+      } = useConfirmDialog(show)
+
+      onConfirm(() => {
+        message.value = 'final'
+      })
+      expect(message.value).toBe('initial')
+
+      showDialog()
+      expect(message.value).toBe('initial')
+      confirm()
+      expect(message.value).toBe('final')
+    })
+  })
+
+  it('should execute a callback inside `onCancel` hook only after canceling dialog', () => {
+    useSetup(() => {
+      const show = ref(false)
+      const message = ref('initial')
+
+      const {
+        showDialog,
+        cancel,
+        onCancel,
+      } = useConfirmDialog(show)
+
+      onCancel(() => {
+        message.value = 'final'
+      })
+      expect(message.value).toBe('initial')
+
+      showDialog()
+      expect(message.value).toBe('initial')
+      cancel()
+      expect(message.value).toBe('final')
     })
   })
 })
