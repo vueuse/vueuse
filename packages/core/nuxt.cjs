2b11461246fe6904daed4cc70c4349f2ac77d26a
Anthony Fu <anthonyfu117@hotmail.com>
2021-11-15 23:41:54 +0800

    fix(nuxt): opt-out vite optimization

--- packages/core/nuxt.cjs
+++ packages/core/nuxt.cjs
@@ -21,6 +21,23 @@ const disabled = [
 module.exports = function() {
   const { nuxt } = this
 
+  // opt-out Vite deps optimization for VueUse
+  nuxt.hook('vite:extend', ({ config }) => {
+    config.optimizeDeps = config.optimizeDeps || {}
+    config.optimizeDeps.exclude = config.optimizeDeps.exclude || []
+    config.optimizeDeps.exclude.push(
+      '@vueuse/core',
+      '@vueuse/shared',
+      '@vueuse/integrations',
+      '@vueuse/components',
+      '@vueuse/motion',
+      '@vueuse/firebase',
+      '@vueuse/rxjs',
+      '@vueuse/sound',
+      '@vueuse/head',
+    )
+  })
+
   nuxt.hook('autoImports:sources', (sources) => {
     if (sources.find(i => i.from === '@vueuse/core/nuxt'))
       return
