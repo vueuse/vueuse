9eeb41021ffdf6aa0a57cec923087fa9bb976915
Anthony Fu <anthonyfu117@hotmail.com>
2021-11-19 20:34:28 +0800

    chore: update

--- packages/core/useMemory/demo.vue
+++ packages/core/useMemory/demo.vue
@@ -1,8 +1,16 @@
 <template>
-  <div v-if="isSupported">
-    <div>totalJSHeapSize:{{ memory?.totalJSHeapSize }}</div>
-    <div>usedJSHeapSize:{{ memory?.usedJSHeapSize }}</div>
-    <div>jsHeapSizeLimit:{{ memory?.jsHeapSizeLimit }}</div>
+  <div v-if="isSupported && memory" class="inline-grid grid-cols-2 gap-x-4 gap-y-2">
+    <template v-if="memory">
+      <div opacity="50">
+        Used
+      </div><div>{{ size(memory.usedJSHeapSize) }}</div>
+      <div opacity="50">
+        Allocated
+      </div><div>{{ size(memory.totalJSHeapSize) }}</div>
+      <div opacity="50">
+        Limit
+      </div><div>{{ size(memory.jsHeapSizeLimit) }}</div>
+    </template>
   </div>
   <div v-else>
     Your browser does not support performance memory API
@@ -12,5 +20,9 @@
 <script lang="ts" setup>
 import { useMemory } from '@vueuse/core'
 
+const size = (v: number) => {
+  const kb = v / 1024 / 1024
+  return `${kb.toFixed(2)} MB`
+}
 const { isSupported, memory } = useMemory()
 </script>
