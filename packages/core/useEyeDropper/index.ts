bc7bae86aba7e8885813bbf51f5e2a87f34a780a
okxiaoliang4 <353742991@qq.com>
2021-11-23 12:01:57 +0800

    chore(useEyeDropper): ts, initialValue

--- packages/core/useEyeDropper/index.ts
+++ packages/core/useEyeDropper/index.ts
@@ -1,14 +1,28 @@
 import { ref } from 'vue-demi'
 
-export function useEyeDropper() {
+type WindowEyeDropper = Window & typeof globalThis & {
+  EyeDropper: {
+    new(): {
+      open: () => Promise<{ sRGBHex: string }>
+      [Symbol.toStringTag]: 'EyeDropper'
+    }
+  }
+}
+
+/**
+ * Reactive [EyeDropper API](https://developer.mozilla.org/en-US/docs/Web/API/EyeDropper_API)
+ *
+ * @see https://vueuse.org/useEyeDropper
+ * @param initialValue string
+ */
+export function useEyeDropper(initialValue = '') {
   const isSupported = Boolean(typeof window !== 'undefined' && 'EyeDropper' in window)
-  const sRGBHex = ref('')
+  const sRGBHex = ref(initialValue)
 
   async function open() {
     if (!isSupported)
       return
-    // @ts-expect-error
-    const eyeDropper = new window.EyeDropper()
+    const eyeDropper = new (window as WindowEyeDropper).EyeDropper()
     const result = await eyeDropper.open()
     sRGBHex.value = result.sRGBHex
     return result
