55450dab20ac854d41fb1a5c3ea9e9db95c59ed5
Anthony Fu <anthonyfu117@hotmail.com>
2021-11-25 01:04:20 +0800

    chore: set Vite and Nuxt playgrounds

---
+++ playgrounds/nuxt3/nuxt.config.mjs
@@ -0,0 +1,12 @@
+import { resolve, dirname } from 'path'
+import { fileURLToPath } from 'url'
+import { defineNuxtConfig } from 'nuxt3'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+export default defineNuxtConfig({
+  alias: {
+    '@vueuse/core': resolve(__dirname, '../../packages/core/index.ts'),
+    '@vueuse/shared': resolve(__dirname, '../../packages/shared/index.ts'),
+  },
+})
