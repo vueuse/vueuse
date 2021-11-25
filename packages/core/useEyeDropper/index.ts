ec633034d00e485a7a5e02fb2dcfc4bee2754704
okxiaoliang4 <353742991@qq.com>
2021-11-25 11:05:41 +0800

    chore(useEyeDropper): options,types

--- packages/core/useEyeDropper/index.ts
+++ packages/core/useEyeDropper/index.ts
@@ -1,12 +1,33 @@
 import { ref } from 'vue-demi'
 
+export interface EyeDropperOpenOptions {
+  /**
+   * @see https://developer.mozilla.org/en-US/docs/Web/API/AbortSignal
+   */
+  signal?: AbortSignal
+}
+
+export interface EyeDropperPrototype {
+  open: (options?: EyeDropperOpenOptions) => Promise<{ sRGBHex: string }>
+  [Symbol.toStringTag]: 'EyeDropper'
+}
+
+export interface EyeDropper {
+  prototype: EyeDropperPrototype
+  new(): EyeDropperPrototype
+}
+
+export interface UseEyeDropperOptions extends EyeDropperOpenOptions {
+  /**
+   * Initial sRGBHex.
+   *
+   * @default ''
+   */
+  initialValue?: string
+}
+
 type WindowEyeDropper = Window & typeof globalThis & {
-  EyeDropper: {
-    new(): {
-      open: () => Promise<{ sRGBHex: string }>
-      [Symbol.toStringTag]: 'EyeDropper'
-    }
-  }
+  EyeDropper: EyeDropper
 }
 
 /**
@@ -15,15 +36,18 @@ type WindowEyeDropper = Window & typeof globalThis & {
  * @see https://vueuse.org/useEyeDropper
  * @param initialValue string
  */
-export function useEyeDropper(initialValue = '') {
+export function useEyeDropper(options: UseEyeDropperOptions = {}) {
+  const { initialValue = '', signal } = options
   const isSupported = Boolean(typeof window !== 'undefined' && 'EyeDropper' in window)
   const sRGBHex = ref(initialValue)
 
+  const eyeDropper = new (window as WindowEyeDropper).EyeDropper()
   async function open() {
     if (!isSupported)
       return
-    const eyeDropper = new (window as WindowEyeDropper).EyeDropper()
-    const result = await eyeDropper.open()
+    const result = await eyeDropper.open({
+      signal,
+    })
     sRGBHex.value = result.sRGBHex
     return result
   }
