3d06a33b7c962e86a1ed88045af74fffbc91066e
caoyugang <1442770751@qq.com>
2021-12-02 23:30:43 +0800

    chore: update useAsyncQueue

---
+++ packages/core/useAsyncQueue/demo.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { useAsyncQueue } from '.'
+
+const p1 = () => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(1000)
+    }, 10)
+  })
+}
+
+const p2 = (result: number) => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(1000 + result)
+    }, 20)
+  })
+}
+
+const { activeIndex, result } = useAsyncQueue([p1, p2])
+
+</script>
+
+<template>
+  <div>
+    <note>activeIndex: {{ activeIndex }}</note>
+    <note>result: {{ result }}</note>
+    <button @click="execute()">
+      Execute
+    </button>
+  </div>
+</template>
