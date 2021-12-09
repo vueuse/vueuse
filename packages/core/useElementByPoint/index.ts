c321dbc7815fb2ba60497cb292fea68285cf6f3e
okxiaoliang4 <353742991@qq.com>
2021-12-09 10:33:22 +0800

    chore(useElementByPoint): options

--- packages/core/useElementByPoint/index.ts
+++ packages/core/useElementByPoint/index.ts
@@ -2,8 +2,24 @@ import { ref, unref } from 'vue-demi'
 import { MaybeRef } from '@vueuse/shared'
 import { useRafFn } from '@vueuse/core'
 
-export function useElementByPoint(x: MaybeRef<number>, y: MaybeRef<number>) {
+export interface UseElementByPointOptions {
+  x: MaybeRef<number>
+  y: MaybeRef<number>
+}
+
+/**
+ * Reactive element by point.
+ *
+ * @see https://vueuse.org/useElementByPoint
+ * @param options - UseElementByPointOptions
+ */
+export function useElementByPoint(options: UseElementByPointOptions) {
   const element = ref<HTMLElement | null>(null)
+
+  const {
+    x,
+    y,
+  } = options
 
   const controls = useRafFn(() => {
     element.value = document.elementFromPoint(unref(x), unref(y)) as HTMLElement | null
@@ -14,3 +30,5 @@ export function useElementByPoint(x: MaybeRef<number>, y: MaybeRef<number>) {
     ...controls,
   }
 }
+
+export type UseElementByPointReturn = ReturnType<typeof useElementByPoint>
