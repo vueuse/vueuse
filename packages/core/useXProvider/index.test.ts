85af3477ae8a2ba60e4a1742fa99165d0a20f522
刘云飞 <liuyunfei3@xiaomi.com>
2021-12-10 19:03:46 +0800

    ✅ test: useXProvider: should work with partical types

--- packages/core/useXProvider/index.test.ts
+++ packages/core/useXProvider/index.test.ts
@@ -1,4 +1,6 @@
 
+import { computed, defineComponent, h } from 'vue-demi'
+import { mount } from '../../.test'
 import { useXProvider } from '.'
 describe('useXProvider', () => {
   it('should not work without instantiation of ProviderComponent', () => {
@@ -14,9 +16,50 @@ describe('useXProvider', () => {
     const providerFoundation = useXProvider<IContext>(defaultState)
 
     const state = providerFoundation.state
-    console.log(state)
 
-    expect(state.age).toBe(undefined)
-    expect(state.name).toBe(undefined)
+    expect(state.age).toBeUndefined()
+    expect(state.name).toBeUndefined()
+  })
+
+  it('should work with partical types', () => {
+    interface IContext {
+      name: string
+      age: number
+      action: Function
+    }
+    const defaultState = {
+      name: 'hello',
+      age: 20,
+    }
+    const providerFoundation = useXProvider<IContext>(defaultState)
+    const state = providerFoundation.state
+    const useContext = providerFoundation.useConText
+
+    const descendNode = defineComponent({
+      setup() {
+        const demoContext = useContext()
+        const name = computed(() => demoContext.name)
+
+        expect(name.value).toEqual(state.name)
+
+        return {
+          name,
+        }
+      },
+      render() {
+        return h('div')
+      },
+    })
+    const vm = mount(defineComponent({
+      render() {
+        return h(providerFoundation.ProviderComponent, h(descendNode))
+      },
+    }))
+
+    console.log(vm.$refs[0])
+
+    expect(state.age).toEqual(20)
+    expect(state.name).toEqual('hello')
+    expect(state.action).toBeUndefined()
   })
 })
