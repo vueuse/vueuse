b995216a470036e612eb0fe77daef5b71564fe0e
Sergey Shumov <DarkXanteR@users.noreply.github.com>
2021-12-12 05:14:40 +0300

    feat(useAsyncState): add option for choosing shallowRef or ref (#1040)

--- packages/core/useAsyncState/index.ts
+++ packages/core/useAsyncState/index.ts
@@ -41,6 +41,13 @@ export interface AsyncStateOptions {
    * @default true
    */
   resetOnExecute?: boolean
+
+  /**
+   * Use shallowRef.
+   *
+   * @default true
+   */
+  shallow?: boolean
 }
 
 /**
@@ -62,9 +69,10 @@ export function useAsyncState<T>(
     delay = 0,
     onError = noop,
     resetOnExecute = true,
+    shallow = true,
   } = options
 
-  const state = shallowRef(initialState)
+  const state = shallow ? shallowRef(initialState) : ref(initialState) as Ref<T>
   const isReady = ref(false)
   const error = ref<unknown | undefined>(undefined)
 
