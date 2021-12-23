93404e4222406f8629c8c411691f6450803ddd07
Jessica Sachs <jess@jessicasachs.io>
2021-12-22 23:29:35 -0500

    adding useFetch and more tests

--- packages/.test/test.setup.ts
+++ packages/.test/test.setup.ts
@@ -22,7 +22,7 @@ Object.defineProperty(window, 'localStorage', {
 })
 
 // Like `until` but works off of any assertion, not application code.
-export const retry = (assertion: Function, { interval = 20, timeout = 1000 } = {}) => {
+export const retry = (assertion: Function, { interval = 1, timeout = 100 } = {}) => {
   return new Promise((resolve, reject) => {
     const startTime = Date.now();
 
