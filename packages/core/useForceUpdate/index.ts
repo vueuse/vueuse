b0df4d5c37e7b7a471d2ebbddb654ceda00adbe4
okxiaoliang4 <353742991@qq.com>
2021-12-26 17:57:41 +0800

    docs(useForceUpdate): update comment

--- packages/core/useForceUpdate/index.ts
+++ packages/core/useForceUpdate/index.ts
@@ -1,5 +1,10 @@
 import { getCurrentInstance } from 'vue-demi'
 
+/**
+ * $forceUpdate in setup
+ *
+ * @see https://vueuse.org/core/useForceUpdate
+ */
 export function useForceUpdate() {
   const instance = getCurrentInstance()
   return () => instance?.proxy?.$forceUpdate()
