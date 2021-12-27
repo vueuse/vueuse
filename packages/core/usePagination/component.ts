c7a80d3f46c942662ca4f9146103133ead2e6c65
okxiaoliang4 <353742991@qq.com>
2021-12-27 23:52:47 +0800

    feat(usePagination) new component

---
+++ packages/core/usePagination/component.ts
@@ -0,0 +1,40 @@
+import { defineComponent, reactive } from 'vue-demi'
+import { usePagination } from '@vueuse/core'
+
+export const UsePagination = defineComponent({
+  name: 'UsePagination',
+  props: {
+    total: {
+      type: Number,
+      required: true,
+    },
+    pageSize: {
+      type: Number,
+      default: 10,
+    },
+    page: {
+      type: Number,
+      default: 1,
+    },
+  },
+  emits: ['page-change', 'page-size-change', 'page-count-change'],
+  setup(props, { slots, emit }) {
+    const data = reactive(usePagination({
+      ...props,
+      onPageChange(...args) {
+        emit('page-change', args)
+      },
+      onPageSizeChange(...args) {
+        emit('page-size-change', args)
+      },
+      onPageCountChange(...args) {
+        emit('page-count-change', args)
+      },
+    }))
+
+    return () => {
+      if (slots.default)
+        return slots.default(data)
+    }
+  },
+})
