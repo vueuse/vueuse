6db1ef14ff0d10a20ebf510f81768833e1749c91
Anthony Fu <anthonyfu117@hotmail.com>
2022-01-23 08:23:44 +0800

    chore: fix docs

--- vitest.config.ts
+++ vitest.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
       '@vueuse/shared': resolve(__dirname, 'packages/shared/index.ts'),
       '@vueuse/core': resolve(__dirname, 'packages/core/index.ts'),
       '@vueuse/components': resolve(__dirname, 'packages/components/index.ts'),
-      '@vueuse/docs-utils': resolve(__dirname, 'packages/.vitepress/utils.ts'),
+      '@vueuse/docs-utils': resolve(__dirname, 'packages/.vitepress/plugins/utils.ts'),
     },
     dedupe: [
       'vue',
