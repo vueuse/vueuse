e95d4bb2dffa125816c56aa894bfefd15d29f6be
lsdsjy <lsdsjy@163.com>
2022-01-25 15:39:02 +0800

    feat(whenever): improve typing of callback params

--- packages/shared/whenever/index.ts
+++ packages/shared/whenever/index.ts
@@ -1,15 +1,18 @@
 import type { WatchCallback, WatchOptions, WatchSource } from 'vue-demi'
 import { watch } from 'vue-demi'
 
+type Truthy<T, U = NonNullable<T>> = U extends boolean ? true : U
+type WheneverCallback<T> = WatchCallback<Truthy<T>, T | undefined>
+
 /**
  * Shorthand for watching value to be truthy
  *
  * @see https://vueuse.js.org/whenever
  */
-export function whenever<T>(source: WatchSource<T>, cb: WatchCallback, options?: WatchOptions) {
+export function whenever<T>(source: WatchSource<T>, wcb: WheneverCallback<T>, options?: WatchOptions) {
   return watch(
     source,
-    (v, ov, onInvalidate) => { if (v) cb(v, ov, onInvalidate) },
+    (v, ov, onInvalidate) => { if (v) wcb(v as Truthy<T>, ov, onInvalidate) },
     options,
   )
 }
