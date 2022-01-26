06d15a742714fd66b044397ad2fc9f276e2165c5
wangwenlu <wangwenlu@kuaishou.com>
2022-01-26 14:08:12 +0800

    fix(useEagerComputed): allow specify watchOptions

--- packages/shared/eagerComputed/index.ts
+++ packages/shared/eagerComputed/index.ts
@@ -1,14 +1,18 @@
 // ported from https://dev.to/linusborg/vue-when-a-computed-property-can-be-the-wrong-tool-195j
 // by @linusborg https://github.com/LinusBorg
 
-import type { Ref } from 'vue-demi'
-import { readonly, shallowRef, watchSyncEffect } from 'vue-demi'
+import type { Ref, WatchOptionsBase } from 'vue-demi'
+import { readonly, shallowRef, watchEffect } from 'vue-demi'
 
-export function eagerComputed<T>(fn: () => T): Readonly<Ref<T>> {
+export function eagerComputed<T>(fn: () => T, options?: WatchOptionsBase): Readonly<Ref<T>> {
+  const { flush = 'sync' } = options ?? {}
+
   const result = shallowRef()
 
-  watchSyncEffect(() => {
+  watchEffect(() => {
     result.value = fn()
+  }, {
+    flush,
   })
 
   return readonly(result)
