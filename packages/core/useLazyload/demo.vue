377ca40d7cb5a81555815ee48ed5acee64eff0fc
unknown_ <jeremychan595@gmail.com>
2022-01-27 18:34:18 +0800

    feat(useLazyload): new function

---
+++ packages/core/useLazyload/demo.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import { ref } from 'vue-demi'
+import { useLazyload } from '.'
+
+const target = ref(null)
+useLazyload(target)
+</script>
+
+<template>
+  <div ref="root" class="root">
+    <p class="notice">
+      Scroll me down!
+    </p>
+    <img ref="target" class="target" data-src="/vue.png">
+  </div>
+</template>
+
+<style scoped>
+.root {
+  border: 2px dashed #ccc;
+  height: 200px;
+  margin: 0 2rem 1rem;
+  overflow-y: scroll;
+}
+.notice {
+  text-align: center;
+  padding: 2em 0;
+  margin-bottom: 300px;
+  font-style: italic;
+  font-size: 1.2rem;
+  opacity: 0.8;
+}
+.target {
+  border: 2px dashed var(--c-brand);
+  padding: 10px;
+  max-height: 150px;
+  margin: 0 2rem 1rem;
+  margin-bottom: 400px;
+}
+</style>
