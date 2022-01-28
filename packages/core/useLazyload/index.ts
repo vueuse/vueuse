6aea15585df3422d4703e3a2649cb2f541ed8dc5
unknown_ <jeremychan595@gmail.com>
2022-01-28 17:31:09 +0800

    feat(useLazyload): support video and remove preload function

--- packages/core/useLazyload/index.ts
+++ packages/core/useLazyload/index.ts
@@ -21,18 +21,12 @@ export interface LazyloadOptions extends ConfigurableWindow{
   threshold?: number | number[]
 }
 
-function preLoad(target: MaybeElementRef) {
-  const el = unrefElement(target)
-  if (!el) return
-  const placeholder = el.getAttribute('data-placeholder-background')
-  if (placeholder)
-    el.style.background = placeholder
-}
 function load(target: MaybeElementRef) {
   const el = unrefElement(target)
   if (!el) return
   const src = el.getAttribute('data-src')
   const srcSet = el.getAttribute('data-srcset')
+  const alt = el.getAttribute('data-alt')
   const backgroundImage = el.getAttribute('data-background-image')
   if (el.nodeName.toLowerCase() === 'picture') {
     let img = el.querySelector('img')
@@ -43,24 +37,36 @@ function load(target: MaybeElementRef) {
     }
     if (src)
       img.setAttribute('src', src)
+    if (alt)
+      img.setAttribute('alt', alt)
     if (shouldAppend) el.append(img)
+  }
+  if (el.nodeName.toLowerCase() === 'video' && !src) {
+    const children = el.children
+    if (children) {
+      for (let i = 0; i < children.length; i++) {
+        const child = children[i]
+        const childSrc = child.getAttribute('data-src')
+        if (childSrc)
+          child.setAttribute('src', childSrc)
+      }
+      (el as HTMLVideoElement).load()
+    }
   }
   if (src)
     el.setAttribute('src', src)
   if (srcSet)
     el.setAttribute('srcset', srcSet)
+  if (alt)
+    el.setAttribute('alt', alt)
   if (backgroundImage)
     el.style.backgroundImage = `url(${backgroundImage})`
 }
 export function useLazyload(target: MaybeElementRef, options: LazyloadOptions = {}) {
   const { isSupported } = useIntersectionObserver(target, ([{ isIntersecting }]) => {
-    if (isIntersecting) {
-      preLoad(target)
+    if (isIntersecting)
       load(target)
-    }
   }, options)
-  if (!isSupported) {
-    preLoad(target)
+  if (!isSupported)
     load(target)
-  }
 }
