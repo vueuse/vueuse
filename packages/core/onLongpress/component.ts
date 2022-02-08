eed28896c9200aa077922396d9bf0c702d5353a4
Anthony Fu <anthonyfu117@hotmail.com>
2022-02-08 17:55:07 +0800

    feat(onLongPress): new function (#1225)
    Co-authored-by: Anthony Fu <anthonyfu117@hotmail.com>
    Co-authored-by: Blackfaded <r.heinen@nanogiants.de>
    Co-authored-by: René Heinen <rene.heinen@gmx.net>

---
+++ packages/core/onLongpress/component.ts
@@ -0,0 +1,28 @@
+import { defineComponent, h, ref } from 'vue-demi'
+import type { RenderableComponent } from '../types'
+import type { OnLongPressOptions } from '.'
+import { onLongPress } from '.'
+
+export interface OnLongPressProps extends RenderableComponent {
+  options?: OnLongPressOptions
+}
+
+export const OnLongPress = defineComponent<OnLongPressProps>({
+  name: 'OnLongPress',
+  props: ['as', 'options'] as unknown as undefined,
+  emits: ['trigger'],
+  setup(props, { slots, emit }) {
+    const target = ref()
+    onLongPress(
+      target,
+      (e) => {
+        emit('trigger', e)
+      },
+      props.options,
+    )
+    return () => {
+      if (slots.default)
+        return h(props.as || 'div', { ref: target }, slots.default())
+    }
+  },
+})
