a2b2b0df69ae36bb0c6f84f4fc1dddaee7761588
Jelf <353742991@qq.com>
2022-02-08 09:52:11 +0800

    feat(useInterval): support ref as interval (#1215)

--- packages/shared/useIntervalFn/demo.vue
+++ packages/shared/useIntervalFn/demo.vue
@@ -4,14 +4,19 @@ import { useIntervalFn } from '.'
 
 const greetings = ['Hello', 'Hi', 'Yo!', 'Hey', 'Hola', 'こんにちは', 'Bonjour', 'Salut!', '你好']
 const word = ref('Hello')
+const interval = ref(500)
 
 const { pause, resume, isActive } = useIntervalFn(() => {
   word.value = greetings[Math.round(Math.random() * (greetings.length - 1))]
-}, 500)
+}, interval)
 </script>
 
 <template>
   <p>{{ word }}</p>
+  <p>
+    interval:
+    <input v-model="interval" type="number" placeholder="interval">
+  </p>
   <button v-if="isActive" class="orange" @click="pause">
     Pause
   </button>
