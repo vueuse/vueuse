aa65b5600bc56372c3177cc88531789504ca7639
btea <2356281422@qq.com>
2022-02-10 22:21:31 +0800

    feat(useCssVar): support prop be ref

--- packages/core/useCssVar/index.md
+++ packages/core/useCssVar/index.md
@@ -13,4 +13,8 @@ import { useCssVar } from '@vueuse/core'
 
 const el = ref(null)
 const color = useCssVar('--color', el)
+
+const elv = ref(null)
+const key = ref('--color')
+const colorVal = useCssVar(key, elv)
 ```
--- packages/core/useCssVar/index.ts
+++ packages/core/useCssVar/index.ts
@@ -1,4 +1,5 @@
-import { computed, ref, watch } from 'vue-demi'
+import { computed, ref, unref, watch } from 'vue-demi'
+import type { MaybeRef } from '@vueuse/shared'
 import type { ConfigurableWindow } from '../_configurable'
 import { defaultWindow } from '../_configurable'
 import type { MaybeElementRef } from '../unrefElement'
@@ -13,18 +14,26 @@ import { unrefElement } from '../unrefElement'
  * @param options
  */
 export function useCssVar(
-  prop: string,
+  prop: MaybeRef<string>,
   target?: MaybeElementRef,
   { window = defaultWindow }: ConfigurableWindow = {},
 ) {
   const variable = ref('')
   const elRef = computed(() => unrefElement(target) || window?.document?.documentElement)
+  const attr = computed(() => unref(prop))
+
+  watch(
+    attr,
+    (v) => {
+      variable.value = window?.getComputedStyle(elRef.value!).getPropertyValue(v) || ''
+    },
+  )
 
   watch(
     elRef,
     (el) => {
       if (el && window)
-        variable.value = window.getComputedStyle(el).getPropertyValue(prop)
+        variable.value = window.getComputedStyle(el).getPropertyValue(attr.value)
     },
     { immediate: true },
   )
@@ -33,7 +42,7 @@ export function useCssVar(
     variable,
     (val) => {
       if (elRef.value?.style)
-        elRef.value.style.setProperty(prop, val)
+        elRef.value.style.setProperty(attr.value, val)
     },
   )
 
