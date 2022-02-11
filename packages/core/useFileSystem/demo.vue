f515aaccb29b70526cbbb661a6d4767cdaf2ca0e
okxiaoliang4 <353742991@qq.com>
2022-02-11 23:04:09 +0800

    feat(useFileSystem): new function

---
+++ packages/core/useFileSystem/demo.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { useFileSystem } from '.'
+
+const { content, create, save, open } = useFileSystem()
+
+</script>
+
+<template>
+  <div>
+    <button @click="create">
+      new file
+    </button>
+    <button @click="open">
+      open
+    </button>
+    <button @click="save">
+      save
+    </button>
+
+    <div>
+      <textarea v-model="content" rows="20" cols="40" />
+    </div>
+  </div>
+</template>
---
+++ packages/core/useFileSystem/index.md
@@ -0,0 +1,15 @@
+---
+category: Browser
+---
+
+# useFileSystem
+
+Creating, Reading and Writing local files.
+
+## Usage
+
+```ts
+import { useFileSystem } from '@vueuse/core'
+
+const { content, create, save, open } = useFileSystem()
+```
