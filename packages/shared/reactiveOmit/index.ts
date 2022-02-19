18481120d17f5de1792489a16dce011e1cb0eadb
tolking <qw13131wang@gmail.com>
2022-02-19 17:02:50 +0800

    feat(reactiveOmit): support added or deleted keys

--- packages/shared/reactiveOmit/index.ts
+++ packages/shared/reactiveOmit/index.ts
@@ -1,5 +1,6 @@
 import type { UnwrapRef } from 'vue-demi'
-import { reactivePick } from '../reactivePick'
+import { computed, ref, watch } from 'vue-demi'
+import { reactivePick, toReactive } from '../index'
 
 /**
  * Reactively omit fields from a reactive object
@@ -9,8 +10,13 @@ import { reactivePick } from '../reactivePick'
 export function reactiveOmit<T extends object, K extends keyof T>(
   obj: T,
   ...keys: K[]
-): { [S in Exclude<keyof T, K>]: UnwrapRef<T[S]>; } {
-  const pickKeys = Object.keys(obj).filter(k => !keys.includes(k as any)) as Array<Exclude<keyof T, K>>
+): { [S in Exclude<keyof T, K>]: UnwrapRef<T[S]> } {
+  const v = ref({} as { [S in Exclude<keyof T, K>]: UnwrapRef<T[S]> })
+  const k = computed(() => Object.keys(obj).filter(k => !keys.includes(k as any)) as Array<Exclude<keyof T, K>>)
 
-  return reactivePick(obj, ...pickKeys)
+  watch(k, () => {
+    v.value = reactivePick(obj, ...k.value)
+  }, { immediate: true })
+
+  return toReactive(v)
 }
