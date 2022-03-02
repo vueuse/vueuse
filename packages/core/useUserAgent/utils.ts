07fbb8ea394caf4154d24b46f43f4459f0385dbb
Michael Roberts <michael@observerly.com>
2022-03-02 09:39:34 +0000

    Minor code style tweaks.

--- packages/core/useUserAgent/utils.ts
+++ packages/core/useUserAgent/utils.ts
@@ -1,6 +1,6 @@
 export function createVersionParts(count: number): string[] {
   const output = []
-  for (let ii = 0; ii < count; ii++)
+  for (let i = 0; i < count; i++)
     output.push('0')
 
   return output
