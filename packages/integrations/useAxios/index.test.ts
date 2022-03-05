2dec17261af4aaa28729f27cf33f407bc604115b
马灿 <45714422+hengshanMWC@users.noreply.github.com>
2022-03-05 17:29:41 +0800

    test(useAxios): Add waitUntilFinished logic case (#1363)

--- packages/integrations/useAxios/index.test.ts
+++ packages/integrations/useAxios/index.test.ts
@@ -1,4 +1,3 @@
-import { watch } from 'vue-demi'
 import type { AxiosRequestConfig } from 'axios'
 import axios from 'axios'
 import { useAxios } from '.'
@@ -14,120 +13,101 @@ describe('useAxios', () => {
   const options = { immediate: false }
   const path = '/todos/1'
   test('params: url', (done) => {
-    const { isFinished, data } = useAxios(url)
+    const { isFinished, data, then } = useAxios(url)
     expect(isFinished.value).toBeFalsy()
-    watch(
-      () => data.value,
-      (result) => {
-        expect(Object.keys(result).length > 0).toBeTruthy()
-        // macrotask
-        setTimeout(() => {
-          expect(isFinished.value).toBeTruthy()
-          done()
-        })
-      },
-    )
+    const onRejected = vitest.fn()
+
+    then((result) => {
+      expect(Object.keys(data.value).length > 0).toBeTruthy()
+      expect(result.data).toBe(data)
+      expect(isFinished.value).toBeTruthy()
+      expect(onRejected).toBeCalledTimes(0)
+      done()
+    }, onRejected)
   })
 
   test('params: url config', (done) => {
-    const { isFinished, data } = useAxios(url, config)
+    const { isFinished, then } = useAxios(url, config)
     expect(isFinished.value).toBeFalsy()
-    watch(
-      () => data.value,
-      (result) => {
-        expect(Object.keys(result).length > 0).toBeTruthy()
-        // macrotask
-        setTimeout(() => {
-          expect(isFinished.value).toBeTruthy()
-          done()
-        })
-      },
-    )
+    const onRejected = vitest.fn()
+
+    then()
+      .then((result) => {
+        expect(Object.keys(result.data.value).length > 0).toBeTruthy()
+        expect(isFinished.value).toBeTruthy()
+        expect(onRejected).toBeCalledTimes(0)
+        done()
+      }, onRejected)
   })
 
   test('params: url config options', (done) => {
-    const { isLoading, data, execute } = useAxios(url, config, options)
+    const { isLoading, execute, then } = useAxios(url, config, options)
     expect(isLoading.value).toBeFalsy()
     execute()
     expect(isLoading.value).toBeTruthy()
-    watch(
-      () => data.value,
-      (result) => {
-        expect(Object.keys(result).length > 0).toBeTruthy()
-        // macrotask
-        setTimeout(() => {
-          expect(isLoading.value).toBeFalsy()
-          done()
-        })
-      },
-    )
+    const onRejected = vitest.fn()
+
+    then((result) => {
+      expect(Object.keys(result.data.value).length > 0).toBeTruthy()
+      expect(isLoading.value).toBeFalsy()
+      expect(onRejected).toBeCalledTimes(0)
+      done()
+    }, onRejected)
   })
 
   test('params: url instance', (done) => {
-    const { isFinished, data } = useAxios(path, instance)
+    const { isFinished, then } = useAxios(path, instance)
     expect(isFinished.value).toBeFalsy()
-    watch(
-      () => data.value,
-      (result) => {
-        expect(Object.keys(result).length > 0).toBeTruthy()
-        // macrotask
-        setTimeout(() => {
-          expect(isFinished.value).toBeTruthy()
-          done()
-        })
-      },
-    )
+    const onRejected = vitest.fn()
+
+    then((result) => {
+      expect(Object.keys(result.data.value).length > 0).toBeTruthy()
+      expect(isFinished.value).toBeTruthy()
+      expect(onRejected).toBeCalledTimes(0)
+      done()
+    }, onRejected)
   })
 
   test('params: url instance options', (done) => {
-    const { isLoading, data, execute } = useAxios(path, instance, options)
+    const { isLoading, execute, then } = useAxios(path, instance, options)
     expect(isLoading.value).toBeFalsy()
     execute()
     expect(isLoading.value).toBeTruthy()
-    watch(
-      () => data.value,
-      (result) => {
-        expect(Object.keys(result).length > 0).toBeTruthy()
-        // macrotask
-        setTimeout(() => {
-          expect(isLoading.value).toBeFalsy()
-          done()
-        })
-      },
-    )
+    const onRejected = vitest.fn()
+
+    then((result) => {
+      expect(Object.keys(result.data.value).length > 0).toBeTruthy()
+      expect(isLoading.value).toBeFalsy()
+      expect(onRejected).toBeCalledTimes(0)
+      done()
+    }, onRejected)
   })
 
   test('params: url config instance', (done) => {
-    const { isFinished, data } = useAxios(path, config, instance)
+    const { isFinished, then } = useAxios(path, config, instance)
     expect(isFinished.value).toBeFalsy()
-    watch(
-      () => data.value,
-      (result) => {
-        expect(Object.keys(result).length > 0).toBeTruthy()
-        // macrotask
-        setTimeout(() => {
-          expect(isFinished.value).toBeTruthy()
-          done()
-        })
-      },
-    )
+    const onRejected = vitest.fn()
+
+    then((result) => {
+      expect(Object.keys(result.data.value).length > 0).toBeTruthy()
+      expect(isFinished.value).toBeTruthy()
+      expect(onRejected).toBeCalledTimes(0)
+      done()
+    }, onRejected)
   })
 
   test('params: url config instance options', (done) => {
-    const { isLoading, data, execute } = useAxios(path, config, instance, options)
+    const { isLoading, then, execute } = useAxios(path, config, instance, options)
     expect(isLoading.value).toBeFalsy()
     execute()
     expect(isLoading.value).toBeTruthy()
-    watch(
-      () => data.value,
-      (result) => {
-        expect(Object.keys(result).length > 0).toBeTruthy()
-        // macrotask
-        setTimeout(() => {
-          expect(isLoading.value).toBeFalsy()
-          done()
-        })
-      },
-    )
+    const onRejected = vitest.fn()
+
+    then((result) => {
+      expect(Object.keys(result.data.value).length > 0).toBeTruthy()
+      expect(isLoading.value).toBeFalsy()
+      expect(onRejected).toBeCalledTimes(0)
+      done()
+    }, onRejected)
   })
 })
