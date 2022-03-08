1346456deb467cc05474befcb3c3d67e850b905d
webfansplz <308241863@qq.com>
2022-03-08 18:34:29 +0800

    feat(useElementSize): directive support (#1365)

---
+++ packages/core/useElementSize/directive.ts
@@ -0,0 +1,23 @@
+import type { FunctionDirective } from 'vue-demi'
+import { watch } from 'vue-demi'
+import { useElementSize } from '.'
+import type { ElementSize } from '.'
+
+type RemoveFirstFromTuple<T extends any[]> =
+  T['length'] extends 0 ? undefined :
+    (((...b: T) => void) extends (a: any, ...b: infer I) => void ? I : [])
+
+type BindingValueFunction = (size: ElementSize) => void
+type VElementSizeOptions = RemoveFirstFromTuple<Parameters<typeof useElementSize>>
+type BindingValueArray = [BindingValueFunction, ...VElementSizeOptions]
+
+export const vElementSize: FunctionDirective<
+HTMLElement,
+BindingValueFunction | BindingValueArray
+> = (el, binding) => {
+  const handler = typeof binding.value === 'function' ? binding.value : binding.value?.[0]
+  const options = (typeof binding.value === 'function' ? [] : binding.value.slice(1)) as RemoveFirstFromTuple<BindingValueArray>
+
+  const { width, height } = useElementSize(el, ...options)
+  watch([width, height], ([width, height]) => handler({ width, height }))
+}
