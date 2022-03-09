aec4b0ee438e24048c0e0d028ce599ee4c405064
Anthony Fu <anthonyfu117@hotmail.com>
2022-03-09 19:02:22 +0800

    chore: update version list

--- meta/versions.ts
+++ meta/versions.ts
@@ -4,7 +4,9 @@ export const currentVersion = `v${version}`
 
 export const versions = [
   { version: currentVersion },
-  { version: 'v5.3', link: 'https://v5-3-0.vueuse.org/' },
-  { version: 'v4.11', link: 'https://v4-11-2.vueuse.org/' },
-  { version: 'v4.0', link: 'https://v4-0-6.vueuse.org/' },
+  { version: 'v7.7.1', link: 'https://v7-7-1.vueuse.org/' },
+  { version: 'v6.7.6', link: 'https://v6-7-6.vueuse.org/' },
+  { version: 'v5.3.0', link: 'https://v5-3-0.vueuse.org/' },
+  { version: 'v4.11.2', link: 'https://v4-11-2.vueuse.org/' },
+  { version: 'v4.0.6', link: 'https://v4-0-6.vueuse.org/' },
 ]
