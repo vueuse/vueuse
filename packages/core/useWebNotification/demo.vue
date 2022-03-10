6bc8dad27e1d29630fd00cd30ddf98c57518025e
wuliann <610575024@qq.com>
2022-03-10 19:15:38 +0800

    docs: add useWebNotification demo

---
+++ packages/core/useWebNotification/demo.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import { useWebNotification } from '@vueuse/core'
+import type { UseWebNotificationOptions } from '@vueuse/core'
+
+const options: UseWebNotificationOptions = {
+  title: 'Hello, VueUse world!',
+  dir: 'auto',
+  lang: 'en',
+  renotify: true,
+  tag: 'test',
+}
+
+const {
+  isSupported,
+  show,
+} = useWebNotification(options)
+</script>
+
+<template>
+  <div>
+    <p>
+      Supported:
+      <b>{{ isSupported }}</b>
+    </p>
+  </div>
+
+  <div v-if="isSupported">
+    <button @click="show">
+      show notification
+    </button>
+  </div>
+  <div v-else>
+    The Notification Web API is not supported in your browser.
+  </div>
+</template>
