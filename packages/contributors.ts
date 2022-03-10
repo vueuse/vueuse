4e45556560f29a500f8f980fb692567691f5ffbb
okxiaoliang4 <353742991@qq.com>
2022-03-10 14:43:39 +0800

    fix: links broken

--- packages/contributors.ts
+++ packages/contributors.ts
@@ -18,7 +18,7 @@ export interface CoreTeam {
 
 const contributorsAvatars: Record<string, string> = {}
 
-const getAvatarUrl = (name: string) => import.meta.hot ? `https://github.com/${name}.png` : `/user-avatars/${name}.png`
+const getAvatarUrl = (name: string) => `https://github.com/${name}.png`
 
 const contributorList = (contributors as string[]).reduce((acc, name) => {
   contributorsAvatars[name] = getAvatarUrl(name)
