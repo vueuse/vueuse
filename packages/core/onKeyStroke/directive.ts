7159bd2a565ec26dbdaed7347cd518e7aaed3703
webfansplz <308241863@qq.com>
2022-03-14 19:06:43 +0800

    fix(onKeyStroke): make directive only registered once

--- packages/core/onKeyStroke/directive.ts
+++ packages/core/onKeyStroke/directive.ts
@@ -6,21 +6,28 @@ type BindingValueFunction = (event: KeyboardEvent) => void
 
 type BindingValueArray = [BindingValueFunction, KeyStrokeOptions]
 
-export const vOnKeyStroke: FunctionDirective<
+const vOnKeyStrokeHandler = (): FunctionDirective<
 HTMLElement,
 BindingValueFunction | BindingValueArray
-> = (el, binding) => {
-  const keys = binding.arg?.split(',') ?? []
-  if (typeof binding.value === 'function') {
-    onKeyStroke(keys, binding.value, {
-      target: el,
-    })
-  }
-  else {
-    const [handler, options] = binding.value
-    onKeyStroke(keys, handler, {
-      target: el,
-      ...options,
-    })
+> => {
+  let isMounted = false
+  return (el, binding) => {
+    if (isMounted) return
+    isMounted = true
+    const keys = binding.arg?.split(',') ?? []
+    if (typeof binding.value === 'function') {
+      onKeyStroke(keys, binding.value, {
+        target: el,
+      })
+    }
+    else {
+      const [handler, options] = binding.value
+      onKeyStroke(keys, handler, {
+        target: el,
+        ...options,
+      })
+    }
   }
 }
+
+export const vOnKeyStroke = vOnKeyStrokeHandler()
