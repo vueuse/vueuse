2348cc09f372b7df85f5f5d227f625d476907941
webfansplz <308241863@qq.com>
2022-03-14 19:12:47 +0800

    fix(useInfiniteScroll): make directive only registered once

--- packages/core/useInfiniteScroll/directive.ts
+++ packages/core/useInfiniteScroll/directive.ts
@@ -5,12 +5,19 @@ import type { UseInfiniteScrollOptions } from '.'
 type BindingValueFunction = Parameters<typeof useInfiniteScroll>[1]
 type BindingValueArray = [BindingValueFunction, UseInfiniteScrollOptions]
 
-export const vInfiniteScroll: FunctionDirective<
+export const vInfiniteScrollHandler = (): FunctionDirective<
 HTMLElement,
 BindingValueFunction | BindingValueArray
-> = (el, binding) => {
-  if (typeof binding.value === 'function')
-    useInfiniteScroll(el, binding.value)
-  else
-    useInfiniteScroll(el, ...binding.value)
+> => {
+  let isMounted = false
+  return (el, binding) => {
+    if (isMounted) return
+    isMounted = true
+    if (typeof binding.value === 'function')
+      useInfiniteScroll(el, binding.value)
+    else
+      useInfiniteScroll(el, ...binding.value)
+  }
 }
+
+export const vInfiniteScroll = vInfiniteScrollHandler()
