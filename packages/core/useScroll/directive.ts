9c49f427fb44fddc6ba8fd824dd6623322fea899
webfansplz <308241863@qq.com>
2022-03-15 11:44:29 +0800

    chore: update

--- packages/core/useScroll/directive.ts
+++ packages/core/useScroll/directive.ts
@@ -10,14 +10,10 @@ const vScrollHandler = (): FunctionDirective<
 HTMLElement,
 BindingValueFunction | BindingValueArray
 > => {
-  let isMounted = false
-  let ele = null as unknown as HTMLElement
+  let mountedEle: HTMLElement | null = null
   return (el, binding) => {
-    if (el !== ele) isMounted = false
-    if (isMounted) return
-    isMounted = true
-    ele = el
-
+    if (el === mountedEle) return
+    mountedEle = el
     if (typeof binding.value === 'function') {
       const handler = binding.value
       const state = useScroll(el, {
