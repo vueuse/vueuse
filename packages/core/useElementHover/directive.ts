008ba8f62fb5c60acfb3698af591d68303b478b1
webfansplz <308241863@qq.com>
2022-03-15 19:07:26 +0800

    chore: update

--- packages/core/useElementHover/directive.ts
+++ packages/core/useElementHover/directive.ts
@@ -1,23 +1,18 @@
 import { watch } from 'vue-demi'
-import type { FunctionDirective } from 'vue-demi'
+import { directiveHooks } from '@vueuse/shared'
+import type { ObjectDirective } from 'vue-demi'
 import { useElementHover } from '.'
 
 type BindingValueFunction = (state: boolean) => void
 
-export const vElementHoverHandler = (): FunctionDirective<
+export const vElementHover: ObjectDirective<
 HTMLElement,
 BindingValueFunction
-> => {
-  let mountedEle: HTMLElement | null = null
-  return (el, binding) => {
-    if (el === mountedEle) return
-    mountedEle = el
-
+> = {
+  [directiveHooks.mounted](el, binding) {
     if (typeof binding.value === 'function') {
       const isHovered = useElementHover(el)
       watch(isHovered, v => binding.value(v))
     }
-  }
+  },
 }
-
-export const vElementHover = vElementHoverHandler()
