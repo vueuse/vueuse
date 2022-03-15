1a50f2fa636cedc2e06558fa187dc162d37fa7e2
wuliann <610575024@qq.com>
2022-03-15 19:08:16 +0800

    docs: add useNavigatorLanguage demo

---
+++ packages/core/useNavigatorLanguage/demo.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { useNavigatorLanguage } from '@vueuse/core'
+
+const { isSupported, language } = useNavigatorLanguage()
+</script>
+
+<template>
+  <p>
+    Supported: <BooleanDisplay :value="isSupported" />
+  </p>
+  <note class="mb-2">
+    Navigator Language:
+  </note>
+  <div v-if="isSupported">
+    <code class="mr-2">{{ language }}</code>
+  </div>
+  <div v-else>
+    The Navigator.language API is not supported in your browser.
+  </div>
+</template>
