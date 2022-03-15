a3af53eb78d64dbd338964af607b8e107f407dfa
webfansplz <308241863@qq.com>
2022-03-15 10:53:43 +0800

    fix(useScrollLock): directive should work w/ v-if

--- packages/core/useScrollLock/directive.ts
+++ packages/core/useScrollLock/directive.ts
@@ -7,11 +7,15 @@ HTMLElement,
 boolean
 > => {
   let isMounted = false
+  let ele = null as unknown as HTMLElement
   const state = ref(false)
   return (el, binding) => {
     state.value = binding.value
+    if (el !== ele) isMounted = false
     if (isMounted) return
     isMounted = true
+    ele = el
+
     const isLocked = useScrollLock(el, binding.value)
     watch(state, v => isLocked.value = v)
   }
