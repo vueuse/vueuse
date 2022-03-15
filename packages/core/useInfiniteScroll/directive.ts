3a8d89571852ab8126c85e0b30b65edd0bc61f66
webfansplz <308241863@qq.com>
2022-03-15 19:12:07 +0800

    chore: update

--- packages/core/useInfiniteScroll/directive.ts
+++ packages/core/useInfiniteScroll/directive.ts
@@ -1,24 +1,19 @@
-import type { FunctionDirective } from 'vue-demi'
+import { directiveHooks } from '@vueuse/shared'
+import type { ObjectDirective } from 'vue-demi'
 import { useInfiniteScroll } from '.'
 import type { UseInfiniteScrollOptions } from '.'
 
 type BindingValueFunction = Parameters<typeof useInfiniteScroll>[1]
 type BindingValueArray = [BindingValueFunction, UseInfiniteScrollOptions]
 
-export const vInfiniteScrollHandler = (): FunctionDirective<
+export const vInfiniteScroll: ObjectDirective<
 HTMLElement,
 BindingValueFunction | BindingValueArray
-> => {
-  let mountedEle: HTMLElement | null = null
-  return (el, binding) => {
-    if (el === mountedEle) return
-    mountedEle = el
-
+> = {
+  [directiveHooks.mounted](el, binding) {
     if (typeof binding.value === 'function')
       useInfiniteScroll(el, binding.value)
     else
       useInfiniteScroll(el, ...binding.value)
-  }
+  },
 }
-
-export const vInfiniteScroll = vInfiniteScrollHandler()
