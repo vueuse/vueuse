3977417bb34222e33c24a74dbbde513558e851a8
webfansplz <308241863@qq.com>
2022-03-15 11:45:55 +0800

    chore: update

--- packages/core/onKeyStroke/directive.ts
+++ packages/core/onKeyStroke/directive.ts
@@ -10,13 +10,10 @@ const vOnKeyStrokeHandler = (): FunctionDirective<
 HTMLElement,
 BindingValueFunction | BindingValueArray
 > => {
-  let isMounted = false
-  let ele = null as unknown as HTMLElement
+  let mountedEle: HTMLElement | null = null
   return (el, binding) => {
-    if (el !== ele) isMounted = false
-    if (isMounted) return
-    isMounted = true
-    ele = el
+    if (el === mountedEle) return
+    mountedEle = el
 
     const keys = binding.arg?.split(',') ?? []
     if (typeof binding.value === 'function') {
