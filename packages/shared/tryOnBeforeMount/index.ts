83358c60ce136f387729b40478b972feea377f92
ryanmoyo <iherewithmyheart@gmail.com>
2022-03-26 17:49:34 +0800

    fix(useMediaQuery): media query before mount

---
+++ packages/shared/tryOnBeforeMount/index.ts
@@ -0,0 +1,18 @@
+// eslint-disable-next-line no-restricted-imports
+import { getCurrentInstance, nextTick, onBeforeMount } from 'vue-demi'
+import type { Fn } from '../utils'
+
+/**
+ * Call onBeforeMount() if it's inside a component lifecycle, if not, just call the function
+ *
+ * @param fn
+ * @param sync if set to false, it will run in the nextTick() of Vue
+ */
+export function tryOnBeforeMount(fn: Fn, sync = true) {
+  if (getCurrentInstance())
+    onBeforeMount(fn)
+  else if (sync)
+    fn()
+  else
+    nextTick(fn)
+}
