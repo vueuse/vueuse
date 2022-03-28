c91465f18a5dbde3ae138450cdd3e680840a7fdd
okxiaoliang4 <353742991@qq.com>
2022-03-28 12:00:45 +0800

    feat(useSort): dirty

--- packages/core/useSort/index.test.ts
+++ packages/core/useSort/index.test.ts
@@ -1,7 +1,71 @@
+import { unref } from 'vue'
+import { quickSort } from '../useQuickSort'
 import { useSort } from '.'
 
 describe('useSort', () => {
+  const arr = [10, 3, 5, 7, 2, 1, 8, 6, 9, 4]
+
   it('should be defined', () => {
     expect(useSort).toBeDefined()
+  })
+
+  it('should sort', () => {
+    const sorted = useSort(arr, quickSort)
+    expect(unref(sorted)).toMatchObject([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+  })
+
+  it('should pure sort function', () => {
+    const sorted = useSort(arr, quickSort)
+    expect(unref(sorted)).toMatchObject([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+  })
+
+  it('should dirty sort', () => {
+    const dirtyArr = [...arr]
+    const sorted = useSort(dirtyArr, quickSort, { dirty: true })
+    expect(unref(sorted)).toMatchObject([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+    expect(unref(dirtyArr)).toMatchObject([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+  })
+
+  it('should sort object', () => {
+    const objArr = [
+      {
+        name: 'John',
+        age: 40,
+      },
+      {
+        name: 'Jane',
+        age: 20,
+      },
+      {
+        name: 'Joe',
+        age: 30,
+      },
+      {
+        name: 'Jenny',
+        age: 22,
+      },
+    ]
+    const sorted = useSort(objArr, quickSort, {
+      compareFn: (a, b) => a.age - b.age,
+    })
+
+    expect(unref(sorted)).toMatchObject([
+      {
+        name: 'Jane',
+        age: 20,
+      },
+      {
+        name: 'Jenny',
+        age: 22,
+      },
+      {
+        name: 'Joe',
+        age: 30,
+      },
+      {
+        name: 'John',
+        age: 40,
+      },
+    ])
   })
 })
--- packages/core/useSort/index.ts
+++ packages/core/useSort/index.ts
@@ -1,6 +1,6 @@
 import { computed } from '@vue/reactivity'
 import type { MaybeRef } from '@vueuse/shared'
-import { unref } from 'vue-demi'
+import { isRef, unref, watchEffect } from 'vue-demi'
 
 export type UseSortCompareFn<T> = (a: T, b: T) => number
 
@@ -10,9 +10,22 @@ const defaultCompare: UseSortCompareFn<number> = (a, b) => a - b
 
 export interface UseSortOptions<T> {
   compareFn?: UseSortCompareFn<T>
+  dirty?: boolean
 }
 
 export function useSort<T>(arr: MaybeRef<T[]>, sortFn: UseSortFn<T>, options: UseSortOptions<T> = {}) {
-  const { compareFn = defaultCompare } = options
-  return computed(() => sortFn(unref(arr), compareFn as UseSortCompareFn<T>))
+  const { compareFn = defaultCompare, dirty = false } = options
+  if (!dirty) return computed(() => sortFn(unref(arr), compareFn as UseSortCompareFn<T>))
+
+  // dirty
+  watchEffect(() => {
+    const result = sortFn(unref(arr), compareFn as UseSortCompareFn<T>)
+    if (isRef(arr)) { arr.value = result }
+    else {
+      arr.length = 0
+      arr.push(...result)
+    }
+  })
+
+  return arr
 }
