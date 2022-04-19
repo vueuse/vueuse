06f7ef1918ad8faeffefd5bdf2326eb7b1efc121
Anthony Fu <anthonyfu117@hotmail.com>
2022-04-19 11:32:00 +0800

    Update packages/core/useRafFn/index.ts

--- packages/core/useRafFn/index.ts
+++ packages/core/useRafFn/index.ts
@@ -46,7 +46,7 @@ export function useRafFn(fn: Fn, options: RafFnOptions = {}): Pausable {
 
   function pause() {
     isActive.value = false
-    if (typeof rafId === 'number' && window) {
+    if (rafId != null && window) {
       window.cancelAnimationFrame(rafId)
       rafId = null
     }
