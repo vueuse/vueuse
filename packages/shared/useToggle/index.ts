cc4f0d3579e943b1c55a747474a595317c16b7ea
okxiaoliang4 <353742991@qq.com>
2022-04-23 01:29:12 +0800

    chore: update

--- packages/shared/useToggle/index.ts
+++ packages/shared/useToggle/index.ts
@@ -7,8 +7,8 @@ export interface UseToggleOptions<Truly, Falsely> {
   falselyValue?: MaybeRef<Falsely>
 }
 
-export function useToggle<Truly, Falsely, T = Truly | Falsely>(initialValue?: Ref<T>, options?: UseToggleOptions<Truly, Falsely>): (value?: T) => T
-export function useToggle<Truly, Falsely, T = Truly | Falsely>(initialValue?: T, options?: UseToggleOptions<Truly, Falsely>): [Ref<T>, (value?: T) => T]
+export function useToggle<Truly, Falsely, T = Truly | Falsely>(initialValue: Ref<T>, options?: UseToggleOptions<Truly, Falsely>): (value?: T) => T
+export function useToggle<Truly = true, Falsely = false, T = Truly | Falsely>(initialValue?: T, options?: UseToggleOptions<Truly, Falsely>): [Ref<T>, (value?: T) => T]
 
 /**
  * A boolean ref with a toggler
@@ -26,19 +26,15 @@ export function useToggle(initialValue: MaybeRef<unknown> = false, options: UseT
   const innerValue = ref(initialValue) as Ref<boolean>
 
   function toggle(value?: boolean) {
-    const _toggle = (value?: boolean) => {
-      if (value! === unref(trulyValue))
-        innerValue.value = unref(falselyValue)
-      else
-        innerValue.value = unref(trulyValue)
+    // has arguments
+    if (arguments.length) {
+      innerValue.value = value!
       return innerValue.value
     }
-
-    // has arguments
-    if (arguments.length)
-      return _toggle(value!)
-    else
-      return _toggle(innerValue.value)
+    else {
+      innerValue.value = innerValue.value === unref(trulyValue) ? unref(falselyValue) : unref(trulyValue)
+      return innerValue.value
+    }
   }
 
   if (valueIsRef)
