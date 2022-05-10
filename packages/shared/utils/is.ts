6ad5f075b2f97053b1e96de68b536a8c716d9a5c
LittleSound <464388324@qq.com>
2022-05-10 21:53:37 +0800

    ✨ TODO isIOS from useScrollLock to shared

--- packages/shared/utils/is.ts
+++ packages/shared/utils/is.ts
@@ -23,3 +23,4 @@ export const rand = (min: number, max: number) => {
   max = Math.floor(max)
   return Math.floor(Math.random() * (max - min + 1)) + min
 }
+export const isIOS = isClient && window?.navigator && window?.navigator?.userAgent && /iP(ad|hone|od)/.test(window?.navigator?.userAgent)
