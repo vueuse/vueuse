c0ceb7328ab905f47cdd61c900d93aea2b6b5dae
webfansplz <308241863@qq.com>
2022-06-13 15:02:33 +0800

    fix(useThrottleFn): `trailing` option should be `false` by default

--- packages/shared/useThrottleFn/index.ts
+++ packages/shared/useThrottleFn/index.ts
@@ -9,13 +9,13 @@ import { createFilterWrapper, throttleFilter } from '../utils'
  *                                    to `callback` when the throttled-function is executed.
  * @param   ms             A zero-or-greater delay in milliseconds. For event callbacks, values around 100 or 250 (or even higher) are most useful.
  *
- * @param [trailing=true] if true, call fn again after the time is up
+ * @param [trailing=false] if true, call fn again after the time is up
  *
  * @param [leading=true] if true, call fn on the leading edge of the ms timeout
  *
  * @return  A new, throttled, function.
  */
-export function useThrottleFn<T extends FunctionArgs>(fn: T, ms: MaybeRef<number> = 200, trailing = true, leading = true): T {
+export function useThrottleFn<T extends FunctionArgs>(fn: T, ms: MaybeRef<number> = 200, trailing = false, leading = true): T {
   return createFilterWrapper(
     throttleFilter(ms, trailing, leading),
     fn,
