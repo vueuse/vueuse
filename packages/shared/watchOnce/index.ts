77d4b4e7c9f250e583672af9044aefdb5b521dab
lvjiaxuan <471501748@qq.com>
2022-06-16 17:02:38 +0800

    chore(watchOnce,watchAtMost): remove duplicated overload (#1690)

--- packages/shared/watchOnce/index.ts
+++ packages/shared/watchOnce/index.ts
@@ -5,8 +5,6 @@ import type { MapOldSources, MapSources } from '../utils'
 // overlads
 export function watchOnce<T extends Readonly<WatchSource<unknown>[]>, Immediate extends Readonly<boolean> = false>(source: T, cb: WatchCallback<MapSources<T>, MapOldSources<T, Immediate>>, options?: WatchOptions<Immediate>): void
 
-export function watchOnce<T extends Readonly<WatchSource<unknown>[]>, Immediate extends Readonly<boolean> = false>(source: T, cb: WatchCallback<MapSources<T>, MapOldSources<T, Immediate>>, options?: WatchOptions<Immediate>): void
-
 export function watchOnce<T, Immediate extends Readonly<boolean> = false>(sources: WatchSource<T>, cb: WatchCallback<T, Immediate extends true ? T | undefined : T>, options?: WatchOptions<Immediate>): void
 
 // implementation
