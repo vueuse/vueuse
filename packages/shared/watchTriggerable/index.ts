9e6c6a66666ed9e71705d747e0b8d9a58fd3695d
LittleSound <464388324@qq.com>
2022-07-02 18:14:15 +0800

    first version

--- packages/shared/watchTriggerable/index.ts
+++ packages/shared/watchTriggerable/index.ts
@@ -1,4 +1,6 @@
-import { ref } from 'vue-demi'
+import type { WatchCallback, WatchSource } from 'vue-demi'
+import { isReactive, unref } from 'vue-demi'
+import type { MapOldSources, MapSources } from '../utils'
 import type { WatchIgnorableReturn } from '../watchIgnorable'
 import { watchIgnorable } from '../watchIgnorable'
 import type { WatchWithFilterOptions } from '../watchWithFilter'
@@ -7,27 +9,66 @@ export interface WatchTriggerableReturn extends WatchIgnorableReturn {
   trigger: () => void
 }
 
+export function watchTriggerable<T extends Readonly<WatchSource<unknown>[]>, Immediate extends Readonly<boolean> = false>(sources: [...T], cb: WatchCallback<MapSources<T>, MapOldSources<T, true>>, options?: WatchWithFilterOptions<Immediate>): WatchTriggerableReturn
+export function watchTriggerable<T, Immediate extends Readonly<boolean> = false>(source: WatchSource<T>, cb: WatchCallback<T, T | undefined>, options?: WatchWithFilterOptions<Immediate>): WatchTriggerableReturn
+export function watchTriggerable<T extends object, Immediate extends Readonly<boolean> = false>(source: T, cb: WatchCallback<T, T | undefined>, options?: WatchWithFilterOptions<Immediate>): WatchTriggerableReturn
+
 export function watchTriggerable<Immediate extends Readonly<boolean> = false>(
   source: any,
   cb: any,
   options: WatchWithFilterOptions<Immediate> = {},
 ): WatchTriggerableReturn {
-  const triggerCounter = ref(0)
+  let cleanupFn: () => void
 
-  const trigger = () => triggerCounter.value += 1
+  function onEffect() {
+    if (cleanupFn)
+      cleanupFn()
+  }
 
-  const _source = Array.isArray(source)
-    ? [triggerCounter, ...source]
-    : [triggerCounter, source]
+  function onCleanup(callback: () => void) {
+    cleanupFn = callback
+  }
 
   const _cb = (
-    [, ...value]: any[],
-    [, ...oldValue]: any[],
-    onCleanup: any,
-  ) => cb(value, oldValue, onCleanup)
+    value: any,
+    oldValue: any,
+    // onCleanup: any,
+  ) => {
+    onEffect()
+    cb(value, oldValue, onCleanup)
+  }
+  const res = watchIgnorable(source, _cb, options)
+  const { ignoreUpdates } = res
+
+  const trigger = () => {
+    ignoreUpdates(() => {
+      _cb(getWatchSources(source), getOldValue(source))
+    })
+  }
 
   return {
-    ...watchIgnorable(_source, _cb, options),
+    ...res,
     trigger,
   }
+}
+
+function getWatchSources(sources: any) {
+  if (isReactive(sources))
+    return sources
+  if (Array.isArray(sources))
+    return sources.map(item => getOneWatchSource(item))
+  return getOneWatchSource(sources)
+}
+
+function getOneWatchSource(source: Readonly<WatchSource<unknown>>) {
+  return typeof source === 'function'
+    ? source()
+    : unref(source)
+}
+
+// For calls triggered by trigger, the old value is unknown, so it cannot be returned
+function getOldValue(source: any) {
+  return Array.isArray(source)
+    ? source.map(() => undefined)
+    : undefined
 }
