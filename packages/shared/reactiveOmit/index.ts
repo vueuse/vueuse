c48e4807d16c84f53f598b565b7be0db7fd798d9
Brain777777 <35163869+Brain777777@users.noreply.github.com>
2022-07-04 16:12:36 +0800

    fix: Omit Second parameter support Array

--- packages/shared/reactiveOmit/index.ts
+++ packages/shared/reactiveOmit/index.ts
@@ -1,5 +1,6 @@
 import { toRefs } from 'vue-demi'
 import { reactiveComputed } from '../reactiveComputed'
+import type { Many } from '../utils'
 
 /**
  * Reactively omit fields from a reactive object
@@ -8,7 +9,7 @@ import { reactiveComputed } from '../reactiveComputed'
  */
 export function reactiveOmit<T extends object, K extends keyof T>(
   obj: T,
-  ...keys: K[]
+  ...keys: Array<Many<K>>
 ): Omit<T, K> {
   return reactiveComputed<any>(() => Object.fromEntries(Object.entries(toRefs(obj)).filter(e => !keys.includes(e[0] as any))))
 }
