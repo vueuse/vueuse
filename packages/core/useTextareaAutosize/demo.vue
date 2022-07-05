189a997f83bd56d74906c6ea8fc4f3731c33c5c8
Enzo Innocenzi <enzo@innocenzi.dev>
2022-07-05 22:45:59 +0200

    feat: add `useTextareaAutosize`

---
+++ packages/core/useTextareaAutosize/demo.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { useTextarea } from '@vueuse/core'
+
+const { textarea, input } = useTextarea()
+</script>
+
+<template>
+  <div>
+    <span>Type, the textarea will grow:</span>
+    <textarea
+      ref="textarea"
+      v-model="input"
+      class="resize-none"
+      placeholder="What's on your mind?"
+    />
+  </div>
+</template>
