dd94d76a236399bcf584187653b8fba581cd2217
Brain777777 <35163869+Brain777777@users.noreply.github.com>
2022-07-06 10:46:12 +0800

    fix(types): `reactiveOmit` and `reactivePick` support array as second arg (#1742)

--- packages/shared/reactiveOmit/index.ts
+++ packages/shared/reactiveOmit/index.ts
@@ -8,7 +8,7 @@ import { reactiveComputed } from '../reactiveComputed'
  */
 export function reactiveOmit<T extends object, K extends keyof T>(
   obj: T,
-  ...keys: K[]
+  ...keys: (K | K[])[]
 ): Omit<T, K> {
   return reactiveComputed<any>(() => Object.fromEntries(Object.entries(toRefs(obj)).filter(e => !keys.includes(e[0] as any))))
 }
