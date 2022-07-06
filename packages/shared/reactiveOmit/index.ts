80c164876503ea58c9974443934570e4b0d639d8
Anthony Fu <anthonyfu117@hotmail.com>
2022-07-06 11:05:58 +0800

    fix(reactivePick): allow nested keys

--- packages/shared/reactiveOmit/index.ts
+++ packages/shared/reactiveOmit/index.ts
@@ -10,5 +10,6 @@ export function reactiveOmit<T extends object, K extends keyof T>(
   obj: T,
   ...keys: (K | K[])[]
 ): Omit<T, K> {
-  return reactiveComputed<any>(() => Object.fromEntries(Object.entries(toRefs(obj)).filter(e => !keys.includes(e[0] as any))))
+  const flatKeys = keys.flat() as K[]
+  return reactiveComputed<any>(() => Object.fromEntries(Object.entries(toRefs(obj)).filter(e => !flatKeys.includes(e[0] as any))))
 }
