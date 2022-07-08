e57f03054e589532ce0a15b8f5f0f8cfc4ecdfc0
Anthony Fu <anthonyfu117@hotmail.com>
2022-07-08 13:38:48 +0800

    chore: update

--- packages/shared/logicOr/index.ts
+++ packages/shared/logicOr/index.ts
@@ -1,14 +1,15 @@
 import type { ComputedRef } from 'vue-demi'
-import { computed, unref } from 'vue-demi'
-import type { MaybeRef } from '../utils'
+import { computed } from 'vue-demi'
+import { resolveUnref } from '../resolveUnref'
+import type { MaybeComputedRef } from '../utils'
 
 /**
  * `OR` conditions for refs.
  *
  * @see https://vueuse.org/logicOr
  */
-export function logicOr(...args: MaybeRef<any>[]): ComputedRef<boolean> {
-  return computed(() => args.some(i => unref(i)))
+export function logicOr(...args: MaybeComputedRef<any>[]): ComputedRef<boolean> {
+  return computed(() => args.some(i => resolveUnref(i)))
 }
 
 // alias
