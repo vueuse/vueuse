7fbcae75b1bbe9463d99a80273dd3823e0c2b30e
okxiaoliang4 <353742991@qq.com>
2022-07-10 00:00:46 +0800

    chore: use `tryOnMounted`

--- packages/core/useSupported/index.ts
+++ packages/core/useSupported/index.ts
@@ -1,14 +1,14 @@
+import { tryOnMounted } from '@vueuse/shared'
 import type { Ref } from 'vue-demi'
-// eslint-disable-next-line no-restricted-imports
-import { onMounted, ref } from 'vue-demi'
+import { ref } from 'vue-demi'
 
-export function useSupported(callback: () => unknown) {
+export function useSupported(callback: () => unknown, sync = false) {
   const isSupported = ref() as Ref<boolean>
 
   const update = () => isSupported.value = Boolean(callback())
 
   update()
 
-  onMounted(update)
+  tryOnMounted(update, sync)
   return isSupported
 }
