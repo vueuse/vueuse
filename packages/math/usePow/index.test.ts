33419ab51cf3dc5c8d3b6af46fc83f0e0eb2a5e8
webfansplz <308241863@qq.com>
2022-07-10 14:21:38 +0800

    chore: update

--- packages/math/usePow/index.test.ts
+++ packages/math/usePow/index.test.ts
@@ -1,8 +1,18 @@
-// import { ref } from 'vue-demi'
+import { ref } from 'vue-demi'
 import { usePow } from '.'
 
 describe('usePow', () => {
   it('should be defined', () => {
     expect(usePow).toBeDefined()
+  })
+  it('should work', () => {
+    const base = ref(2)
+    const exponent = ref(2)
+    const result = usePow(base, exponent)
+    expect(result.value).toBe(4)
+    base.value = 4
+    expect(result.value).toBe(16)
+    exponent.value = 3
+    expect(result.value).toBe(64)
   })
 })
