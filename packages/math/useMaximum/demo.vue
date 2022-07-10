b870be37c9df3bd330cad6908438c2fce352c119
frankfang <frankfang1990@gmail.com>
2022-07-10 21:47:01 +0800

    fix: typo

--- packages/math/useMaximum/demo.vue
+++ packages/math/useMaximum/demo.vue
@@ -1,31 +1,31 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 import { useMaximum } from '@vueuse/math'
-const min1 = ref(10)
-const min2 = ref(20)
-const result = useMaximum(min1, min2)
+const number1 = ref(10)
+const number2 = ref(20)
+const result = useMaximum(number1, number2)
 </script>
 
 <template>
   <div>
     <div>
-      min1:
-      <input v-model="min1" type="number">
-      <button @click="min1--">
-        min1 -= 1
+      number1:
+      <input v-model="number1" type="number">
+      <button @click="number1--">
+        number1 -= 1
       </button>
-      <button @click="min1++">
-        min1 += 1
+      <button @click="number1++">
+        number1 += 1
       </button>
     </div>
     <div>
-      min2:
-      <input v-model="min2" type="number">
-      <button @click="min2--">
-        min2 -= 1
+      number2:
+      <input v-model="number2" type="number">
+      <button @click="number2--">
+        number2 -= 1
       </button>
-      <button @click="min2++">
-        min2 += 1
+      <button @click="number2++">
+        number2 += 1
       </button>
     </div>
     result:{{ result }}
