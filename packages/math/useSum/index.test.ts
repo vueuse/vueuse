b2b381c59467e3962d578737766aec356e270fa8
LittleSound <464388324@qq.com>
2022-07-10 17:08:48 +0800

    this should work

--- packages/math/useSum/index.test.ts
+++ packages/math/useSum/index.test.ts
@@ -1,7 +1,36 @@
+import { reactive, ref } from 'vue-demi'
 import { useSum } from '.'
 
 describe('useSum', () => {
   test('should be defined', () => {
     expect(useSum).toBeDefined()
   })
+
+  test('this should work', () => {
+    const value1 = ref(10)
+    const value2 = ref(20)
+
+    const sum = useSum([value1, value2])
+
+    expect(sum.value).toBe(30)
+
+    value1.value = 20
+    expect(sum.value).toBe(40)
+
+    value2.value = 30
+    expect(sum.value).toBe(50)
+  })
+
+  // test('reactive list', () => {
+  //   const list = reactive([10, 20])
+
+  //   const sum = useSum(list)
+  //   expect(sum.value).toBe(30)
+
+  //   list[0] = 21
+  //   expect(sum.value).toBe(41)
+
+  //   list.push(5)
+  //   expect(sum.value).toBe(46)
+  // })
 })
--- packages/math/useSum/index.ts
+++ packages/math/useSum/index.ts
@@ -1,3 +1,11 @@
-export function useSum() {
+import type { MaybeRef } from '@vueuse/shared'
+import { resolveUnref } from '@vueuse/shared'
+import type { ComputedRef } from 'vue-demi'
+import { computed } from 'vue-demi'
 
+export function useSum<T = number>(list: MaybeRef<T>[]): ComputedRef<T> {
+  return computed(() => {
+    const reduce = Array.prototype.reduce.bind(list)
+    return reduce((sum: any, value: any) => resolveUnref(sum) + resolveUnref(value))
+  })
 }
