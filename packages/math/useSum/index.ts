bf05983811a9d96054422ba3cf44ee3d9f07eb8a
Anthony Fu <anthonyfu117@hotmail.com>
2022-07-12 23:06:43 +0800

    feat(useSum): support rest usage

--- packages/math/useSum/index.ts
+++ packages/math/useSum/index.ts
@@ -3,11 +3,22 @@ import { computed } from 'vue-demi'
 import type { MaybeComputedRef } from '@vueuse/shared'
 import { resolveUnref } from '@vueuse/shared'
 
+export function useSum(array: MaybeComputedRef<MaybeComputedRef<number>[]>): ComputedRef<number>
+export function useSum(...args: MaybeComputedRef<number>[]): ComputedRef<number>
+
 /**
  * Get the sum of a set of numbers.
  *
  * @see https://vueuse.org/useSum
  */
-export function useSum(array: MaybeComputedRef<Array<MaybeComputedRef<number>>>): ComputedRef<number> {
-  return computed(() => resolveUnref(array).reduce<number>((sum, v) => sum += resolveUnref(v), 0))
+export function useSum(...args: any[]): ComputedRef<number> {
+  return computed(() => args
+    .flatMap((i) => {
+      const v = resolveUnref(i)
+      if (Array.isArray(v))
+        return v.map(i => resolveUnref(i))
+      return [v]
+    })
+    .reduce<number>((sum, v) => sum += v, 0),
+  )
 }
