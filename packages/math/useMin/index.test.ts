be80e43b3ccabf484c511bc8bc9236f47444a83f
holazz <2418184580@qq.com>
2022-07-13 08:49:49 +0800

    feat: support rest usage

--- packages/math/useMin/index.test.ts
+++ packages/math/useMin/index.test.ts
@@ -5,20 +5,21 @@ describe('useMin', () => {
   it('should be defined', () => {
     expect(useMin).toBeDefined()
   })
-  it('should accept ref array', () => {
-    const nums = ref([1, 2, 3, 4])
-    const min = useMin(nums)
+
+  it('array usage', () => {
+    const array = ref([1, 2, 3, 4])
+    const min = useMin(array)
     expect(min.value).toBe(1)
-    nums.value = [-1, -2, 3, 4]
+    array.value = [-1, -2, 3, 4]
     expect(min.value).toBe(-2)
   })
-  it('should accept ref item', () => {
-    const num1 = ref(1)
-    const num2 = ref(2)
-    const num3 = ref(3)
-    const min = useMin([num1, num2, num3])
+
+  it('rest usage', () => {
+    const a = ref(1)
+    const b = ref(2)
+    const min = useMin(a, () => b.value, 3)
     expect(min.value).toBe(1)
-    num3.value = -1
-    expect(min.value).toBe(-1)
+    b.value = -3
+    expect(min.value).toBe(-3)
   })
 })
--- packages/math/useMin/index.ts
+++ packages/math/useMin/index.ts
@@ -3,14 +3,20 @@ import { computed } from 'vue-demi'
 import type { MaybeComputedRef } from '@vueuse/shared'
 import { resolveUnref } from '@vueuse/shared'
 
+export function useMin(array: MaybeComputedRef<MaybeComputedRef<number>[]>): ComputedRef<number>
+export function useMin(...args: MaybeComputedRef<number>[]): ComputedRef<number>
+
 /**
  * Get the minimum value of a set of numbers.
  *
  * @see https://vueuse.org/useMin
  * @param nums
  */
-export function useMin(nums: MaybeComputedRef<Array<MaybeComputedRef<number>>>): ComputedRef<number> {
-  return computed(
-    () => Math.min(...resolveUnref(nums).map(resolveUnref)),
-  )
+export function useMin(...args: any[]): ComputedRef<number> {
+  return computed(() => Math.min(...args.flatMap((i) => {
+    const v = resolveUnref(i)
+    if (Array.isArray(v))
+      return v.map(i => resolveUnref(i))
+    return [v]
+  })))
 }
