e8bd1d555f44e586c17ed3c217ffdbfefd7c1bb7
webfansplz <308241863@qq.com>
2022-07-26 12:34:29 +0800

    chore: update

--- packages/shared/useDateFormat/index.ts
+++ packages/shared/useDateFormat/index.ts
@@ -6,7 +6,9 @@ export type DateLike = Date | number | string | undefined
 
 export interface UseDateFormatOptions {
   /**
-   * The locale(s) to use
+   * The locale(s) to used for dd/ddd/dddd format
+   *
+   * [MDN](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl#locales_argument).
    */
   locales?: Intl.LocalesArgument
 }
