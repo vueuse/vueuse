027717d8648f47186eb9c6d5e11f2888775ce40b
Waleed Khaled <waleed1kh@outlook.com>
2022-07-27 23:17:49 +0400

    useColorMode demo state implementation

--- packages/core/useColorMode/demo.vue
+++ packages/core/useColorMode/demo.vue
@@ -1,29 +1,31 @@
 <script setup lang="ts">
 import { useColorMode, useCycleList } from '@vueuse/core'
 
-const mode = useColorMode({
-  emitAuto: true,
+const { currentMode, setting } = useColorMode({
   modes: {
     contrast: 'dark contrast',
     cafe: 'cafe',
   },
-})
+}, 'state')
 
-const { next } = useCycleList(['dark', 'light', 'cafe', 'contrast', 'auto'], { initialValue: mode })
+const { next } = useCycleList(['dark', 'light', 'cafe', 'contrast', 'auto'], { initialValue: setting })
 </script>
 
 <template>
-  <button @click="next()">
-    <i v-if="mode === 'dark'" i-carbon-moon inline-block align-middle class="align-middle" />
-    <i v-if="mode === 'light'" i-carbon-sun inline-block align-middle class="align-middle" />
-    <i v-if="mode === 'cafe'" i-carbon-cafe inline-block align-middle class="align-middle" />
-    <i v-if="mode === 'contrast'" i-carbon-contrast inline-block align-middle class="align-middle" />
-    <i v-if="mode === 'auto'" i-carbon-laptop inline-block align-middle class="align-middle" />
-
-    <span class="ml-2 capitalize">{{ mode }}</span>
-  </button>
-
-  <span class="p-4 opacity-50">← Click to change the color mode</span>
+  <div>
+    <button @click="next()">
+      <i v-if="setting === 'dark'" i-carbon-moon inline-block align-middle class="align-middle" />
+      <i v-if="setting === 'light'" i-carbon-sun inline-block align-middle class="align-middle" />
+      <i v-if="setting === 'cafe'" i-carbon-cafe inline-block align-middle class="align-middle" />
+      <i v-if="setting === 'contrast'" i-carbon-contrast inline-block align-middle class="align-middle" />
+      <i v-if="setting === 'auto'" i-carbon-laptop inline-block align-middle class="align-middle" />
+      <span class="ml-2 capitalize">{{ setting }}</span>
+    </button>
+    <span class="p-4 opacity-50">← Click to change the color mode</span>
+  </div>
+  <div>
+    <span>Current mode: {{ currentMode }}</span>
+  </div>
 </template>
 
 <style>
