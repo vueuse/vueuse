764810b9626c87b590309c3d1b9464477f145821
wangwujun <wangwujun@cmss.chinamobile.com>
2022-08-04 15:52:10 +0800

    fix(useIntervalFn): fix typescript Generic Type ,rename any

--- packages/shared/useIntervalFn/index.ts
+++ packages/shared/useIntervalFn/index.ts
@@ -33,7 +33,7 @@ export function useIntervalFn(cb: Fn, interval: MaybeComputedRef<number> = 1000,
     immediateCallback = false,
   } = options
 
-  let timer: any = null
+  let timer: NodeJS.Timer | null = null
   const isActive = ref(false)
 
   function clean() {
