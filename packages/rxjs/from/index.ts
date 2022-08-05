5856a394f1ac2540b1b74c56bf44bdb9f6bfad97
luolei <luolei@kuaishou.com>
2022-08-05 18:28:26 +0800

    feat(fromEvent): support HTMLElement

--- packages/rxjs/from/index.ts
+++ packages/rxjs/from/index.ts
@@ -19,9 +19,12 @@ export function from<T>(value: ObservableInput<T> | Ref<T>, watchOptions?: Watch
   }
 }
 
-export function fromEvent<T extends HTMLElement>(value: Ref<T>, event: string): Observable<Event> {
-  return from(value, { immediate: true }).pipe(
-    filter(value => value instanceof HTMLElement),
-    mergeMap(value => fromEventRx(value, event)),
-  )
+export function fromEvent<T extends HTMLElement>(value: Ref<T> | T, event: string): Observable<Event> {
+  if (isRef<T>(value)) {
+    return from(value, { immediate: true }).pipe(
+      filter(value => value instanceof HTMLElement),
+      mergeMap(value => fromEventRx(value, event)),
+    )
+  }
+  return fromEventRx(value, event);
 }
