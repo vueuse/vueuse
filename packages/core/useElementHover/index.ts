ef75cc16b7a754581ce3e63877f376f41d3f0d60
林佳涛 <lne128gene980@163.com>
2022-08-12 15:10:37 +0800

    fix(useElementHover): support set el ref type

--- packages/core/useElementHover/index.ts
+++ packages/core/useElementHover/index.ts
@@ -3,7 +3,7 @@ import { ref } from 'vue-demi'
 import type { MaybeComputedRef } from '@vueuse/shared'
 import { useEventListener } from '../useEventListener'
 
-export function useElementHover(el: MaybeComputedRef<EventTarget>): Ref<boolean> {
+export function useElementHover(el: MaybeComputedRef<EventTarget | null | undefined>): Ref<boolean> {
   const isHovered = ref(false)
 
   useEventListener(el, 'mouseenter', () => isHovered.value = true)
