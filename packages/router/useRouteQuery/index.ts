dabf67485c787a462334608608ca5a71655288e9
jeongtae <dev@jeongtae.com>
2022-08-21 00:53:45 +0900

    feat(useRouteQuery): add `preserveHash` option

--- packages/router/useRouteQuery/index.ts
+++ packages/router/useRouteQuery/index.ts
@@ -1,10 +1,20 @@
 import type { Ref } from 'vue-demi'
 import { computed, nextTick, unref } from 'vue-demi'
 import { useRoute, useRouter } from 'vue-router'
+import type { MaybeRef } from '@vueuse/shared'
 import type { ReactiveRouteOptions } from '../_types'
 
+export type UseRouteQueryOptions = ReactiveRouteOptions & {
+  /**
+   * Whether the hash is preserved when the router query is updated, ref is also acceptable
+   *
+   * @default false
+   */
+  preserveHash?: MaybeRef<boolean>
+}
+
 export function useRouteQuery(name: string): Ref<null | string | string[]>
-export function useRouteQuery<T extends null | undefined | string | string[] = null | string | string[]>(name: string, defaultValue?: T, options?: ReactiveRouteOptions): Ref<T>
+export function useRouteQuery<T extends null | undefined | string | string[] = null | string | string[]>(name: string, defaultValue?: T, options?: UseRouteQueryOptions): Ref<T>
 export function useRouteQuery<T extends string | string[]>(
   name: string,
   defaultValue?: T,
@@ -12,7 +22,8 @@ export function useRouteQuery<T extends string | string[]>(
     mode = 'replace',
     route = useRoute(),
     router = useRouter(),
-  }: ReactiveRouteOptions = {},
+    preserveHash = false,
+  }: UseRouteQueryOptions = {},
 ) {
   return computed<any>({
     get() {
@@ -25,7 +36,10 @@ export function useRouteQuery<T extends string | string[]>(
     },
     set(v) {
       nextTick(() => {
-        router[unref(mode)]({ query: { ...route.query, [name]: v === defaultValue || v === null ? undefined : v } })
+        router[unref(mode)]({
+          query: { ...route.query, [name]: v === defaultValue || v === null ? undefined : v },
+          ...(unref(preserveHash) && { hash: route.hash }),
+        })
       })
     },
   })
