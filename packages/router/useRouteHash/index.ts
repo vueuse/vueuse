eb219c7bf326ccb788fff3e52059e3ce824dae27
jeongtae <dev@jeongtae.com>
2022-08-21 00:53:34 +0900

    feat(useRouteHash): add `preserveQuery` option

--- packages/router/useRouteHash/index.ts
+++ packages/router/useRouteHash/index.ts
@@ -1,6 +1,16 @@
 import { computed, nextTick, unref } from 'vue-demi'
 import { useRoute, useRouter } from 'vue-router'
+import type { MaybeRef } from '@vueuse/shared'
 import type { ReactiveRouteOptions } from '../_types'
+
+export type UseRouteHashOptions = ReactiveRouteOptions & {
+  /**
+   * Whether the query is preserved when the router hash is updated, ref is also acceptable
+   *
+   * @default false
+   */
+  preserveQuery?: MaybeRef<boolean>
+}
 
 export function useRouteHash(
   defaultValue?: string,
@@ -8,7 +18,8 @@ export function useRouteHash(
     mode = 'replace',
     route = useRoute(),
     router = useRouter(),
-  }: ReactiveRouteOptions = {},
+    preserveQuery = false,
+  }: UseRouteHashOptions = {},
 ) {
   return computed<string>({
     get() {
@@ -16,7 +27,10 @@ export function useRouteHash(
     },
     set(v) {
       nextTick(() => {
-        router[unref(mode)]({ hash: v })
+        router[unref(mode)]({
+          hash: v,
+          ...(unref(preserveQuery) && { query: route.query }),
+        })
       })
     },
   })
