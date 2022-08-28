a7f2786e3b0341fd2893197cf7158c1ad730fdde
vaakian <vaakian@gmail.com>
2022-08-28 21:21:48 +0800

    fix(useMouseInElement): use `MaybeComputedElementRef`

--- packages/core/useMouseInElement/index.ts
+++ packages/core/useMouseInElement/index.ts
@@ -1,5 +1,6 @@
+import { resolveRef } from '@vueuse/shared'
 import { ref, watch } from 'vue-demi'
-import type { MaybeElementRef } from '../unrefElement'
+import type { MaybeComputedElementRef } from '../unrefElement'
 import { unrefElement } from '../unrefElement'
 import type { UseMouseOptions } from '../useMouse'
 import { useMouse } from '../useMouse'
@@ -17,7 +18,7 @@ export interface MouseInElementOptions extends UseMouseOptions {
  * @param options
  */
 export function useMouseInElement(
-  target?: MaybeElementRef,
+  target?: MaybeComputedElementRef,
   options: MouseInElementOptions = {},
 ) {
   const {
@@ -26,7 +27,11 @@ export function useMouseInElement(
   } = options
   const { x, y, sourceType } = useMouse(options)
 
-  const targetRef = ref(target ?? window?.document.body)
+  const targetRef = ref(
+    target
+      ? resolveRef(target)
+      : window?.document.body,
+  )
   const elementX = ref(0)
   const elementY = ref(0)
   const elementPositionX = ref(0)
