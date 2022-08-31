d3b3e837a7a8732e12859130da1a5116789a664d
azaleta <24407500@qq.com>
2022-08-31 14:43:44 +0800

    chore: reset code formatter

--- packages/core/useBreakpoints/demo.vue
+++ packages/core/useBreakpoints/demo.vue
@@ -16,33 +16,12 @@ const xxxl = breakpoints['2xl']
 
 <template>
   <div class="font-mono">
-    <div>
-      sm(&lt;{{ smWidth }}px):
-      <BooleanDisplay :value="sm" />
-    </div>
-    <div>
-      sm(&lt;={{ smWidth }}px):
-      <BooleanDisplay :value="sme" />
-    </div>
-    <div>
-      md:
-      <BooleanDisplay :value="md" />
-    </div>
-    <div>
-      lg:
-      <BooleanDisplay :value="lg" />
-    </div>
-    <div>
-      xl:
-      <BooleanDisplay :value="xl" />
-    </div>
-    <div>
-      2xl:
-      <BooleanDisplay :value="xxl" />
-    </div>
-    <div>
-      3xl:
-      <BooleanDisplay :value="xxxl" />
-    </div>
+    <div> sm(&lt;{{ smWidth }}px): <BooleanDisplay :value="sm" /></div>
+    <div> sm(&lt;={{ smWidth }}px): <BooleanDisplay :value="sme" /></div>
+    <div> md: <BooleanDisplay :value="md" /></div>
+    <div> lg: <BooleanDisplay :value="lg" /></div>
+    <div> xl: <BooleanDisplay :value="xl" /></div>
+    <div>2xl: <BooleanDisplay :value="xxl" /></div>
+    <div>3xl: <BooleanDisplay :value="xxxl" /></div>
   </div>
 </template>
