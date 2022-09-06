7f7ba1bdbb8200d64746b0820c87767a13be5951
三咲智子 <sxzz@sxzz.moe>
2022-09-06 23:07:52 +0800

    refactor: reactive cleanup

--- packages/core/useMediaQuery/index.ts
+++ packages/core/useMediaQuery/index.ts
@@ -2,7 +2,7 @@
 
 import { ref, watchEffect } from 'vue-demi'
 import type { MaybeComputedRef } from '@vueuse/shared'
-import { resolveRef, tryOnBeforeMount, tryOnScopeDispose } from '@vueuse/shared'
+import { resolveRef, tryOnScopeDispose } from '@vueuse/shared'
 import type { ConfigurableWindow } from '../_configurable'
 import { defaultWindow } from '../_configurable'
 import { useSupported } from '../useSupported'
@@ -16,36 +16,41 @@ import { useSupported } from '../useSupported'
  */
 export function useMediaQuery(query: MaybeComputedRef<string>, options: ConfigurableWindow = {}) {
   const { window = defaultWindow } = options
-  const isSupported = useSupported(() => window && 'matchMedia' in window && typeof window!.matchMedia === 'function')
+  const isSupported = useSupported(() => window && 'matchMedia' in window && typeof window.matchMedia === 'function')
 
   let mediaQuery: MediaQueryList | undefined
   const matches = ref(false)
 
+  const cleanup = () => {
+    if (!mediaQuery)
+      return
+    if ('removeEventListener' in mediaQuery)
+      // eslint-disable-next-line @typescript-eslint/no-use-before-define
+      mediaQuery.removeEventListener('change', update)
+    else
+      // @ts-expect-error deprecated API
+      // eslint-disable-next-line @typescript-eslint/no-use-before-define
+      mediaQuery.removeListener(update)
+  }
+
   const update = () => {
     if (!isSupported.value)
       return
+
+    cleanup()
+
     mediaQuery = window!.matchMedia(resolveRef(query).value)
     matches.value = mediaQuery.matches
-  }
-  watchEffect(update)
-
-  tryOnBeforeMount(() => {
-    if (!mediaQuery)
-      return
 
     if ('addEventListener' in mediaQuery)
       mediaQuery.addEventListener('change', update)
     else
       // @ts-expect-error deprecated API
       mediaQuery.addListener(update)
+  }
+  watchEffect(update)
 
-    tryOnScopeDispose(() => {
-      if ('removeEventListener' in mediaQuery!)
-        mediaQuery!.removeEventListener('change', update)
-      else
-        mediaQuery!.removeListener(update)
-    })
-  })
+  tryOnScopeDispose(() => cleanup())
 
   return matches
 }
