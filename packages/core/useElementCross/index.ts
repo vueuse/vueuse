961dcef76da366949fe2c7bbbafc1d6864ef0611
林佳涛 <lne128gene980@163.com>
2022-09-06 14:44:20 +0800

    feat(useElementCross): add update to refresh state

--- packages/core/useElementCross/index.ts
+++ packages/core/useElementCross/index.ts
@@ -1,10 +1,10 @@
-import { computed } from 'vue-demi'
+import { computed, nextTick } from 'vue-demi'
 import { useElementBounding } from '../useElementBounding'
 import type { MaybeComputedElementRef } from '../unrefElement'
 
 export function useElementCross(...targets: [MaybeComputedElementRef, MaybeComputedElementRef]) {
-  const { top: t0, left: l0, width: w0, height: h0 } = useElementBounding(targets[0])
-  const { top: t1, left: l1, width: w1, height: h1 } = useElementBounding(targets[1])
+  const { top: t0, left: l0, width: w0, height: h0, update: update0 } = useElementBounding(targets[0])
+  const { top: t1, left: l1, width: w1, height: h1, update: update1 } = useElementBounding(targets[1])
 
   const isCross = computed<boolean>(() => {
     return l1.value - l0.value >= -w1.value
@@ -13,8 +13,15 @@ export function useElementCross(...targets: [MaybeComputedElementRef, MaybeCompu
       && t1.value - t0.value <= h0.value
   })
 
+  async function update() {
+    await nextTick()
+    update0()
+    update1()
+  }
+
   return {
     isCross,
+    update,
   }
 }
 
