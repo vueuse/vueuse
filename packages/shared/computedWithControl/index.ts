f00f3a115c074effa6126cb98aa554c8bb335d3e
vaakian <vaakian@gmail.com>
2022-09-09 12:05:36 +0800

    feat: add vu2/vue3 compatibility

--- packages/shared/computedWithControl/index.ts
+++ packages/shared/computedWithControl/index.ts
@@ -13,25 +13,37 @@ export interface ComputedWithControlRefExtra {
 export interface ComputedRefWithControl<T> extends ComputedRef<T>, ComputedWithControlRefExtra {}
 export interface WritableComputedRefWithControl<T> extends WritableComputedRef<T>, ComputedWithControlRefExtra {}
 
-export function computedWithControl<T, S>(
-  source: WatchSource<S> | WatchSource<S>[],
-  fn: ComputedGetter<T>
-): ComputedRefWithControl<T>
+export type ArrayComputedRefWithControl<T> = [ComputedRef<T>, ComputedWithControlRefExtra['trigger']]
+export type ArrayWritableComputedRefWithControl<T> = [WritableComputedRef<T>, ComputedWithControlRefExtra['trigger']]
 
-export function computedWithControl<T, S>(
+export function computedWithControl<T, S, U extends boolean = false>(
   source: WatchSource<S> | WatchSource<S>[],
-  fn: WritableComputedOptions<T>
-): WritableComputedRefWithControl<T>
+  fn: ComputedGetter<T>,
+  separate?: U
+): U extends true
+  ? ArrayComputedRefWithControl<T>
+  : ComputedRefWithControl<T>
+
+export function computedWithControl<T, S, U extends boolean = false>(
+  source: WatchSource<S> | WatchSource<S>[],
+  fn: WritableComputedOptions<T>,
+  separate?: U
+): U extends true
+  ? ArrayWritableComputedRefWithControl<T>
+  : WritableComputedRefWithControl<T>
 
 /**
  * Explicitly define the deps of computed.
  *
  * @param source
  * @param fn
+ * @param separate `true` is both compatible vue2/vue3.
+ * `false` by default for backward usage compatibility.
  */
 export function computedWithControl<T, S>(
   source: WatchSource<S> | WatchSource<S>[],
   fn: ComputedGetter<T> | WritableComputedOptions<T>,
+  separate: false,
 ) {
   let v: T = undefined!
   let track: Fn
@@ -66,6 +78,9 @@ export function computedWithControl<T, S>(
       },
     }
   }) as ComputedRefWithControl<T>
+
+  if (separate)
+    return [result, update] as ArrayComputedRefWithControl<T>
 
   if (Object.isExtensible(result))
     result.trigger = update
