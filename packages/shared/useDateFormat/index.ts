a6cf1d5492d9b13e36fee5c6fb2e9b2ab547864e
webfansplz <308241863@qq.com>
2022-09-24 12:34:10 +0800

    docs(useDateFormat): update desc (#2247)

--- packages/shared/useDateFormat/index.ts
+++ packages/shared/useDateFormat/index.ts
@@ -6,7 +6,7 @@ export type DateLike = Date | number | string | undefined
 
 export interface UseDateFormatOptions {
   /**
-   * The locale(s) to used for dd/ddd/dddd format
+   * The locale(s) to used for dd/ddd/dddd/MMM/MMMM format
    *
    * [MDN](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl#locales_argument).
    */
