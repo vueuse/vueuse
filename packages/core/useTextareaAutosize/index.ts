a00b5bed9a1fefeb996692021cd55e6b0c6e4918
JD Solanki <47495003+jd-solanki@users.noreply.github.com>
2022-10-11 16:47:16 +0530

    feat(useTextareaAutosize): allow detaching styles
    is `detachStyles` is `true` styles won't get applied to the textarea

--- packages/core/useTextareaAutosize/index.ts
+++ packages/core/useTextareaAutosize/index.ts
@@ -11,18 +11,30 @@ export interface UseTextareaAutosizeOptions {
   watch?: WatchSource | Array<WatchSource>
   /** Function called when the textarea size changes. */
   onResize?: () => void
+  /** Prevent attaching styles directly to textarea. Useful for adding styles to wrapper or some other custom element. */
+  detachStyles?: MaybeRef<boolean>
 }
 
 export function useTextareaAutosize(options?: UseTextareaAutosizeOptions) {
   const textarea = ref<HTMLTextAreaElement>(options?.element as any)
   const input = ref<string>(options?.input as any)
+  const textareaScrollHeight = ref(1)
+  const detachStyles = ref(options?.detachStyles || false)
 
   function triggerResize() {
     if (!textarea.value)
       return
 
+    let height = 'unset'
+
     textarea.value!.style.height = '1px'
-    textarea.value!.style.height = `${textarea.value?.scrollHeight}px`
+    textareaScrollHeight.value = textarea.value?.scrollHeight
+
+    // If we don't want to detach style apply scrollHeight to textarea by updating height
+    if (!detachStyles.value)
+      height = `${textareaScrollHeight.value}px`
+
+    textarea.value!.style.height = height
 
     options?.onResize?.()
   }
@@ -34,6 +46,7 @@ export function useTextareaAutosize(options?: UseTextareaAutosizeOptions) {
 
   return {
     textarea,
+    textareaScrollHeight,
     input,
     triggerResize,
   }
