6423a2a8522abf14d6f9c66ff97d18dd6e257669
franciscoemanuel <francisco.emanuel@hotmail.com.br>
2022-11-20 19:39:45 -0300

    docs(useTimer): basic usage

---
+++ packages/core/useTimer/demo.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { useTimer } from '@vueuse/core'
+import { ref } from 'vue-demi'
+
+const initialTime = ref(86400)
+
+const { start, pause, reset, status, timer, hours, minutes, seconds, days } = useTimer(initialTime, () => {
+  initialTime.value += 5
+})
+</script>
+
+<template>
+  <div>
+    <p>UseTimer: {{ timer }}</p>
+    <button :disabled="status === 'RUNNING'" @click="start">
+      start
+    </button>
+    <button @click="pause">
+      pause
+    </button>
+    <button @click="reset">
+      reset
+    </button>
+    <p>Days: {{ days }}</p>
+    <p>Hours: {{ hours }}</p>
+    <p>Minutes: {{ minutes }}</p>
+    <p>Seconds: {{ seconds }}</p>
+    <p>Status: {{ status }}</p>
+  </div>
+</template>
---
+++ packages/core/useTimer/index.md
@@ -0,0 +1,15 @@
+---
+category: Animation
+---
+
+# useTimer
+
+Basic timer function.
+
+## Usage
+
+```ts
+import { useTimer } from '@vueuse/core'
+
+const { timer, start } = useTimer()
+```
