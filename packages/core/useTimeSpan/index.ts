13390458879fbc68db1c6433fb99d629a86049c9
Waleed Khaled <waleed1kh@outlook.com>
2022-11-22 18:42:24 +0400

    formatTimeSpan function

--- packages/core/useTimeSpan/index.ts
+++ packages/core/useTimeSpan/index.ts
@@ -1,8 +1,7 @@
 import { type ComputedRef, computed } from 'vue-demi'
 import { type MaybeComputedRef, resolveRef } from '@vueuse/shared'
 
-const
-  msSecond = 1000
+const msSecond = 1000
 const secMinute = 60
 const minHour = 60
 const hrDay = 24
@@ -24,27 +23,29 @@ export interface TimeSpan {
   formatted: ComputedRef<string>
 }
 
-function format(ts: TimeSpan) {
-  let str = ''
-  const isNegative = (ts.totalMilliseconds.value < 0)
-  const d = Math.abs(ts.days.value)
-  const h = Math.abs(ts.hours.value)
-  const m = Math.abs(ts.minutes.value)
-  const s = Math.abs(ts.seconds.value)
-  const f = Math.abs(ts.milliseconds.value)
+const REGEX_LITERAL = /'([^']+)'/
+const REGEX_SYMBOLS = /[+-]|d+|h{1,2}|m{1,2}|s{1,2}|f{1,3}/
+const REGEX_FORMAT = new RegExp(`${REGEX_LITERAL.source}|${REGEX_SYMBOLS.source}`, 'g')
+const REGEX_OPT_FORMAT = new RegExp(`\\[(?:${REGEX_LITERAL.source})?(${REGEX_SYMBOLS.source})(?:${REGEX_LITERAL.source})?]`, 'g')
 
-  if (isNegative)
-    str += '-'
-  if (d)
-    str += `${d.toString().padStart(2, '0')}.`
-  if (h || d)
-    str += `${h.toString().padStart(2, '0')}:`
-  str += `${m.toString().padStart(2, '0')}:`
-  str += `${s.toString().padStart(2, '0')}`
-  if (f)
-    str += `.${f.toString().slice(0, 2).padEnd(2, '0')}`
+const matches: Record<string, (ts: TimeSpan, pad: number, opt?: boolean) => string> = {
+  '+': (ts, _) => (ts.totalMilliseconds.value >= 0) ? '+' : '-',
+  '-': (ts, _) => (ts.totalMilliseconds.value < 0) ? '-' : '',
+  'd': (ts, pad, opt) => (!opt || Math.abs(ts.totalDays.value) > 1) ? String(Math.abs(ts.days.value)).padStart(pad, '0') : '',
+  'h': (ts, pad, opt) => (!opt || Math.abs(ts.totalHours.value) > 1) ? String(Math.abs(ts.hours.value)).padStart(pad, '0') : '',
+  'm': (ts, pad, opt) => (!opt || Math.abs(ts.totalMinutes.value) > 1) ? String(Math.abs(ts.minutes.value)).padStart(pad, '0') : '',
+  's': (ts, pad, opt) => (!opt || Math.abs(ts.totalSeconds.value) > 1) ? String(Math.abs(ts.seconds.value)).padStart(pad, '0') : '',
+  'f': (ts, pad, opt) => {
+    const ms = String(Math.abs(ts.milliseconds.value)).padStart(3, '0').slice(0, pad)
+    return (!opt || !/^0+$/.test(ms)) ? ms : ''
+  },
+}
 
-  return str
+export const formatTimeSpan = (ts: TimeSpan, formatStr: string) => {
+  return formatStr.replace(REGEX_OPT_FORMAT, (_, $1, $2, $3) => {
+    const f = matches[$2[0]](ts, $2.length, true)
+    return f ? `${$1 ?? ''}${f}${$3 ?? ''}` : ''
+  }).replace(REGEX_FORMAT, (match, $1) => $1 || matches[match[0]](ts, match.length))
 }
 
 /**
@@ -72,7 +73,7 @@ export function useTimeSpan(milliseconds: MaybeComputedRef<number>): TimeSpan {
     minutes: computed(() => Math.trunc(totalMinutes.value) % minHour),
     hours: computed(() => Math.trunc(totalHours.value) % hrDay),
     days: computed(() => Math.trunc(totalDays.value)),
-    formatted: computed(() => format(ts)),
+    formatted: computed(() => formatTimeSpan(ts, '-[d\'.\']hh\':\'mm\':\'ss[\'.\'ff]')),
   }
 
   return ts
