7f4914f0acdc06b7f8f3f358386d09da6728d2cc
Waleed Khaled <waleed1kh@outlook.com>
2022-11-24 11:21:51 +0400

    format TimeSpan escape character

--- packages/core/useTimeSpan/index.ts
+++ packages/core/useTimeSpan/index.ts
@@ -23,14 +23,20 @@ export interface TimeSpan {
   formatted: ComputedRef<string>
 }
 
-const REGEX_LITERAL = /'([^']+)'/
+// This regex will generate 2 groups and only one is defined with literal
+// It could generate one group for both case ('' & \) but unfortunately
+// JS doesn't support Branch Reset Group (?|...) and Safari doesn't
+// support Lookbehind (?<=...)
+// Branch Reset exp => (?|'([^']+)'|\\(.))
+// Lookbehind exp => ((?<=')[^']+(?=')|(?<=\\).)
+const REGEX_LITERAL = /(?:'([^']+)'|\\(.))/
 const REGEX_SYMBOLS = /[+-]|d+|h{1,2}|m{1,2}|s{1,2}|f{1,3}/
 const REGEX_FORMAT = new RegExp(`${REGEX_LITERAL.source}|${REGEX_SYMBOLS.source}`, 'g')
-const REGEX_OPT_FORMAT = new RegExp(`\\[(?:${REGEX_LITERAL.source})?(${REGEX_SYMBOLS.source})(?:${REGEX_LITERAL.source})?]`, 'g')
+const REGEX_OPT_FORMAT = new RegExp(`\\[${REGEX_LITERAL.source}?(${REGEX_SYMBOLS.source})${REGEX_LITERAL.source}?]`, 'g')
 
 const matches: Record<string, (ts: TimeSpan, pad: number, opt?: boolean) => string> = {
-  '+': (ts, _) => (ts.totalMilliseconds.value >= 0) ? '+' : '-',
-  '-': (ts, _) => (ts.totalMilliseconds.value < 0) ? '-' : '',
+  '+': ({ totalMilliseconds: ms }) => (ms.value >= 0) ? '+' : '-',
+  '-': ({ totalMilliseconds: ms }) => (ms.value < 0) ? '-' : '',
   'd': (ts, pad, opt) => (!opt || Math.abs(ts.totalDays.value) > 1) ? String(Math.abs(ts.days.value)).padStart(pad, '0') : '',
   'h': (ts, pad, opt) => (!opt || Math.abs(ts.totalHours.value) > 1) ? String(Math.abs(ts.hours.value)).padStart(pad, '0') : '',
   'm': (ts, pad, opt) => (!opt || Math.abs(ts.totalMinutes.value) > 1) ? String(Math.abs(ts.minutes.value)).padStart(pad, '0') : '',
@@ -42,10 +48,10 @@ const matches: Record<string, (ts: TimeSpan, pad: number, opt?: boolean) => stri
 }
 
 export const formatTimeSpan = (ts: TimeSpan, formatStr: string) => {
-  return formatStr.replace(REGEX_OPT_FORMAT, (_, $1, $2, $3) => {
-    const f = matches[$2[0]](ts, $2.length, true)
-    return f ? `${$1 ?? ''}${f}${$3 ?? ''}` : ''
-  }).replace(REGEX_FORMAT, (match, $1) => $1 || matches[match[0]](ts, match.length))
+  return formatStr.replace(REGEX_OPT_FORMAT, (_, $1, $2, $3, $4, $5) => {
+    const f = matches[$3[0]](ts, $3.length, true)
+    return f ? `${$1 ?? $2 ?? ''}${f}${$4 ?? $5 ?? ''}` : ''
+  }).replace(REGEX_FORMAT, (match, $1, $2) => $1 || $2 || matches[match[0]](ts, match.length))
 }
 
 /**
@@ -73,7 +79,7 @@ export function useTimeSpan(milliseconds: MaybeComputedRef<number>): TimeSpan {
     minutes: computed(() => Math.trunc(totalMinutes.value) % minHour),
     hours: computed(() => Math.trunc(totalHours.value) % hrDay),
     days: computed(() => Math.trunc(totalDays.value)),
-    formatted: computed(() => formatTimeSpan(ts, '-[d\'.\']hh\':\'mm\':\'ss[\'.\'ff]')),
+    formatted: computed(() => formatTimeSpan(ts, '-[d\\.]hh:mm:ss[\\.ff]')),
   }
 
   return ts
