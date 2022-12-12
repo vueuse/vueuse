53e922d0bf9737a3cf1e0eafb84696aaf9ba1911
骆沛 <luopei@11.com>
2022-12-12 11:16:38 +0800

    feat(useCounter): add loop option

--- packages/shared/useCounter/index.ts
+++ packages/shared/useCounter/index.ts
@@ -3,6 +3,7 @@ import { ref } from 'vue-demi'
 export interface UseCounterOptions {
   min?: number
   max?: number
+  loop?: boolean
 }
 
 /**
@@ -18,10 +19,29 @@ export function useCounter(initialValue = 0, options: UseCounterOptions = {}) {
   const {
     max = Infinity,
     min = -Infinity,
+    loop = false,
   } = options
 
-  const inc = (delta = 1) => count.value = Math.min(max, count.value + delta)
-  const dec = (delta = 1) => count.value = Math.max(min, count.value - delta)
+  const inc = (delta = 1) => {
+    if (loop) {
+      if (count.value === max)
+        count.value = initialValue
+
+      else
+        count.value = Math.min(max, count.value + delta)
+    }
+    else { count.value = Math.min(max, count.value + delta) }
+  }
+  const dec = (delta = 1) => {
+    if (loop) {
+      if (count.value === min)
+        count.value = initialValue
+
+      else
+        count.value = Math.max(min, count.value - delta)
+    }
+    else { count.value = Math.max(min, count.value - delta) }
+  }
   const get = () => count.value
   const set = (val: number) => (count.value = Math.max(min, Math.min(max, val)))
   const reset = (val = initialValue) => {
