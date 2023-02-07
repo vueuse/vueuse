2bd17116097dc9c3538de6651e51e70ae02e3f9d
Admin <Admin@PF25TD71-PHW.inc.bytedance.com>
2023-02-07 16:42:09 +0800

    (WIP)feat(usePerformanceRecorder): new function

--- packages/core/usePerformanceRecorder/index.ts
+++ packages/core/usePerformanceRecorder/index.ts
@@ -1,6 +1,12 @@
-import { computed, reactive } from 'vue-demi'
+import { reactive } from 'vue-demi'
 
 interface recordInfoType {
+  TTI: any[]
+  LCP?: number | undefined
+  FCP?: number | undefined
+  CLS?: number
+  FP?: number | undefined
+  FLP?: number | undefined
   lookup?: number | undefined
   receiving?: number | undefined
   parsing?: number | undefined
@@ -11,6 +17,7 @@ interface recordInfoType {
   appcache?: number | undefined
   unloadEvent?: number | undefined
   connect?: number | undefined
+  [key: string]: any
 }
 /**
  * Reactive state to show whether mouse leaves the page.
@@ -18,11 +25,16 @@ interface recordInfoType {
  * @see https://vueuse.org/usePerformanceRecorder
  * @param options
  */
+window.__vueuse__observer = null
 
 export function usePerformanceRecorder() {
-  const referTiming = reactive(performance.timing)
-  const recorderInfo = computed(() => {
-    const detail: recordInfoType = {}
+  const detail = reactive<recordInfoType>({
+    TTI: [],
+    LCP: 0,
+    CLS: 0,
+  })
+  function computPerformanceInfo(entry: PerformanceEntry) {
+    const referTiming = performance.timing
     detail.lookup = referTiming.domainLookupEnd - referTiming.domainLookupStart
     detail.receiving = referTiming.responseEnd - referTiming.requestStart
     detail.parsing = referTiming.domComplete - referTiming.domLoading
@@ -33,8 +45,21 @@ export function usePerformanceRecorder() {
     detail.appcache = referTiming.domainLookupStart - referTiming.fetchStart
     detail.unloadEvent = referTiming.unloadEventEnd - referTiming.unloadEventStart
     detail.connect = referTiming.connectEnd - referTiming.connectStart
-    return detail
+    if (entry.name === 'first-paint')
+      detail.FP = entry.startTime
+    else if (entry.name === 'first-contentful-paint')
+      detail.FCP = entry.startTime
+    else if (entry.entryType === 'longtask')
+      detail.TTI.push(entry)
+    else if (entry.entryType === 'layout-shift')
+      detail.CLS += entry?.value
+    else if (entry.entryType === 'largest-contentful-paint')
+      detail.LCP = entry.renderTime || entry.startTime
+  }
+  window._vueuse__performance__observer = new PerformanceObserver((list) => {
+    for (const entry of list.getEntries())
+      computPerformanceInfo(entry)
   })
-
-  return recorderInfo
+  window._vueuse__performance__observer.observe({ entryTypes: ['longtask', 'frame', 'navigation', 'measure', 'paint', 'largest-contentful-paint', 'layout-shift'] })
+  return detail
 }
