890112be4b83d8cbf74f58131e9ff4440486658a
付志强 <zhiqiangfu6@gmail.com>
2023-02-10 15:07:30 +0800

    chore(test): replace toEqual with toBe for boolean results

--- packages/shared/useTimeout/index.test.ts
+++ packages/shared/useTimeout/index.test.ts
@@ -3,13 +3,13 @@ import { useTimeout } from '../useTimeout'
 describe('useTimeout', () => {
   it('works', () => {
     const ready = useTimeout(10)
-    expect(ready.value).toEqual(false)
-    setTimeout(() => expect(ready.value).toEqual(true), 10)
+    expect(ready.value).toBe(false)
+    setTimeout(() => expect(ready.value).toBe(true), 10)
   })
 
   it('works with controls', () => {
     const { ready } = useTimeout(10, { controls: true })
-    expect(ready.value).toEqual(false)
-    setTimeout(() => expect(ready.value).toEqual(true), 10)
+    expect(ready.value).toBe(false)
+    setTimeout(() => expect(ready.value).toBe(true), 10)
   })
 })
