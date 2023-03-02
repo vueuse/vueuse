8bb676f9d4f6f382c4093f4ec0bcf28afc94fbbf
Anthony Fu <anthonyfu117@hotmail.com>
2023-03-02 12:57:39 +0100

    chore: update

--- packages/shared/createGlobalState/index.ts
+++ packages/shared/createGlobalState/index.ts
@@ -1,25 +1,23 @@
 import { effectScope } from 'vue-demi'
 
-export type CreateGlobalStateReturn<T> = (...args: any[]) => T
-
 /**
  * Keep states in the global scope to be reusable across Vue instances.
  *
  * @see https://vueuse.org/createGlobalState
  * @param stateFactory A factory function to create the state
  */
-export function createGlobalState<T>(
-  stateFactory: (...args: any[]) => T,
-): CreateGlobalStateReturn<T> {
+export function createGlobalState<Fn extends (...args: any[]) => any>(
+  stateFactory: Fn,
+): Fn {
   let initialized = false
-  let state: T
+  let state: any
   const scope = effectScope(true)
 
-  return (...args: any[]) => {
+  return ((...args: any[]) => {
     if (!initialized) {
       state = scope.run(() => stateFactory(...args))!
       initialized = true
     }
     return state
-  }
+  }) as Fn
 }
