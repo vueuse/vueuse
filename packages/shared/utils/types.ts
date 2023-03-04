c2fee4e5f9273c02d559b2577ff67ab9dc225ce5
丶远方 <yangpanteng@gmail.com>
2023-03-04 20:51:15 +0800

    refactor!: remove deprecated types (#2656)

--- packages/shared/utils/types.ts
+++ packages/shared/utils/types.ts
@@ -19,11 +19,6 @@ export type RemovableRef<T> = Omit<Ref<T>, 'value'> & {
 }
 
 /**
- * @deprecated Use `RemovableRef`
- */
-export type RemoveableRef<T> = RemovableRef<T>
-
-/**
  * Maybe it's a ref, or a plain value
  *
  * ```ts
@@ -113,11 +108,6 @@ export interface Stoppable<StartFnArgs extends any[] = any[]> {
    */
   start: (...args: StartFnArgs) => void
 }
-
-/**
- * @deprecated Use `Stoppable`
- */
-export type Stopable = Stoppable
 
 export interface ConfigurableFlush {
   /**
