6aaa66e2a3c50dc46968a46faa0dd0786225c9c6
Anthony Fu <anthonyfu117@hotmail.com>
2023-03-10 11:01:22 +0100

    docs: hide banner in iframe

--- packages/.vitepress/theme/index.ts
+++ packages/.vitepress/theme/index.ts
@@ -15,7 +15,8 @@ export default {
   },
 }
 
-if (typeof window !== 'undefined') {
+// Vue School banner, skip on SSG or in an iframe
+if (typeof window !== 'undefined' && window.self === window.top) {
   const script = document.createElement('script')
   script.async = true
   script.src = 'https://vueschool.io/banner.js?affiliate=vueuse&type=bottom'
