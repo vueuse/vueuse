d9a6769ede23b4d7d3cc0c81cceb61dafcb08f2b
丶远方 <yangpanteng@gmail.com>
2023-03-22 19:24:05 +0800

    refactor: refactor error to shallowRef

--- packages/core/useAsyncState/index.ts
+++ packages/core/useAsyncState/index.ts
@@ -91,7 +91,7 @@ export function useAsyncState<Data, Params extends any[] = [], Shallow extends b
   const state = shallow ? shallowRef(initialState) : ref(initialState)
   const isReady = ref(false)
   const isLoading = ref(false)
-  const error = ref<unknown | undefined>(undefined)
+  const error = shallowRef<unknown | undefined>(undefined)
 
   async function execute(delay = 0, ...args: any[]) {
     if (resetOnExecute)
