837f88d36a24cfb4d5a5a4e5654d7644f47bc67b
Fernando Fernández <ferferga@hotmail.com>
2023-03-24 10:52:03 +0000

    refactor(useSupported): address review comments

--- packages/core/useSupported/index.ts
+++ packages/core/useSupported/index.ts
@@ -3,7 +3,13 @@ import { useMounted } from '../useMounted'
 
 export function useSupported(callback: () => unknown) {
   const isMounted = useMounted()
-  const callbackResult = computed(callback)
 
-  return computed(() => (isMounted.value && Boolean(callbackResult.value)) || Boolean(callbackResult.value))
+  return computed(() => {
+    /**
+     * Although we're sure isMounted.value will never be undefined,
+     * we want to track changes to it regardless its value (for SSR compat),
+     * so it must be in the body of computed's callback.
+     */
+    return isMounted.value !== undefined ? Boolean(callback()) : false
+  })
 }
