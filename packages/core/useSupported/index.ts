23eaf4653e1a0543d255dcd7c6f3ee6bedd901f4
Anthony Fu <anthonyfu117@hotmail.com>
2023-03-28 11:17:51 +0200

    chore: update

--- packages/core/useSupported/index.ts
+++ packages/core/useSupported/index.ts
@@ -3,5 +3,11 @@ import { useMounted } from '../useMounted'
 
 export function useSupported(callback: () => unknown) {
   const isMounted = useMounted()
-  return computed(() => isMounted.value && Boolean(callback()))
+
+  return computed(() => {
+    // to trigger the ref
+    // eslint-disable-next-line no-unused-expressions
+    isMounted.value
+    return Boolean(callback())
+  })
 }
