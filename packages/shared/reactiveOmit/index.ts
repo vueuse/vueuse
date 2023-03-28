2e297db77d91fc1abccf0be0f43db1e398cab79b
丶远方 <yangpanteng@gmail.com>
2023-03-28 18:12:09 +0800

    feat(reactiveOmit): add predicate parameter (#2849)
    Co-authored-by: Anthony Fu <anthonyfu117@hotmail.com>

--- packages/shared/reactiveOmit/index.ts
+++ packages/shared/reactiveOmit/index.ts
@@ -1,6 +1,17 @@
 import { toRefs } from 'vue-demi'
+import { resolveUnref } from '../resolveUnref'
 import { reactiveComputed } from '../reactiveComputed'
 
+export type ReactiveOmitPredicate<T> = (value: T[keyof T], key: keyof T) => boolean
+
+export function reactiveOmit<T extends object, K extends keyof T>(
+  obj: T,
+  ...keys: (K | K[])[]
+): Omit<T, K>
+export function reactiveOmit<T extends object>(
+  obj: T,
+  predicate: ReactiveOmitPredicate<T>,
+): Partial<T>
 /**
  * Reactively omit fields from a reactive object
  *
@@ -11,5 +22,10 @@ export function reactiveOmit<T extends object, K extends keyof T>(
   ...keys: (K | K[])[]
 ): Omit<T, K> {
   const flatKeys = keys.flat() as K[]
-  return reactiveComputed<any>(() => Object.fromEntries(Object.entries(toRefs(obj)).filter(e => !flatKeys.includes(e[0] as any))))
+  const predicate = flatKeys[0] as unknown as ReactiveOmitPredicate<T>
+  return reactiveComputed<any>(() =>
+    typeof predicate === 'function'
+      ? Object.fromEntries(Object.entries(toRefs(obj)).filter(([k, v]) => !predicate(resolveUnref(v) as T[K], k as K)))
+      : Object.fromEntries(Object.entries(toRefs(obj)).filter(e => !flatKeys.includes(e[0] as any))),
+  )
 }
