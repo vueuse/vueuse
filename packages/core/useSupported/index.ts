7cb78e41ce4595aaab17ebfc72a599dd5f46165d
Anthony Fu <anthonyfu117@hotmail.com>
2023-03-28 11:15:55 +0200

    chore: update

--- packages/core/useSupported/index.ts
+++ packages/core/useSupported/index.ts
@@ -3,13 +3,5 @@ import { useMounted } from '../useMounted'
 
 export function useSupported(callback: () => unknown) {
   const isMounted = useMounted()
-
-  return computed(() => {
-    /**
-     * Although we're sure isMounted.value will never be undefined,
-     * we want to track changes to it regardless its value (for SSR compat),
-     * so it must be in the body of computed's callback.
-     */
-    return isMounted.value !== undefined ? Boolean(callback()) : false
-  })
+  return computed(() => isMounted.value && Boolean(callback()))
 }
