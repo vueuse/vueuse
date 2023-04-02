307ff06556db41c1b72d6cb00baeda966fe13ab1
丶远方 <yangpanteng@gmail.com>
2023-04-02 20:11:05 +0800

    fix(resolveRef): fix overload signature

--- packages/shared/resolveRef/index.ts
+++ packages/shared/resolveRef/index.ts
@@ -1,13 +1,12 @@
 import type { ComputedRef, Ref } from 'vue-demi'
 import { computed, ref } from 'vue-demi'
-import type { MaybeComputedRef, MaybeRef } from '../utils'
+import type { MaybeComputedRef, MaybeReadonlyRef, MaybeRef } from '../utils'
 
 /**
  * Normalize value/ref/getter to `ref` or `computed`.
  */
-export function resolveRef<T>(r: MaybeComputedRef<T>): ComputedRef<T>
+export function resolveRef<T>(r: MaybeReadonlyRef<T>): ComputedRef<T>
 export function resolveRef<T>(r: MaybeRef<T>): Ref<T>
-export function resolveRef<T>(r: T): Ref<T>
 export function resolveRef<T>(r: MaybeComputedRef<T>) {
   return typeof r === 'function'
     ? computed<T>(r as any)
