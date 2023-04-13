f307ff77d16a54b1f3aad13e01161f9470645757
Anthony Fu <anthonyfu117@hotmail.com>
2023-04-13 09:52:20 +0200

    feat(toRef): support two argument overrides from Vue

--- packages/shared/toRef/index.ts
+++ packages/shared/toRef/index.ts
@@ -1,6 +1,7 @@
-import type { ComputedRef, Ref } from 'vue-demi'
-import { computed, ref } from 'vue-demi'
+import type { ComputedRef, Ref, ToRef } from 'vue-demi'
+import { customRef, ref, toRef as vueToRef } from 'vue-demi'
 import type { MaybeComputedRef, MaybeRef } from '../utils'
+import { noop } from '../utils'
 
 /**
  * Normalize value/ref/getter to `ref` or `computed`.
@@ -8,9 +9,14 @@ import type { MaybeComputedRef, MaybeRef } from '../utils'
 export function toRef<T>(r: MaybeComputedRef<T>): ComputedRef<T>
 export function toRef<T>(r: MaybeRef<T>): Ref<T>
 export function toRef<T>(r: T): Ref<T>
-export function toRef<T>(r: MaybeComputedRef<T>) {
+export function toRef<T extends object, K extends keyof T>(object: T, key: K): ToRef<T[K]>
+export function toRef<T extends object, K extends keyof T>(object: T, key: K, defaultValue: T[K]): ToRef<Exclude<T[K], undefined>>
+export function toRef<T>(...args: any[]) {
+  if (args.length !== 1)
+    return vueToRef(...args as [any, any])
+  const r = args[0]
   return typeof r === 'function'
-    ? computed<T>(r as any)
+    ? customRef(() => ({ get: r as any, set: noop }))
     : ref(r)
 }
 
