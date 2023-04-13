2a912d8e68fdd219deec17296c7b303b9856cc92
Anthony Fu <anthonyfu117@hotmail.com>
2023-04-13 22:38:05 +0200

    chore: improve tree shaking

--- packages/shared/extendRef/index.ts
+++ packages/shared/extendRef/index.ts
@@ -1,6 +1,5 @@
 import type { Ref, ShallowUnwrapRef } from 'vue-demi'
-import { isRef } from 'vue-demi'
-import { __onlyVue27Plus } from '../utils/compatibility'
+import { isRef, isVue3, version } from 'vue-demi'
 
 export interface ExtendRefOptions<Unwrap extends boolean = boolean> {
   /**
@@ -29,7 +28,12 @@ export function extendRef<R extends Ref<any>, Extend extends object, Options ext
 
 // implementation
 export function extendRef<R extends Ref<any>, Extend extends object>(ref: R, extend: Extend, { enumerable = false, unwrap = true }: ExtendRefOptions = {}) {
-  __onlyVue27Plus()
+  // compatibility: Vue 2.7 or above
+  if (!isVue3 || !version.startsWith('2.7.')) {
+    if (process.env.NODE_ENV !== 'production')
+      throw new Error('[VueUse] extendRef is only works on Vue 2.7 or above.')
+    return
+  }
 
   for (const [key, value] of Object.entries(extend)) {
     if (key === 'value')
