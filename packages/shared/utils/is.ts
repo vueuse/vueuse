6989fc358949d6709e22f1f42e3574bd04e50c2e
btea <2356281422@qq.com>
2023-04-14 16:07:00 +0800

    feat(rand): add isInteger parameter

--- packages/shared/utils/is.ts
+++ packages/shared/utils/is.ts
@@ -18,10 +18,11 @@ export const now = () => Date.now()
 export const timestamp = () => +Date.now()
 export const clamp = (n: number, min: number, max: number) => Math.min(max, Math.max(min, n))
 export const noop = () => {}
-export const rand = (min: number, max: number) => {
+export const rand = (min: number, max: number, isInteger = true) => {
   min = Math.ceil(min)
   max = Math.floor(max)
-  return Math.floor(Math.random() * (max - min + 1)) + min
+  const v = Math.random() * (max - min + 1) + min
+  return isInteger ? Math.floor(v) : v
 }
 export const hasOwn = <T extends object, K extends keyof T>(val: T, key: K): key is K => Object.prototype.hasOwnProperty.call(val, key)
 
