0d9e2acce7f313781bc95005dedd5bab6265ccae
RAX7 <antoxa2013@gmail.com>
2023-04-18 12:42:42 +0700

    feat(useMouse): add `target` option and `type` enhancement
    fix

--- packages/core/useMouse/demo.vue
+++ packages/core/useMouse/demo.vue
@@ -1,12 +1,26 @@
 <script setup lang="ts">
 import { reactive } from 'vue'
 import { stringify } from '@vueuse/docs-utils'
-import { useMouse } from '@vueuse/core'
+import { type UseMouseExtractFn, useMouse, useParentElement } from '@vueuse/core'
 
-const mouse = reactive(useMouse())
-const text = stringify(mouse)
+const parentEl = useParentElement()
+
+const mouseDefault = reactive(useMouse())
+const textDefault = stringify(mouseDefault)
+
+const extractor: UseMouseExtractFn = event => (
+  event instanceof Touch
+    ? null
+    : { x: event.offsetX, y: event.offsetY }
+)
+
+const mouseWithExtractor = reactive(useMouse({ target: parentEl, type: extractor }))
+const textWithExtractor = stringify(mouseWithExtractor)
 </script>
 
 <template>
-  <pre lang="yaml">{{ text }}</pre>
+  <p>Basic Usage</p>
+  <pre lang="yaml">{{ textDefault }}</pre>
+  <p>Extractor Usage</p>
+  <pre lang="yaml">{{ textWithExtractor }}</pre>
 </template>
--- packages/core/useMouse/index.md
+++ packages/core/useMouse/index.md
@@ -21,6 +21,29 @@ The `dragover` event is used to track mouse position while dragging.
 const { x, y } = useMouse({ touch: false })
 ```
 
+## Usage With Extractor
+
+```js
+import { type UseMouseExtractFn, useMouse, useParentElement } from '@vueuse/core'
+
+const parentEl = useParentElement()
+
+const extractor: UseMouseExtractFn = event => (
+  event instanceof Touch
+    ? null
+    : { x: event.offsetX, y: event.offsetY }
+)
+
+const { x, y, sourceType } = useMouse({ target: parentEl, type: extractor })
+```
+
+Touch is enabled by default. To only detect mouse changes, set `touch` to `false`.
+The `dragover` event is used to track mouse position while dragging.
+
+```js
+const { x, y } = useMouse({ touch: false })
+```
+
 ## Component Usage
 
 ```html
