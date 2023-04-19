8f6a0c5c76ed4f074fa87a61e0cbb1c2c139c933
Alex Liu <dsa1314@gmail.com>
2023-04-20 02:44:45 +0800

    feat(watch): `watchImmediate` and `watchDeep` support overloads (#2998)

--- packages/shared/watchDeep/index.ts
+++ packages/shared/watchDeep/index.ts
@@ -1,14 +1,41 @@
-import type { WatchCallback, WatchOptions, WatchSource } from 'vue-demi'
+import type { WatchCallback, WatchOptions, WatchSource, WatchStopHandle } from 'vue-demi'
 import { watch } from 'vue-demi'
+
+import type { MapOldSources, MapSources, MultiWatchSources } from '../utils/types'
+
+// overloads
+export function watchDeep<
+  T extends Readonly<MultiWatchSources>,
+  Immediate extends Readonly<boolean> = false,
+>(
+  source: T,
+  cb: WatchCallback<MapSources<T>, MapOldSources<T, Immediate>>,
+  options?: Omit<WatchOptions<Immediate>, 'deep'>
+): WatchStopHandle
+
+export function watchDeep<T, Immediate extends Readonly<boolean> = false>(
+  source: WatchSource<T>,
+  cb: WatchCallback<T, Immediate extends true ? T | undefined : T>,
+  options?: Omit<WatchOptions<Immediate>, 'deep'>
+): WatchStopHandle
+
+export function watchDeep<
+  T extends object,
+  Immediate extends Readonly<boolean> = false,
+>(
+  source: T,
+  cb: WatchCallback<T, Immediate extends true ? T | undefined : T>,
+  options?: Omit<WatchOptions<Immediate>, 'deep'>
+): WatchStopHandle
 
 /**
  * Shorthand for watching value with {deep: true}
  *
  * @see https://vueuse.org/watchDeep
  */
-export function watchDeep<T>(source: WatchSource<T>, cb: WatchCallback<T>, options?: Omit<WatchOptions, 'deep'>) {
+export function watchDeep<T = any, Immediate extends Readonly<boolean> = false>(source: T | WatchSource<T>, cb: any, options?: Omit<WatchOptions<Immediate>, 'deep'>) {
   return watch(
-    source,
+    source as any,
     cb,
     {
       ...options,
