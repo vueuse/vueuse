d1fbbaa59d713b9022c0501b96b0bf7ba179d288
François M <francoism90@users.noreply.github.com>
2023-04-24 17:27:45 +0200

    Add image properties

--- packages/core/useImage/index.ts
+++ packages/core/useImage/index.ts
@@ -11,18 +11,37 @@ export interface UseImageOptions {
   srcset?: string
   /** Image sizes for different page layouts */
   sizes?: string
+  /** Image alternative information */
+  alt?: string
+  /** Image classes */
+  class?: string
+  /** Image loading */
+  loading?: string (lazy, use-credentials)
+  /** Image CORS settings */
+  crossorigin?: string
 }
 
 async function loadImage(options: UseImageOptions): Promise<HTMLImageElement> {
   return new Promise((resolve, reject) => {
     const img = new Image()
-    const { src, srcset, sizes } = options
+    const { src, srcset, sizes, class, loading, crossorigin } = options
 
     img.src = src
+  
     if (srcset)
       img.srcset = srcset
+  
     if (sizes)
       img.sizes = sizes
+  
+    if (class)
+      img.class = class
+  
+    if (loading)
+      img.loading = loading
+  
+    if (crossorigin)
+      img.crossorigin = crossorigin
 
     img.onload = () => resolve(img)
     img.onerror = reject
