0db591065f5e0da40e9324a438fe020d77a6cc1e
chenjiezi <892774940@qq.com>
2023-04-25 16:53:33 +0800

    fix(usePrecision): calculation accuracy issues

--- packages/math/usePrecision/index.ts
+++ packages/math/usePrecision/index.ts
@@ -3,6 +3,25 @@ import { computed } from 'vue-demi'
 import type { MaybeRefOrGetter } from '@vueuse/shared'
 import { toValue } from '@vueuse/shared'
 
+/**
+ * Accuracy of handling numerical values.
+ *
+ * @param value - The value
+ * @param power - The power
+ * @returns The result of multiplying the value with the power
+ */
+export function handleMultiplicationAccuracy(value: number, power: number): number {
+  const valueStr = value.toString()
+  let decimalPlaces = 0
+
+  if (value > 0 && valueStr.includes('.'))
+    decimalPlaces = valueStr.split('.')[1].length
+
+  const multiplier = 10 ** decimalPlaces ?? 1
+
+  return (value * multiplier * power) / multiplier
+}
+
 export interface UsePrecisionOptions {
   /**
    * Method to use for rounding
@@ -26,6 +45,6 @@ export function usePrecision(
     const _value = toValue(value)
     const _digits = toValue(digits)
     const power = 10 ** _digits
-    return Math[toValue(options)?.math || 'round'](_value * power) / power
+    return Math[toValue(options)?.math || 'round'](handleMultiplicationAccuracy(_value, power)) / power
   })
 }
