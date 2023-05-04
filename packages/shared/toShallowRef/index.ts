e2cae147703b4a277eb454b154c2bfef9b1a41ec
Trevor Profitt <trevor@stemble.ca>
2023-05-04 10:52:31 -0300

    feat(toShallowRef): add toShallowRef

---
+++ packages/shared/toShallowRef/index.ts
@@ -0,0 +1,20 @@
+import type { ComputedRef, Ref, ShallowRef } from 'vue-demi'
+import {
+  isRef,
+  shallowRef,
+} from 'vue-demi'
+import type { MaybeRefOrGetter } from '../utils'
+import { toRef } from '../'
+
+/**
+ * Normalize value/ref/getter to `shallowRef` or `computed`.
+ */
+export function toShallowRef<T>(r: () => T): Readonly<Ref<T>>
+export function toShallowRef<T>(r: ComputedRef<T>): ComputedRef<T>
+export function toShallowRef<T>(r: Ref<T>): Ref<T>
+export function toShallowRef<T>(r: T): ShallowRef<T>
+export function toShallowRef<T>(r: MaybeRefOrGetter<T>) {
+  return (isRef(r) || typeof r === 'function')
+    ? toRef(r)
+    : shallowRef(r)
+}
