d1c5f4fdcd8f187b3bfdae9909dc547de29fb21a
Trevor Profitt <trevor@stemble.ca>
2023-05-17 10:26:46 -0300

    test(useEventBus): add tests for unprovided key

--- packages/core/useEventBus/index.test.ts
+++ packages/core/useEventBus/index.test.ts
@@ -10,106 +10,111 @@ describe('useEventBus', () => {
   beforeEach(() => {
     events.clear()
   })
-  it('on event and off listener', () => {
-    const { on, off } = useEventBus<number>('foo')
-    const { inc } = useCounter(0)
-    on(inc)
-    off(inc)
-    expect(events).toEqual(emptyMap)
-  })
-  it('on event', () => {
-    let event = false
-    const { emit, on, reset } = useEventBus<boolean>('on-event')
-    on((_event) => {
-      event = _event
+
+  describe.each(['with', 'without'])('%s key', (withOrWithoutKey) => {
+    const buildKey = (key: any) => withOrWithoutKey === 'with' ? key : undefined
+
+    it('on event and off listener', () => {
+      const { on, off } = useEventBus<number>(buildKey('foo'))
+      const { inc } = useCounter(0)
+      on(inc)
+      off(inc)
+      expect(events).toEqual(emptyMap)
     })
-    emit(true)
-    expect(event).toBe(true)
-    reset()
-    expect(events).toEqual(emptyMap)
-  })
-  it('once event', () => {
-    const { once, emit, reset } = useEventBus<number>('foo')
-    const { inc, count } = useCounter(0)
-    once(inc)
-    emit()
-    emit()
-    emit()
-    expect(count.value).toBe(1)
-    reset()
-    expect(events).toEqual(emptyMap)
-  })
-  it('useEventBus off event', () => {
-    const { emit, on, reset } = useEventBus<number>('useEventBus-off')
-    const { count, inc } = useCounter(0)
-    on(inc)
-
-    emit()
-    reset()
-
-    on(inc)
-
-    emit()
-    reset()
-
-    expect(count.value).toBe(2)
-    expect(events).toEqual(emptyMap)
-  })
-  it('event off event', () => {
-    const event1 = useEventBus<number>('event-off-1')
-    const event2 = useEventBus<number>('event-off-2')
-    const { count, inc } = useCounter(0)
-    event2.on(inc)
-    event1.emit() // 1
-    event2.emit() // 1
-
-    event1.reset()
-
-    event2.on(inc)
-    event1.emit() // 2
-    event2.emit() // 2
-
-    event1.reset()
-    event2.reset()
-
-    expect(count.value).toBe(2)
-    expect(events).toEqual(emptyMap)
-  })
-
-  it('setup unmount off', async () => {
-    const vm = useSetup(() => {
-      const { on } = useEventBus('setup-unmount')
-      on(() => {})
-      on(() => {})
-      on(() => {})
-      on(() => {})
-      on(() => {})
+    it('on event', () => {
+      let event = false
+      const { emit, on, reset } = useEventBus<boolean>(buildKey('on-event'))
+      on((_event) => {
+        event = _event
+      })
+      emit(true)
+      expect(event).toBe(true)
+      reset()
+      expect(events).toEqual(emptyMap)
     })
-    expect(events).not.toEqual(emptyMap)
-    vm.unmount()
-    await nextTick()
-    expect(events).toEqual(emptyMap)
-  })
+    it('once event', () => {
+      const { once, emit, reset } = useEventBus<number>(buildKey('foo'))
+      const { inc, count } = useCounter(0)
+      once(inc)
+      emit()
+      emit()
+      emit()
+      expect(count.value).toBe(1)
+      reset()
+      expect(events).toEqual(emptyMap)
+    })
+    it('useEventBus off event', () => {
+      const { emit, on, reset } = useEventBus<number>(buildKey('useEventBus-off'))
+      const { count, inc } = useCounter(0)
+      on(inc)
 
-  it('should work with payload', async () => {
-    const { on, emit } = useEventBus<'inc' | 'dec', number>('counter')
-    const counter = useCounter(0)
-    on((event, payload) => counter[event](payload))
-    emit('inc', 3)
-    expect(counter.count.value).toBe(3)
-    emit('dec', 1)
-    expect(counter.count.value).toBe(2)
-  })
+      emit()
+      reset()
 
-  it('the same key, the same listener, will only be triggered once', () => {
-    const listener = vi.fn()
-    const { on, emit, off } = useEventBus<'inc' | 'dec', number>('counter')
-    on(listener)
-    on(listener)
-    emit()
-    off(listener)
-    expect(listener).toBeCalledTimes(1)
-    expect(events).toEqual(emptyMap)
+      on(inc)
+
+      emit()
+      reset()
+
+      expect(count.value).toBe(2)
+      expect(events).toEqual(emptyMap)
+    })
+    it('event off event', () => {
+      const event1 = useEventBus<number>(buildKey('event-off-1'))
+      const event2 = useEventBus<number>(buildKey('event-off-2'))
+      const { count, inc } = useCounter(0)
+      event2.on(inc)
+      event1.emit() // 1
+      event2.emit() // 1
+
+      event1.reset()
+
+      event2.on(inc)
+      event1.emit() // 2
+      event2.emit() // 2
+
+      event1.reset()
+      event2.reset()
+
+      expect(count.value).toBe(2)
+      expect(events).toEqual(emptyMap)
+    })
+
+    it('setup unmount off', async () => {
+      const vm = useSetup(() => {
+        const { on } = useEventBus(buildKey('setup-unmount'))
+        on(() => {})
+        on(() => {})
+        on(() => {})
+        on(() => {})
+        on(() => {})
+      })
+      expect(events).not.toEqual(emptyMap)
+      vm.unmount()
+      await nextTick()
+      expect(events).toEqual(emptyMap)
+    })
+
+    it('should work with payload', async () => {
+      const { on, emit } = useEventBus<'inc' | 'dec', number>(buildKey('counter'))
+      const counter = useCounter(0)
+      on((event, payload) => counter[event](payload))
+      emit('inc', 3)
+      expect(counter.count.value).toBe(3)
+      emit('dec', 1)
+      expect(counter.count.value).toBe(2)
+    })
+
+    it('the same key, the same listener, will only be triggered once', () => {
+      const listener = vi.fn()
+      const { on, emit, off } = useEventBus<'inc' | 'dec', number>(buildKey('counter'))
+      on(listener)
+      on(listener)
+      emit()
+      off(listener)
+      expect(listener).toBeCalledTimes(1)
+      expect(events).toEqual(emptyMap)
+    })
   })
 
   describe('off ignores non-existent', () => {
