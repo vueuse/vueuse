13ffb350f37ce27e70f71b1944c5e43185f63d82
Jason Liang <lainbo@foxmail.com>
2023-05-31 22:05:49 +0800

    style(useColorMode): minify css string

--- packages/core/useColorMode/index.ts
+++ packages/core/useColorMode/index.ts
@@ -156,17 +156,7 @@ export function useColorMode<T extends string = BasicColorMode>(
       let style: HTMLStyleElement | undefined
       if (disableTransition) {
         style = window!.document.createElement('style')
-        const styleString = `
-          *,
-          *::before,
-          *::after {
-            -webkit-transition: none !important;
-            -moz-transition: none !important;
-            -o-transition: none !important;
-            -ms-transition: none !important;
-            transition: none !important;
-          }
-        `
+        const styleString = '*,*::before,*::after{-webkit-transition:none!important;-moz-transition:none!important;-o-transition:none!important;-ms-transition:none!important;transition:none!important}'
         style.appendChild(document.createTextNode(styleString))
         window!.document.head.appendChild(style)
       }
