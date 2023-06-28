98567af92563ce4506b4d0d90af99cf8f9c68fc2
Anthony Fu <anthonyfu117@hotmail.com>
2023-06-28 11:22:49 +0200

    chore: try fix tests

--- packages/.test/polyfillFetch.ts
+++ packages/.test/polyfillFetch.ts
@@ -1,6 +1,9 @@
 import nodeFetch from 'node-fetch'
 
+// @ts-expect-error remove XMLHttpRequest from global scope to prevent it from being used in tests
+globalThis.XMLHttpRequest = undefined
+
 // @ts-expect-error override
-window.fetch = nodeFetch
+globalThis.fetch = nodeFetch
 
 export {}
