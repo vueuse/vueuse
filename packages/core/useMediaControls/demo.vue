55cd96dfd512cd09944542918ccb2492d0879cb5
Justin Halsall <Juice10@users.noreply.github.com>
2023-06-30 01:30:14 +0200

    Fix broken video src

--- packages/core/useMediaControls/demo.vue
+++ packages/core/useMediaControls/demo.vue
@@ -13,7 +13,7 @@ const poster = 'https://bitmovin.com/wp-content/uploads/2016/06/sintel-poster.jp
 
 const controls = useMediaControls(video, {
   src: {
-    src: 'https://upload.wikimedia.org/wikipedia/commons/transcoded/f/f1/Sintel_movie_4K.webm/Sintel_movie_4K.webm.1080p.vp9.webm',
+    src: 'https://upload.wikimedia.org/wikipedia/commons/f/f1/Sintel_movie_4K.webm',
     type: 'video/webm',
   },
   tracks: [
