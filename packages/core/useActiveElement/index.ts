e7c0bcf2345173a194e3e0d7d512e672d96404d8
Anthony Fu <anthonyfu117@hotmail.com>
2023-07-30 15:39:00 +0200

    chore: update

--- packages/core/useActiveElement/index.ts
+++ packages/core/useActiveElement/index.ts
@@ -3,7 +3,14 @@ import { useEventListener } from '../useEventListener'
 import type { ConfigurableDocumentOrShadowRoot, ConfigurableWindow } from '../_configurable'
 import { defaultWindow } from '../_configurable'
 
-export interface UseActiveElementOptions extends ConfigurableWindow, ConfigurableDocumentOrShadowRoot {}
+export interface UseActiveElementOptions extends ConfigurableWindow, ConfigurableDocumentOrShadowRoot {
+  /**
+   * Search actively deeply in shadow dom
+   *
+   * @default true
+   */
+  deepActiveElement?: boolean
+}
 
 /**
  * Reactive `document.activeElement`
@@ -11,26 +18,33 @@ export interface UseActiveElementOptions extends ConfigurableWindow, Configurabl
  * @see https://vueuse.org/useActiveElement
  * @param options
  */
-export function useActiveElement<T extends HTMLElement>(options: UseActiveElementOptions = {}) {
-  const { window = defaultWindow } = options
+export function useActiveElement<T extends HTMLElement>(
+  options: UseActiveElementOptions = {},
+) {
+  const {
+    window = defaultWindow,
+    deepActiveElement = true,
+  } = options
   const document = options.document ?? window?.document
-  const getDeepestActiveElement = () => {
-    let element = document?.activeElement;
-    while (element?.shadowRoot) {
-        element = element?.shadowRoot?.activeElement;
+
+  const getDeepActiveElement = () => {
+    let element = document?.activeElement
+    if (deepActiveElement) {
+      while (element?.shadowRoot)
+        element = element?.shadowRoot?.activeElement
     }
-    return element;
-  };
+    return element
+  }
+
   const activeElement = computedWithControl(
     () => null,
-    () => getDeepestActiveElement() as T | null | undefined,
+    () => getDeepActiveElement() as T | null | undefined,
   )
 
   if (window) {
     useEventListener(window, 'blur', (event) => {
       if (event.relatedTarget !== null)
         return
-
       activeElement.trigger()
     }, true)
     useEventListener(window, 'focus', activeElement.trigger, true)
