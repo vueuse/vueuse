eed1baa0529316cb0d5dca9d4d467f3ee1dc3cfc
Oliver Mak <oliver139.working@gmail.com>
2023-08-09 00:12:01 +0800

    Providing more accurate support check on demo

--- packages/core/useClipboard/demo.vue
+++ packages/core/useClipboard/demo.vue
@@ -4,26 +4,27 @@ import { useClipboard, usePermission } from '@vueuse/core'
 
 const input = ref('')
 
-const { text, isSupported, copy } = useClipboard()
+const { text, isClipboardApiSupported, isClipboardReadSupported, copy } = useClipboard({ read: true, legacy: true })
 const permissionRead = usePermission('clipboard-read')
 const permissionWrite = usePermission('clipboard-write')
 </script>
 
 <template>
-  <div v-if="isSupported">
-    <note>
-      Clipboard Permission: read <b>{{ permissionRead }}</b> | write
-      <b>{{ permissionWrite }}</b>
-    </note>
-    <p>
-      Current copied: <code>{{ text || 'none' }}</code>
+  <note>
+    <p>Clipboard Permission: read <b>{{ permissionRead }}</b> | write <b>{{ permissionWrite }}</b></p>
+    <p v-if="!isClipboardApiSupported">
+      Your browser does not support Clipboard API. <code>document.execCommand("copy")</code> will be used.
     </p>
+    <p v-if="!isClipboardReadSupported">
+      Your browser does not support <code>navigator.clipboard.readText()</code>. Legacy method will be used.
+    </p>
+  </note>
+
+  <div class="example">
+    <p>Current copied: <code>{{ text || 'none' }}</code></p>
     <input v-model="input" type="text">
     <button @click="copy(input)">
       Copy
     </button>
   </div>
-  <p v-else>
-    Your browser does not support Clipboard API
-  </p>
 </template>
