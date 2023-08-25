4ee0506c211eb4658716d528068864c4baefdff1
Anthony Fu <anthonyfu117@hotmail.com>
2023-08-25 13:30:19 +0200

    chore: typo

--- packages/.vitepress/theme/components/CourseLink.vue
+++ packages/.vitepress/theme/components/CourseLink.vue
@@ -6,7 +6,7 @@ defineProps<{
 
 <template>
   <a :href="href" target="_blank" bg-green:10 px4 py3 rounded-md block mt2 flex items-center gap2>
-    <div i-carbon--play-filled flex-none text-lg />
+    <div i-carbon:play-filled flex-none text-lg />
     <slot />
   </a>
 </template>
