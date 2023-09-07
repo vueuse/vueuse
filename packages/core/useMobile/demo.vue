40f6bba0d09148db613cec2a9c72c56f9802e212
Aarón J. Montes <32050436+ajomuch92@users.noreply.github.com>
2023-09-07 14:24:03 -0600

    feat: 🎸 Providing demo and readme for this new utility

---
+++ packages/core/useMobile/demo.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { computed, reactive } from 'vue'
+import YAML from 'js-yaml'
+import { useMobile } from '@vueuse/core'
+
+const { isAndroid, isIOS, isDesktop } = useMobile()
+
+const code = computed(() => YAML.dump(reactive({
+  isAndroid,
+  isIOS,
+  isDesktop,
+})))
+</script>
+
+<template>
+  <pre lang="json">{{ code }}</pre>
+</template>
---
+++ packages/core/useMobile/index.md
@@ -0,0 +1,15 @@
+---
+category: Browser
+---
+
+# useMobile
+
+Provides a simple and efficient way to determine the platform your application is running on, whether it's Android, iOS, or a desktop environment. 
+
+## Usage
+
+```js
+import { useMobile } from '@vueuse/core'
+
+const { isAndroid, isIOS, isDesktop } = useMobile()
+```
