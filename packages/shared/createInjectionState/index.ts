ccd7ddf095f0209f7cb554b91df382bbcc2edfc6
ZHAO JinXiang <xiaoxiangmoe@gmail.com>
2023-09-10 15:45:58 +0800

    feat(createInjectionState): allow call useProvidingState and useInjectedState in same component

--- packages/shared/createInjectionState/index.ts
+++ packages/shared/createInjectionState/index.ts
@@ -1,4 +1,52 @@
-import { type InjectionKey, inject, provide } from 'vue-demi'
+import { type InjectionKey, getCurrentInstance, inject, provide } from 'vue-demi'
+
+const localProvidedStateMap = new WeakMap<NonNullable<ReturnType<typeof getCurrentInstance>>, Record<string | symbol, any>>()
+
+/**
+ * On the basis of `provide`, it is allowed to directly call inject to obtain the value after call provide in the same component.
+ *
+ * @example
+ * ```ts
+ * provideEnhancedAllowanceOfCallsFromTheSameComponent('MyInjectionKey', 1)
+ * const injectedValue = injectEnhancedAllowanceOfCallsFromTheSameComponent('MyInjectionKey') // injectedValue === 1
+ * ```
+ */
+export const provideEnhancedAllowanceOfCallsFromTheSameComponent: typeof provide = (key, value) => {
+  const instance = getCurrentInstance()
+  if (instance == null)
+    throw new Error('provideEnhancedAllowanceOfCallsFromTheSameComponent must be called in setup')
+
+  if (!localProvidedStateMap.has(instance))
+    localProvidedStateMap.set(instance, Object.create(null))
+
+  const localProvidedState = localProvidedStateMap.get(instance)!
+  // @ts-expect-error allow InjectionKey as key
+  localProvidedState[key] = value
+  provide(key, value)
+}
+
+/**
+ * On the basis of `inject`, it is allowed to directly call inject to obtain the value after call provide in the same component.
+ *
+ * @example
+ * ```ts
+ * provideEnhancedAllowanceOfCallsFromTheSameComponent('MyInjectionKey', 1)
+ * const injectedValue = injectEnhancedAllowanceOfCallsFromTheSameComponent('MyInjectionKey') // injectedValue === 1
+ * ```
+ */
+// @ts-expect-error overloads are not compatible
+export const injectEnhancedAllowanceOfCallsFromTheSameComponent: typeof inject = (...args) => {
+  const key = args[0] as string | symbol
+  const instance = getCurrentInstance()
+  if (instance == null)
+    throw new Error('injectEnhancedAllowanceOfCallsFromTheSameComponent must be called in setup')
+
+  if (localProvidedStateMap.has(instance) && key in localProvidedStateMap.get(instance)!)
+    return localProvidedStateMap.get(instance)![key]
+
+  // @ts-expect-error overloads are not compatible
+  return inject(...args)
+}
 
 /**
  * Create global state that can be injected into components.
@@ -12,9 +60,9 @@ export function createInjectionState<Arguments extends Array<any>, Return>(
   const key: string | InjectionKey<Return> = Symbol('InjectionState')
   const useProvidingState = (...args: Arguments) => {
     const state = composable(...args)
-    provide(key, state)
+    provideEnhancedAllowanceOfCallsFromTheSameComponent(key, state)
     return state
   }
-  const useInjectedState = () => inject(key)
+  const useInjectedState = () => injectEnhancedAllowanceOfCallsFromTheSameComponent(key)
   return [useProvidingState, useInjectedState]
 }
