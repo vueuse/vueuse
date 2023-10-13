e0830b1d72aa2a8fb9f82b6533403c0e6edd3a99
zhuzhenquan <2332778528@qq.com>
2023-10-13 16:12:31 +0800

    feat(useDateFormat): Add Date ordinal formatting to useDateFormat()

--- packages/shared/useDateFormat/index.ts
+++ packages/shared/useDateFormat/index.ts
@@ -20,7 +20,7 @@ export interface UseDateFormatOptions {
 }
 
 const REGEX_PARSE = /* #__PURE__ */ /^(\d{4})[-/]?(\d{1,2})?[-/]?(\d{0,2})[Tt\s]*(\d{1,2})?:?(\d{1,2})?:?(\d{1,2})?[.:]?(\d+)?$/
-const REGEX_FORMAT = /* #__PURE__ */ /\[([^\]]+)]|Y{1,4}|M{1,4}|D{1,2}|d{1,4}|H{1,2}|h{1,2}|a{1,2}|A{1,2}|m{1,2}|s{1,2}|Z{1,2}|SSS/g
+const REGEX_FORMAT = /* #__PURE__ */ /[YMDHhms]o|\[([^\]]+)]|Y{1,4}|M{1,4}|D{1,2}|d{1,4}|H{1,2}|h{1,2}|a{1,2}|A{1,2}|m{1,2}|s{1,2}|Z{1,2}|SSS/g
 
 function defaultMeridiem(hours: number, minutes: number, isLowercase?: boolean, hasPeriod?: boolean) {
   let m = (hours < 12 ? 'AM' : 'PM')
@@ -40,21 +40,28 @@ export function formatDate(date: Date, formatStr: string, options: UseDateFormat
   const day = date.getDay()
   const meridiem = options.customMeridiem ?? defaultMeridiem
   const matches: Record<string, () => string | number> = {
+    Yo: () => `${years}${['st', 'nd', 'rd'][years % 10 - 1] || 'th'}`,
     YY: () => String(years).slice(-2),
     YYYY: () => years,
     M: () => month + 1,
+    Mo: () => `${month + 1}${['st', 'nd', 'rd'][month % 10] || 'th'}`,
     MM: () => `${month + 1}`.padStart(2, '0'),
     MMM: () => date.toLocaleDateString(options.locales, { month: 'short' }),
     MMMM: () => date.toLocaleDateString(options.locales, { month: 'long' }),
     D: () => String(days),
+    Do: () => `${days}${['st', 'nd', 'rd'][days % 10 - 1] || 'th'}`,
     DD: () => `${days}`.padStart(2, '0'),
     H: () => String(hours),
+    Ho: () => `${hours}${['st', 'nd', 'rd'][hours % 10 - 1] || 'th'}`,
     HH: () => `${hours}`.padStart(2, '0'),
     h: () => `${hours % 12 || 12}`.padStart(1, '0'),
+    ho: () => `${hours % 12 || 12}${['st', 'nd', 'rd'][hours % 10 - 1] || 'th'}`,
     hh: () => `${hours % 12 || 12}`.padStart(2, '0'),
     m: () => String(minutes),
+    mo: () => `${minutes}${['st', 'nd', 'rd'][minutes % 10 - 1] || 'th'}`,
     mm: () => `${minutes}`.padStart(2, '0'),
     s: () => String(seconds),
+    so: () => `${seconds}${['st', 'nd', 'rd'][seconds % 10 - 1] || 'th'}`,
     ss: () => `${seconds}`.padStart(2, '0'),
     SSS: () => `${milliseconds}`.padStart(3, '0'),
     d: () => day,
