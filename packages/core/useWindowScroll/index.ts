dfef2375958691e42ed395c4ade4737873913d70
chensiyuan <250758092@qq.com>
2023-10-24 14:34:52 +0800

    feat(useWindowScroll): Add computed for scrolling

--- packages/core/useWindowScroll/index.ts
+++ packages/core/useWindowScroll/index.ts
@@ -1,7 +1,11 @@
-import { ref } from 'vue-demi'
+import { computed, ref } from 'vue-demi'
 import { useEventListener } from '../useEventListener'
 import type { ConfigurableWindow } from '../_configurable'
 import { defaultWindow } from '../_configurable'
+
+export interface UseWindowScrollOptions extends ConfigurableWindow {
+  behavior?: ScrollBehavior
+}
 
 /**
  * Reactive window scroll.
@@ -9,23 +13,40 @@ import { defaultWindow } from '../_configurable'
  * @see https://vueuse.org/useWindowScroll
  * @param options
  */
-export function useWindowScroll({ window = defaultWindow }: ConfigurableWindow = {}) {
+export function useWindowScroll({ window = defaultWindow, behavior = 'auto' }: UseWindowScrollOptions = {}) {
+  const internalX = ref(0)
+  const internalY = ref(0)
+
   if (!window) {
     return {
-      x: ref(0),
-      y: ref(0),
+      x: internalX,
+      y: internalY,
     }
   }
 
-  const x = ref(window.scrollX)
-  const y = ref(window.scrollY)
+  const x = computed({
+    get() {
+      return internalX.value
+    },
+    set(x: number) {
+      scrollTo({ left: x, behavior })
+    },
+  })
+  const y = computed({
+    get() {
+      return internalY.value
+    },
+    set(y: number) {
+      scrollTo({ top: y, behavior })
+    },
+  })
 
   useEventListener(
     window,
     'scroll',
     () => {
-      x.value = window.scrollX
-      y.value = window.scrollY
+      internalX.value = window.scrollX
+      internalY.value = window.scrollY
     },
     {
       capture: false,
