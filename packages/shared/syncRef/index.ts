8495e5652e4c08a81f49c3db06664d4dad232e4c
Doctorwu <wuzhouchun@qq.com>
2023-10-31 11:21:57 +0800

    feat(syncRef): enhance syncRef type restrict
    fix#3514

--- packages/shared/syncRef/index.ts
+++ packages/shared/syncRef/index.ts
@@ -1,9 +1,48 @@
-import type { Ref } from 'vue-demi'
+import { type Ref } from 'vue-demi'
 import type { ConfigurableFlushSync } from '../utils'
 import type { WatchPausableReturn } from '../watchPausable'
 import { pausableWatch } from '../watchPausable'
 
-export interface SyncRefOptions<L, R = L> extends ConfigurableFlushSync {
+type Direction = 'ltr' | 'rtl' | 'both'
+type SpecificFieldPartial<T, K extends keyof T> = Omit<T, K> & Partial<Pick<T, K>>
+/**
+ * A = B
+ */
+type Equal<A, B> = A extends B ? (B extends A ? true : false) : false
+
+/**
+ * A ∩ B ≠ ∅
+ */
+type IntersectButNotEqual<A, B> = Equal<A, B> extends true
+  ? false
+  : A & B extends never
+    ? false
+    : true
+
+/**
+ * A ⊆ B
+ */
+type IncludeButNotEqual<A, B> = Equal<A, B> extends true
+  ? false
+  : A extends B
+    ? true
+    : false
+
+/**
+ * A ∩ B = ∅
+ */
+type NotIntersect<A, B> = Equal<A, B> extends true
+  ? false
+  : A & B extends never
+    ? true
+    : false
+
+interface Transform<L, R> {
+  ltr: (left: L) => R
+  rtl: (right: R) => L
+}
+
+export type SyncRefOptions<L, R, D extends Direction> = ConfigurableFlushSync & {
   /**
    * Watch deeply
    *
@@ -22,36 +61,108 @@ export interface SyncRefOptions<L, R = L> extends ConfigurableFlushSync {
    *
    * @default 'both'
    */
-  direction?: 'ltr' | 'rtl' | 'both'
+  direction?: D
 
+} & {
   /**
    * Custom transform function
    */
-  transform?: {
-    ltr?: (left: L) => R
-    rtl?: (right: R) => L
-  }
-}
+  both: Equal<L, R> extends true
+    ? {
+        transform?: SpecificFieldPartial<Transform<L, R>, 'ltr' | 'rtl'>
+      }
+    : IncludeButNotEqual<L, R> extends true
+      ? {
+          transform: SpecificFieldPartial<Transform<L, R>, 'ltr'>
+        }
+      : IncludeButNotEqual<R, L> extends true
+        ? {
+            transform: SpecificFieldPartial<Transform<L, R>, 'rtl'>
+          }
+        : IntersectButNotEqual<L, R> extends true
+          ? {
+              transform: Transform<L, R>
+            }
+          : NotIntersect<L, R> extends true
+            ? {
+                transform: Transform<L, R>
+              }
+            : never
+  ltr: Equal<L, R> extends true
+    ? {
+        transform?: SpecificFieldPartial<Pick<Transform<L, R>, 'ltr'>, 'ltr'>
+      }
+    : IncludeButNotEqual<L, R> extends true
+      ? {
+          transform: SpecificFieldPartial<Pick<Transform<L, R>, 'ltr'>, 'ltr'>
+        }
+      : IncludeButNotEqual<R, L> extends true
+        ? {
+            transform: Pick<Transform<L, R>, 'ltr'>
+          }
+        : IntersectButNotEqual<L, R> extends true
+          ? {
+              transform: Pick<Transform<L, R>, 'ltr'>
+            }
+          : NotIntersect<L, R> extends true
+            ? {
+                transform: Pick<Transform<L, R>, 'ltr'>
+              }
+            : never
+  rtl: Equal<L, R> extends true
+    ? {
+        transform?: SpecificFieldPartial<Pick<Transform<L, R>, 'rtl'>, 'rtl'>
+      }
+    : IncludeButNotEqual<L, R> extends true
+      ? {
+          transform: Pick<Transform<L, R>, 'rtl'>
+        }
+      : IncludeButNotEqual<R, L> extends true
+        ? {
+            transform: SpecificFieldPartial<Pick<Transform<L, R>, 'rtl'>, 'rtl'>
+          }
+        : IntersectButNotEqual<L, R> extends true
+          ? {
+              transform: Pick<Transform<L, R>, 'rtl'>
+            }
+          : NotIntersect<L, R> extends true
+            ? {
+                transform: Pick<Transform<L, R>, 'rtl'>
+              }
+            : never
+}[D]
 
 /**
  * Two-way refs synchronization.
- *
+ * From the set theory perspective to restrict the option's type
+ * Check in the following order:
+ * 1. A = B
+ * 2. A ∩ B ≠ ∅
+ * 3. A ⊆ B
+ * 4. A ∩ B = ∅
  * @param left
  * @param right
+ * @param args
  */
-export function syncRef<L, R = L>(left: Ref<L>, right: Ref<R>, options: SyncRefOptions<L, R> = {}) {
+export function syncRef<L, R, D extends Direction = 'both'>(
+  left: Ref<L>,
+  right: Ref<R>,
+  ...args: Equal<L, R> extends true
+    ? [SyncRefOptions<L, R, D>?]
+    : [SyncRefOptions<L, R, D>]
+) {
   const {
     flush = 'sync',
     deep = false,
     immediate = true,
     direction = 'both',
     transform = {},
-  } = options
+  } = args[0] || {}
 
   const watchers: WatchPausableReturn[] = []
 
-  const transformLTR = transform.ltr ?? (v => v)
-  const transformRTL = transform.rtl ?? (v => v)
+  const transformLTR = ('ltr' in transform && transform.ltr) || (v => v)
+  const transformRTL = ('rtl' in transform && transform.rtl) || (v => v)
 
   if (direction === 'both' || direction === 'ltr') {
     watchers.push(pausableWatch(
