50018d135d8cd47cc9d27369d5a52c75cc9fbb5d
donaldxdonald <mzhlovefama@gmail.com>
2023-11-23 00:44:25 +0800

    feat(onLongPress): add `threshold` option

--- packages/core/onLongPress/index.ts
+++ packages/core/onLongPress/index.ts
@@ -3,8 +3,10 @@ import { computed } from 'vue-demi'
 import type { MaybeElementRef } from '../unrefElement'
 import { unrefElement } from '../unrefElement'
 import { useEventListener } from '../useEventListener'
+import type { Position } from '../types'
 
 const DEFAULT_DELAY = 500
+const DEFAULT_THRESHOLD = 10
 
 export interface OnLongPressOptions {
   /**
@@ -15,6 +17,13 @@ export interface OnLongPressOptions {
   delay?: number
 
   modifiers?: OnLongPressModifiers
+
+  /**
+   * Distance in pixels
+   *
+   * @default 10
+   */
+  threshold?: number
 }
 
 export interface OnLongPressModifiers {
@@ -33,12 +42,14 @@ export function onLongPress(
   const elementRef = computed(() => unrefElement(target))
 
   let timeout: ReturnType<typeof setTimeout> | undefined
+  let posStart: Position | undefined
 
   function clear() {
     if (timeout) {
       clearTimeout(timeout)
       timeout = undefined
     }
+    posStart = undefined
   }
 
   function onDown(ev: PointerEvent) {
@@ -53,10 +64,34 @@ export function onLongPress(
     if (options?.modifiers?.stop)
       ev.stopPropagation()
 
+    posStart = {
+      x: ev.x,
+      y: ev.y,
+    }
     timeout = setTimeout(
       () => handler(ev),
       options?.delay ?? DEFAULT_DELAY,
     )
+  }
+
+  function onMove(ev: PointerEvent) {
+    if (options?.modifiers?.self && ev.target !== elementRef.value)
+      return
+
+    if (!posStart)
+      return
+
+    if (options?.modifiers?.prevent)
+      ev.preventDefault()
+
+    if (options?.modifiers?.stop)
+      ev.stopPropagation()
+
+    const dx = ev.x - posStart.x
+    const dy = ev.y - posStart.y
+    const distance = Math.sqrt(dx * dx + dy * dy)
+    if (distance >= (options?.threshold ?? DEFAULT_THRESHOLD))
+      clear()
   }
 
   const listenerOptions: AddEventListenerOptions = {
@@ -66,6 +101,7 @@ export function onLongPress(
 
   const cleanup = [
     useEventListener(elementRef, 'pointerdown', onDown, listenerOptions),
+    useEventListener(elementRef, 'pointermove', onMove, listenerOptions),
     useEventListener(elementRef, ['pointerup', 'pointerleave'], clear, listenerOptions),
   ].filter(Boolean) as Fn[]
 
