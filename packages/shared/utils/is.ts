400ae9dc309b2aa5bf9a1daa400c460a2e36493a
Doctorwu <wuzhouchun@qq.com>
2023-11-23 10:14:06 +0800

    feat(isiOS): detect new iPad
    fix #3577

--- packages/shared/utils/is.ts
+++ packages/shared/utils/is.ts
@@ -13,7 +13,7 @@ export const isObject = (val: any): val is object =>
 export const now = () => Date.now()
 export const timestamp = () => +Date.now()
 export const clamp = (n: number, min: number, max: number) => Math.min(max, Math.max(min, n))
-export const noop = () => {}
+export const noop = () => { }
 export const rand = (min: number, max: number) => {
   min = Math.ceil(min)
   max = Math.floor(max)
@@ -21,8 +21,13 @@ export const rand = (min: number, max: number) => {
 }
 export const hasOwn = <T extends object, K extends keyof T>(val: T, key: K): key is K => Object.prototype.hasOwnProperty.call(val, key)
 
+export const isIpad = window?.navigator?.maxTouchPoints > 2 && /iPad|Macintosh/.test(window?.navigator.userAgent)
+
 export const isIOS = /* #__PURE__ */ getIsIOS()
 
 function getIsIOS() {
-  return isClient && /* #__PURE__ */ window?.navigator?.userAgent && /* #__PURE__ */ /iP(ad|hone|od)/.test(/* #__PURE__ */ window.navigator.userAgent)
+  return isClient && /* #__PURE__ */ (
+    (window?.navigator?.userAgent && /* #__PURE__ */ /iP(ad|hone|od)/.test(/* #__PURE__ */ window.navigator.userAgent))
+    || isIpad
+  )
 }
