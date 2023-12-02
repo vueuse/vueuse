bf6d708dd50113bd9307d83efe03ebca7feb7d21
userquin <userquin@gmail.com>
2023-12-02 22:14:28 +0100

    feat(nuxt)!: move to ESM-only and fix types

--- scripts/utils.ts
+++ scripts/utils.ts
@@ -244,9 +244,20 @@ export async function updatePackageJSON(indexes: PackageIndexes) {
       url: 'git+https://github.com/vueuse/vueuse.git',
       directory: `packages/${name}`,
     }
-    packageJSON.main = './index.cjs'
-    packageJSON.types = './index.d.cts'
-    packageJSON.module = './index.mjs'
+    // nuxt module is ESM-only
+    if (name !== 'nuxt') {
+      packageJSON.main = './index.cjs'
+      packageJSON.types = './index.d.cts'
+      packageJSON.module = './index.mjs'
+      packageJSON.exports = {
+        '.': {
+          import: './index.mjs',
+          require: './index.cjs',
+        },
+        './*': './*',
+        ...packageJSON.exports,
+      }
+    }
     if (iife !== false) {
       packageJSON.unpkg = './index.iife.min.js'
       packageJSON.jsdelivr = './index.iife.min.js'
