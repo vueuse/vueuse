e330a351ce58a828615dee7026811f46b805e3bd
userquin <userquin@gmail.com>
2024-01-20 13:41:12 +0100

    chore: cleanup pwa configuration

--- packages/.vitepress/config.ts
+++ packages/.vitepress/config.ts
@@ -194,9 +194,6 @@ export default withPwa(defineConfig({
     srcDir: '.vitepress/',
     filename: 'sw.ts',
     injectRegister: 'inline',
-    experimental: {
-      includeAllowlist: true,
-    },
     manifest: {
       id: '/',
       name: 'VueUse',
@@ -247,70 +244,6 @@ export default withPwa(defineConfig({
     injectManifest: {
       globPatterns: ['**/*.{css,js,html,svg,png,ico,txt,woff2}'],
       globIgnores: ['og-*.png'],
-    },
-    workbox: {
-      clientsClaim: true,
-      cleanupOutdatedCaches: true,
-      globPatterns: ['**/*.{css,js,html,svg,png,ico,txt,woff2}'],
-      globIgnores: ['og-*.png'],
-      runtimeCaching: [
-        {
-          urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
-          handler: 'CacheFirst',
-          options: {
-            cacheName: 'google-fonts-cache',
-            expiration: {
-              maxEntries: 10,
-              maxAgeSeconds: 60 * 60 * 24 * 365, // <== 365 days
-            },
-            cacheableResponse: {
-              statuses: [0, 200],
-            },
-          },
-        },
-        {
-          urlPattern: /^https:\/\/fonts\.gstatic\.com\/.*/i,
-          handler: 'CacheFirst',
-          options: {
-            cacheName: 'gstatic-fonts-cache',
-            expiration: {
-              maxEntries: 10,
-              maxAgeSeconds: 60 * 60 * 24 * 365, // <== 365 days
-            },
-            cacheableResponse: {
-              statuses: [0, 200],
-            },
-          },
-        },
-        {
-          urlPattern: /^https:\/\/avatars\.githubusercontent\.com\/.*/i,
-          handler: 'NetworkFirst',
-          options: {
-            cacheName: 'avatars-images-cache',
-            expiration: {
-              maxEntries: 10,
-              maxAgeSeconds: 60 * 60 * 24 * 7, // <== 7 days
-            },
-            cacheableResponse: {
-              statuses: [0, 200],
-            },
-          },
-        },
-        {
-          urlPattern: /^https:\/\/cdn\.jsdelivr\.net\/.*/i,
-          handler: 'NetworkFirst',
-          options: {
-            cacheName: 'jsdelivr-images-cache',
-            expiration: {
-              maxEntries: 10,
-              maxAgeSeconds: 60 * 60 * 24 * 7, // <== 7 days
-            },
-            cacheableResponse: {
-              statuses: [0, 200],
-            },
-          },
-        },
-      ],
     },
   },
 
