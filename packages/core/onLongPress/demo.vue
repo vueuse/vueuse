1dc1eedd22a788732ec5d65d238ced4f14e09452
Neil <me@neilrichter.com>
2024-02-15 17:59:09 +0100

    play: playground demo

--- packages/core/onLongPress/demo.vue
+++ packages/core/onLongPress/demo.vue
@@ -1,32 +1,45 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 import { onLongPress } from '@vueuse/core'
+import { OnLongPress } from './component'
 
 const htmlRef = ref<HTMLElement | null>(null)
-const htmlRefOptions = ref<HTMLElement | null>(null)
 
 const longPressed = ref(false)
+const clicked = ref(false)
 
 function onLongPressCallback(e: PointerEvent) {
   longPressed.value = true
 }
 
+function onClick() {
+  clicked.value = true
+}
+
 function reset() {
   longPressed.value = false
+  clicked.value = false
 }
 
 onLongPress(htmlRef, onLongPressCallback)
-onLongPress(htmlRefOptions, onLongPressCallback, { delay: 1000 })
 </script>
 
 <template>
   <p>Long Pressed: <BooleanDisplay :value="longPressed" /></p>
+  <p>Clicked: <BooleanDisplay :value="clicked" /></p>
   <button ref="htmlRef" class="ml-2 button small">
     Press long (500ms)
   </button>
-  <button ref="htmlRefOptions" class="ml-2 button small">
+  <OnLongPress
+    as="button"
+    :options="{
+      delay: 1000,
+    }" class="ml-2 button small"
+    @trigger="onLongPressCallback"
+    @click="onClick"
+  >
     Press long (1000ms)
-  </button>
+  </OnLongPress>
   <button class="ml-2 button small" @click="reset">
     Reset
   </button>
