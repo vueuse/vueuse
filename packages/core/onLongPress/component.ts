632c635d45aa7deea78249a28ee7092e83daabaf
Neil <me@neilrichter.com>
2024-02-15 17:56:04 +0100

    feat: differentiate cilck from longPress

--- packages/core/onLongPress/component.ts
+++ packages/core/onLongPress/component.ts
@@ -10,19 +10,38 @@ export interface OnLongPressProps extends RenderableComponent {
 export const OnLongPress = /* #__PURE__ */ defineComponent<OnLongPressProps>({
   name: 'OnLongPress',
   props: ['as', 'options'] as unknown as undefined,
-  emits: ['trigger'],
+  emits: ['trigger', 'click'],
   setup(props, { slots, emit }) {
     const target = ref()
+    const hasLongPressed = ref(false)
+
+    const onClick = (e: Event) => {
+      if (hasLongPressed.value) {
+        hasLongPressed.value = false
+        return
+      }
+      emit('click', e)
+    }
+
     onLongPress(
       target,
       (e) => {
+        hasLongPressed.value = true
         emit('trigger', e)
       },
       props.options,
     )
     return () => {
-      if (slots.default)
-        return h(props.as || 'div', { ref: target }, slots.default())
+      if (slots.default) {
+        return h(
+          props.as || 'div',
+          {
+            ref: target,
+            onClick,
+          },
+          slots.default(),
+        )
+      }
     }
   },
 })
