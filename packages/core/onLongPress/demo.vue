fde201b2e6c1a569aeb1830d96a93c4521172cfb
Neil <me@neilrichter.com>
2024-02-20 11:10:27 +0100

    Revert "play: playground demo"
    This reverts commit b0d1f2701545e3a2c88e034c574d234b66b681ce.

--- packages/core/onLongPress/demo.vue
+++ packages/core/onLongPress/demo.vue
@@ -1,45 +1,32 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 import { onLongPress } from '@vueuse/core'
-import { OnLongPress } from './component'
 
 const htmlRef = ref<HTMLElement | null>(null)
+const htmlRefOptions = ref<HTMLElement | null>(null)
 
 const longPressed = ref(false)
-const clicked = ref(false)
 
 function onLongPressCallback(e: PointerEvent) {
   longPressed.value = true
 }
 
-function onClick() {
-  clicked.value = true
-}
-
 function reset() {
   longPressed.value = false
-  clicked.value = false
 }
 
 onLongPress(htmlRef, onLongPressCallback)
+onLongPress(htmlRefOptions, onLongPressCallback, { delay: 1000 })
 </script>
 
 <template>
   <p>Long Pressed: <BooleanDisplay :value="longPressed" /></p>
-  <p>Clicked: <BooleanDisplay :value="clicked" /></p>
   <button ref="htmlRef" class="ml-2 button small">
     Press long (500ms)
   </button>
-  <OnLongPress
-    as="button"
-    :options="{
-      delay: 1000,
-    }" class="ml-2 button small"
-    @trigger="onLongPressCallback"
-    @click="onClick"
-  >
+  <button ref="htmlRefOptions" class="ml-2 button small">
     Press long (1000ms)
-  </OnLongPress>
+  </button>
   <button class="ml-2 button small" @click="reset">
     Reset
   </button>
