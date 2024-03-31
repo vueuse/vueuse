b2cc2de505e3eb1a8b616d4b8e768382922002ab
thefeymesaleng <thefeymesaleng@gmail.com>
2024-03-31 22:36:17 +0800

    feat(useWebWorkerFn): support local function dependencies

--- packages/core/useWebWorkerFn/lib/createWorkerBlobUrl.ts
+++ packages/core/useWebWorkerFn/lib/createWorkerBlobUrl.ts
@@ -6,6 +6,7 @@ import depsParser from './depsParser'
  *
  * @param fn the function to run with web worker
  * @param deps array of strings, imported into the worker through "importScripts"
+ * @param localDeps array of function, local dependencies
  *
  * @returns a blob url, containing the code of "fn" as a string
  *
@@ -15,8 +16,8 @@ import depsParser from './depsParser'
  * .then(postMessage(['SUCCESS', result]))
  * .catch(postMessage(['ERROR', error])"
  */
-function createWorkerBlobUrl(fn: Function, deps: string[]) {
-  const blobCode = `${depsParser(deps)}; onmessage=(${jobRunner})(${fn})`
+function createWorkerBlobUrl(fn: Function, deps: string[], localDeps: Function[]) {
+  const blobCode = `${depsParser(deps, localDeps)}; onmessage=(${jobRunner})(${fn})`
   const blob = new Blob([blobCode], { type: 'text/javascript' })
   const url = URL.createObjectURL(blob)
   return url
