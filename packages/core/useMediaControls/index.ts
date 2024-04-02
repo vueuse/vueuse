10765c7e78c7c66fc39a78d285b2ad50fad06a33
Heng Lee <hengry1213@gmail.com>
2024-04-02 15:27:24 +0800

    fix: side effect when element turn from null to element

--- packages/core/useMediaControls/index.ts
+++ packages/core/useMediaControls/index.ts
@@ -294,7 +294,10 @@ export function useMediaControls(target: MaybeRef<HTMLMediaElement | null | unde
   /**
    * Apply composable state to the element, also when element is changed
    */
-  watch([target, volume], () => {
+  watch([target, volume], (_, [oldTarget]) => {
+    if (!oldTarget)
+      return
+
     const el = toValue(target)
     if (!el)
       return
@@ -302,7 +305,10 @@ export function useMediaControls(target: MaybeRef<HTMLMediaElement | null | unde
     el.volume = volume.value
   })
 
-  watch([target, muted], () => {
+  watch([target, muted], (_, [oldTarget]) => {
+    if (!oldTarget)
+      return
+
     const el = toValue(target)
     if (!el)
       return
@@ -310,7 +316,10 @@ export function useMediaControls(target: MaybeRef<HTMLMediaElement | null | unde
     el.muted = muted.value
   })
 
-  watch([target, rate], () => {
+  watch([target, rate], (_, [oldTarget]) => {
+    if (!oldTarget)
+      return
+
     const el = toValue(target)
     if (!el)
       return
