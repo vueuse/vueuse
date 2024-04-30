c15a1369c7aed09a6addd22da3809559a2b63feb
wjp <wjp_github@163.com>
2024-04-30 12:27:55 +0800

    fix(useMousePressed)：Fix spelling errors in JSDoc

--- packages/core/useMousePressed/index.ts
+++ packages/core/useMousePressed/index.ts
@@ -43,7 +43,7 @@ export interface MousePressedOptions extends ConfigurableWindow {
 }
 
 /**
- * Reactive Mouse Pressing State.
+ * Reactive mouse pressing state.
  *
  * @see https://vueuse.org/useMousePressed
  * @param options
