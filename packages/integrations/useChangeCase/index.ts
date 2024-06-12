8aaedb6ca6c0b8fd99ce91502326b2e80563ca56
Anthony Fu <github@antfu.me>
2024-06-12 13:35:15 +0200

    chore: update

--- packages/integrations/useChangeCase/index.ts
+++ packages/integrations/useChangeCase/index.ts
@@ -11,7 +11,7 @@ type ChangeCaseKeys = FilterKeys<typeof changeCase>
 
 export type ChangeCaseType = ChangeCaseKeys[keyof ChangeCaseKeys]
 
-const changeCaseTransforms: any = Object.values(changeCase)
+const changeCaseTransforms: any = /* @__PURE__ */ Object.values(changeCase)
   .filter(v => typeof v === 'function' && v.name.endsWith('Case')).reduce((acc, fn) => {
     acc[fn.name] = fn
     return acc
@@ -26,13 +26,20 @@ export function useChangeCase(input: MaybeRefOrGetter<string>, type: MaybeRefOrG
  * @see https://vueuse.org/useChangeCase
  */
 export function useChangeCase(input: MaybeRefOrGetter<string>, type: MaybeRefOrGetter<ChangeCaseType>, options?: MaybeRefOrGetter<Options> | undefined) {
+  const typeRef = computed(() => {
+    const t = toValue(type)
+    if (!changeCaseTransforms[t])
+      throw new Error(`Invalid change case type "${t}"`)
+    return t
+  })
+
   if (typeof input === 'function')
-    return computed(() => changeCaseTransforms[toValue(type)](toValue(input), toValue(options)))
+    return computed(() => changeCaseTransforms[typeRef.value](toValue(input), toValue(options)))
 
   const text = ref(input)
   return computed<string>({
     get() {
-      return changeCaseTransforms[toValue(type)](text.value, toValue(options))
+      return changeCaseTransforms[typeRef.value](text.value, toValue(options))
     },
     set(value) {
       text.value = value
