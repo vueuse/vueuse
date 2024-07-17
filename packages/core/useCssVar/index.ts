fe19c747b821f95860da55bff93c83e7ac5e122e
Fernando Fernández <ferferga@hotmail.com>
2024-07-17 14:55:44 +0200

    feat(useCssVars): remove property on null/undefined (#3821)
    Signed-off-by: GitHub <noreply@github.com>

--- packages/core/useCssVar/index.ts
+++ packages/core/useCssVar/index.ts
@@ -25,18 +25,18 @@ export interface UseCssVarOptions extends ConfigurableWindow {
  * @param options
  */
 export function useCssVar(
-  prop: MaybeRefOrGetter<string>,
+  prop: MaybeRefOrGetter<string | null | undefined>,
   target?: MaybeElementRef,
   options: UseCssVarOptions = {},
 ) {
-  const { window = defaultWindow, initialValue = '', observe = false } = options
-  const variable = ref(initialValue)
+  const { window = defaultWindow, initialValue, observe = false } = options
+  const variable = ref<string | null | undefined>(initialValue)
   const elRef = computed(() => unrefElement(target) || window?.document?.documentElement)
 
   function updateCssVar() {
     const key = toValue(prop)
     const el = toValue(elRef)
-    if (el && window) {
+    if (el && window && key) {
       const value = window.getComputedStyle(el).getPropertyValue(key)?.trim()
       variable.value = value || initialValue
     }
@@ -51,15 +51,24 @@ export function useCssVar(
 
   watch(
     [elRef, () => toValue(prop)],
-    updateCssVar,
+    (_, old) => {
+      if (old[0] && old[1] && window)
+        window.getComputedStyle(old[0]).removeProperty(old[1])
+      updateCssVar()
+    },
     { immediate: true },
   )
 
   watch(
     variable,
     (val) => {
-      if (elRef.value?.style)
-        elRef.value.style.setProperty(toValue(prop), val)
+      const raw_prop = toValue(prop)
+      if (elRef.value?.style && raw_prop) {
+        if (val == null)
+          elRef.value.style.removeProperty(raw_prop)
+        else
+          elRef.value.style.setProperty(raw_prop, val)
+      }
     },
   )
 
