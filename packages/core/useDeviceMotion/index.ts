9b2a16c8f3b27d1a98e7d234ef27d3f260343467
thefeymesaleng <thefeymesaleng@gmail.com>
2024-08-02 00:59:00 +0800

    added permission check and request for ios

--- packages/core/useDeviceMotion/index.ts
+++ packages/core/useDeviceMotion/index.ts
@@ -9,7 +9,9 @@ import type { ConfigurableWindow } from '../_configurable'
 import { defaultWindow } from '../_configurable'
 
 export interface DeviceMotionOptions extends ConfigurableWindow, ConfigurableEventFilter { }
-export interface DeviceMotionOptions extends ConfigurableWindow, ConfigurableEventFilter {}
+interface DeviceMotionEventiOS extends DeviceMotionOptions {
+  requestPermission: () => Promise<'granted' | 'denied'>
+}
 
 /**
  * Reactive DeviceMotionEvent.
@@ -23,6 +25,8 @@ export function useDeviceMotion(options: DeviceMotionOptions = {}) {
     eventFilter = bypassFilter,
   } = options
 
+  const requirePermission = 'requestPermission' in DeviceMotionEvent && typeof DeviceMotionEvent.requestPermission === 'function'
+  const permissionGranted = ref(false)
   const acceleration: Ref<DeviceMotionEvent['acceleration']> = ref({ x: null, y: null, z: null })
   const rotationRate: Ref<DeviceMotionEvent['rotationRate']> = ref({ alpha: null, beta: null, gamma: null })
   const interval = ref(0)
@@ -32,25 +36,48 @@ export function useDeviceMotion(options: DeviceMotionOptions = {}) {
     z: null,
   })
 
-  if (window) {
-    const onDeviceMotion = createFilterWrapper(
-      eventFilter,
-      (event: DeviceMotionEvent) => {
-        acceleration.value = { x: event.acceleration?.x || null, y: event.acceleration?.y || null, z: event.acceleration?.z || null }
-        accelerationIncludingGravity.value = { x: event.accelerationIncludingGravity?.x || null, y: event.accelerationIncludingGravity?.y || null, z: event.accelerationIncludingGravity?.z || null }
-        rotationRate.value = { alpha: event.rotationRate?.alpha || null, beta: event.rotationRate?.beta || null, gamma: event.rotationRate?.gamma || null }
-        interval.value = event.interval
-      },
-    )
+  function init() {
+    if (window) {
+      const onDeviceMotion = createFilterWrapper(
+        eventFilter,
+        (event: DeviceMotionEvent) => {
+          acceleration.value = { x: event.acceleration?.x || null, y: event.acceleration?.y || null, z: event.acceleration?.z || null }
+          accelerationIncludingGravity.value = { x: event.accelerationIncludingGravity?.x || null, y: event.accelerationIncludingGravity?.y || null, z: event.accelerationIncludingGravity?.z || null }
+          rotationRate.value = { alpha: event.rotationRate?.alpha || null, beta: event.rotationRate?.beta || null, gamma: event.rotationRate?.gamma || null }
+          interval.value = event.interval
+        },
+      )
 
-    useEventListener(window, 'devicemotion', onDeviceMotion)
+      useEventListener(window, 'devicemotion', onDeviceMotion)
+    }
   }
+  const trigger = async () => {
+    if (requirePermission) {
+      const requestPermission = (DeviceMotionEvent as unknown as DeviceMotionEventiOS).requestPermission
+      try {
+        const response = await requestPermission()
+        if (response === 'granted') {
+          permissionGranted.value = true
+          init()
+        }
+      }
+      catch (error) {
+        console.error(error)
+      }
+    }
+  }
+
+  if (!requirePermission)
+    init()
 
   return {
     acceleration,
     accelerationIncludingGravity,
     rotationRate,
     interval,
+    requirePermission,
+    permissionGranted,
+    trigger,
   }
 }
 
