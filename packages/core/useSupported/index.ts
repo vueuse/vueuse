5a1bb9df8ee39e5aa9f4832ca584999c013888d7
Onion-L <lixiang020317@gmail.com>
2024-08-16 12:17:19 +0800

    refactor(useSupported): handle memory leak issue

--- packages/core/useSupported/index.ts
+++ packages/core/useSupported/index.ts
@@ -1,13 +1,12 @@
-import { computed } from 'vue-demi'
+import { readonly, ref, watch } from 'vue-demi'
 import { useMounted } from '../useMounted'
 
 export function useSupported(callback: () => unknown) {
   const isMounted = useMounted()
+  const isSupported = ref(false)
 
-  return computed(() => {
-    // to trigger the ref
-    // eslint-disable-next-line ts/no-unused-expressions
-    isMounted.value
-    return Boolean(callback())
-  })
+  watch(isMounted, () => {
+    isSupported.value = Boolean(callback())
+  }, { immediate: true })
+  return readonly(isSupported)
 }
