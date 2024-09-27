0a62d8bcb068f79202cbf52659a75adca94e1334
proc07 <790847400@qq.com>
2024-09-27 20:49:31 +0800

    fix(ssr) set defaultWindow nodejs env

--- packages/core/_configurable.ts
+++ packages/core/_configurable.ts
@@ -35,7 +35,7 @@ export interface ConfigurableLocation {
   location?: Location
 }
 
-export const defaultWindow = /* #__PURE__ */ isClient ? window : undefined
+export const defaultWindow = /* #__PURE__ */ isClient ? window : globalThis
 export const defaultDocument = /* #__PURE__ */ isClient ? window.document : undefined
 export const defaultNavigator = /* #__PURE__ */ isClient ? window.navigator : undefined
 export const defaultLocation = /* #__PURE__ */ isClient ? window.location : undefined
