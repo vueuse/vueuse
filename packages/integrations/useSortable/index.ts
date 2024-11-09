14770b02ebb7d0c65aa907809c2f63801e18b750
IlyaL <a25690756@gmail.com>
2024-11-09 11:58:38 +0800

    fix(useSortable): wrong order of elements

--- packages/integrations/useSortable/index.ts
+++ packages/integrations/useSortable/index.ts
@@ -91,6 +91,10 @@ export function moveArrayElement<T>(
 
   if (to >= 0 && to < array.length) {
     const element = array.splice(from, 1)[0]
+
+    if (_valueIsRef)
+      (list as MaybeRef).value.length = 0
+
     nextTick(() => {
       array.splice(to, 0, element)
       // When list is ref, assign array to list.value
