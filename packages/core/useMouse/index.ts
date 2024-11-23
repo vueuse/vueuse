64bbb6dc20bae766fcb0110eb19e133e5810be43
IlyaL <a25690756@gmail.com>
2024-11-23 16:18:16 +0800

    revert: restore useMouse

--- packages/core/useMouse/index.ts
+++ packages/core/useMouse/index.ts
@@ -52,7 +52,7 @@ export interface UseMouseOptions extends ConfigurableWindow, ConfigurableEventFi
 }
 
 const UseMouseBuiltinExtractors: Record<UseMouseCoordType, UseMouseEventExtractor> = {
-  page: event => [event.clientX, event.clientY],
+  page: event => [event.pageX, event.pageY],
   client: event => [event.clientX, event.clientY],
   screen: event => [event.screenX, event.screenY],
   movement: event => (event instanceof Touch
