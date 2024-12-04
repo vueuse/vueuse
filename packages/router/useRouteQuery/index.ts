0e5eb81ac9c7a89a4f9aa467180c6a7fc501d56e
Andrej Agapow <dev@agapow.de>
2024-12-04 17:06:49 +0100

    fix(useRouteQuery): differentiate `undefined` and `null` when writing value

--- packages/router/useRouteQuery/index.ts
+++ packages/router/useRouteQuery/index.ts
@@ -67,8 +67,8 @@ export function useRouteQuery<
         if (query === v)
           return
 
-        query = (v === defaultValue || v === null) ? undefined : v
-        _queriesQueue.set(name, (v === defaultValue || v === null) ? undefined : v)
+        query = (v === defaultValue) ? undefined : v
+        _queriesQueue.set(name, (v === defaultValue) ? undefined : v)
 
         trigger()
 
