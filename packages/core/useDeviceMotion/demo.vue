cf72929df5d915b0703822947a698051fc792790
thefeymesaleng <39217597+thefeymesaleng@users.noreply.github.com>
2024-12-22 12:46:23 +0800

    feat(useDeviceMotion): add permission controls, improve destructure (#4127)
    Co-authored-by: Anthony Fu <github@antfu.me>

--- packages/core/useDeviceMotion/demo.vue
+++ packages/core/useDeviceMotion/demo.vue
@@ -1,14 +1,34 @@
 <script setup lang="ts">
 import { useDeviceMotion } from '@vueuse/core'
-import { computed, reactive } from 'vue'
 
-const motion = reactive(useDeviceMotion())
-const text = computed(() => JSON.stringify(motion, null, 2))
+const {
+  acceleration,
+  accelerationIncludingGravity,
+  rotationRate,
+  interval,
+  isSupported,
+  ensurePermissions,
+  permissionGranted,
+} = useDeviceMotion()
 </script>
 
 <template>
   <note class="mb-2">
     Device Motion:
   </note>
-  <pre lang="json">{{ text }}</pre>
+  <div v-if="!isSupported">
+    Not supported by your current browser.
+  </div>
+  <template v-else>
+    <pre
+      v-if="!permissionGranted"
+      lang="json"
+    >{{ { acceleration, accelerationIncludingGravity, rotationRate, interval } }}</pre>
+    <div v-else>
+      Permission is required
+    </div>
+    <button v-if="!permissionGranted" @click="ensurePermissions">
+      Request Permission
+    </button>
+  </template>
 </template>
