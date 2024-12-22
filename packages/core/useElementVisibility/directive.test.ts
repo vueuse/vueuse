3a928f49038bf37fb6465de2faa9c5fd7cc37871
Dominik Ritter <dritter03@googlemail.com>
2024-12-22 06:00:36 +0100

    feat(useElementVisibility): add `rootMargin` option (#4100)
    Co-authored-by: Anthony Fu <github@antfu.me>

--- packages/core/useElementVisibility/directive.test.ts
+++ packages/core/useElementVisibility/directive.test.ts
@@ -53,6 +53,7 @@ describe('vElementVisibility', () => {
       onVisibility = vi.fn()
       const options: UseElementVisibilityOptions = {
         scrollTarget: document.body,
+        rootMargin: '10px 0px 0px 0px',
       }
       wrapper = mount(App, {
         props: {
