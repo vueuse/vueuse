ffa00d25be6d6ffe54644ff205dd72b0e978e603
IlyaL <a25690756@gmail.com>
2024-12-23 13:47:14 +0800

    fix(useWebSocket): clear retryTimer when connected (#4383)
    Co-authored-by: Anthony Fu <anthonyfu117@hotmail.com>
    Co-authored-by: Anthony Fu <github@antfu.me>

--- packages/core/useWebSocket/index.ts
+++ packages/core/useWebSocket/index.ts
@@ -181,6 +181,7 @@ export function useWebSocket<Data = any>(
 
   let bufferedData: (string | ArrayBuffer | Blob)[] = []
 
+  let retryTimeout: ReturnType<typeof setTimeout> | undefined
   let pongTimeoutWait: ReturnType<typeof setTimeout> | undefined
 
   const _sendBuffer = () => {
@@ -191,6 +192,13 @@ export function useWebSocket<Data = any>(
     }
   }
 
+  const resetRetry = () => {
+    if (retryTimeout != null) {
+      clearTimeout(retryTimeout)
+      retryTimeout = undefined
+    }
+  }
+
   const resetHeartbeat = () => {
     clearTimeout(pongTimeoutWait)
     pongTimeoutWait = undefined
@@ -198,6 +206,7 @@ export function useWebSocket<Data = any>(
 
   // Status code 1000 -> Normal Closure https://developer.mozilla.org/en-US/docs/Web/API/CloseEvent/code
   const close: WebSocket['close'] = (code = 1000, reason) => {
+    resetRetry()
     if ((!isClient && !isWorker) || !wsRef.value)
       return
     explicitlyClosed = true
@@ -247,10 +256,10 @@ export function useWebSocket<Data = any>(
 
         if (typeof retries === 'number' && (retries < 0 || retried < retries)) {
           retried += 1
-          setTimeout(_init, delay)
+          retryTimeout = setTimeout(_init, delay)
         }
         else if (typeof retries === 'function' && retries()) {
-          setTimeout(_init, delay)
+          retryTimeout = setTimeout(_init, delay)
         }
         else {
           onFailed?.()
@@ -313,6 +322,7 @@ export function useWebSocket<Data = any>(
   const open = () => {
     if (!isClient && !isWorker)
       return
+
     close()
     explicitlyClosed = false
     retried = 0
