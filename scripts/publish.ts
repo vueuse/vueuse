47d806fd2ae137227e166200ff42fe00f16a0e8b
Anthony Fu <github@antfu.me>
2024-12-23 17:41:30 +0800

    chore: update release ci

--- scripts/publish.ts
+++ scripts/publish.ts
@@ -1,7 +1,7 @@
 import { execSync } from 'node:child_process'
 import { version } from '../package.json'
 
-let command = 'pnpm publish -r --access public'
+let command = 'pnpm publish -r --access public --no-git-checks'
 
 if (version.includes('beta'))
   command += ' --tag beta'
