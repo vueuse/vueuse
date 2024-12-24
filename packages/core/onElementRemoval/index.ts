2cb00740fc4948ec6975e756013c0e3016736aab
ben-lau <408736066@qq.com>
2024-12-24 19:02:39 +0800

    feat(onElementRemoval): add onElementRemoval function

---
+++ packages/core/onElementRemoval/index.ts
@@ -0,0 +1,65 @@
+import type { ConfigurableDocumentOrShadowRoot, ConfigurableWindow } from '../_configurable'
+import type { MaybeElementRef } from '../unrefElement'
+import { tryOnScopeDispose } from '@vueuse/shared'
+import { ref, watchEffect } from 'vue'
+import { defaultWindow } from '../_configurable'
+import { unrefElement } from '../unrefElement'
+import { useMutationObserver } from '../useMutationObserver'
+
+interface OnElementRemovalOptions extends ConfigurableWindow, ConfigurableDocumentOrShadowRoot { }
+
+interface StopHandle {
+  (): void
+}
+
+/**
+ * Fires when the element or any element containing it is removed.
+ *
+ * @param target
+ * @param callback
+ * @param options
+ */
+export function onElementRemoval(
+  target: MaybeElementRef,
+  callback: (mutationRecords: MutationRecord[]) => void,
+  options: OnElementRemovalOptions = {},
+) {
+  const { window = defaultWindow } = options
+  const document = options.document ?? window?.document
+  const stopFn = ref(() => { })
+  const cleanupAndUpdate = (fn = () => { }) => {
+    stopFn.value?.()
+    stopFn.value = fn
+  }
+
+  const stopWatch = watchEffect(() => {
+    const el = unrefElement(target)
+    if (el) {
+      const { stop } = useMutationObserver(document as any, (mutationsList) => {
+        const targetRemoved = mutationsList
+          .map(mutation => [...mutation.removedNodes])
+          .flat()
+          .some(node => node === el || node.contains(el))
+
+        if (targetRemoved) {
+          callback(mutationsList)
+        }
+      }, {
+        window,
+        childList: true,
+        subtree: true,
+      })
+
+      cleanupAndUpdate(stop)
+    }
+  })
+
+  const stopHandle: StopHandle = () => {
+    stopWatch()
+    cleanupAndUpdate()
+  }
+
+  tryOnScopeDispose(stopHandle)
+
+  return stopHandle
+}
