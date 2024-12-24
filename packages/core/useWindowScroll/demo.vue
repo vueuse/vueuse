858405096bc9e72159f5df66c7f1e89709461a24
Anthony Fu <github@antfu.me>
2024-12-24 12:40:03 +0800

    Update packages/core/useWindowScroll/demo.vue

--- packages/core/useWindowScroll/demo.vue
+++ packages/core/useWindowScroll/demo.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { useWindowScroll } from './index'
+import { useWindowScroll } from '@vueuse/core'
 
 const { x, y } = useWindowScroll({ behavior: 'smooth' })
 </script>
