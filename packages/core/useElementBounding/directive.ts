2122db0e2cb42c76b6771789cc442ccec0287188
pkc918 <xiaopan2000918@gmail.com>
2024-12-27 18:42:07 +0800

    fix: repair type

--- packages/core/useElementBounding/directive.ts
+++ packages/core/useElementBounding/directive.ts
@@ -1,23 +1,18 @@
 import type { ObjectDirective } from 'vue'
-import type { UseElementBoundingReturn } from '.'
+import type { UseElementBoundingOptions, UseElementBoundingReturn } from '.'
 import { watch } from 'vue'
 import { useElementBounding } from '.'
 
-type RemoveFirstFromTuple<T extends any[]> =
-  T['length'] extends 0 ? undefined : (((...b: T) => void) extends (a: any, ...b: infer I) => void ? I : [])
 type ElementBounding = Omit<UseElementBoundingReturn, 'update'>
-
-type BindingValueFunction = (size: ElementBounding) => void
-type VElementBoundingOptions = RemoveFirstFromTuple<Parameters<typeof useElementBounding>>
-type BindingValueArray = [BindingValueFunction, ...VElementBoundingOptions]
+type BindingValueFunction = (bounding: ElementBounding) => void
+type BindingValueArray = [BindingValueFunction, UseElementBoundingOptions]
 
 export const vElementBounding: ObjectDirective<
   HTMLElement,
   BindingValueFunction | BindingValueArray
 > = {
   mounted(el, binding) {
-    const handler = typeof binding.value === 'function' ? binding.value : binding.value?.[0]
-    const options = (typeof binding.value === 'function' ? [] : binding.value.slice(1)) as RemoveFirstFromTuple<BindingValueArray>
+    const [handler, options] = (typeof binding.value === 'function' ? [binding.value, {}] : binding.value) as BindingValueArray
 
     const {
       height,
@@ -28,7 +23,7 @@ export const vElementBounding: ObjectDirective<
       width,
       x,
       y,
-    } = useElementBounding(el, ...options)
+    } = useElementBounding(el, options)
     watch([height, bottom, left, right, top, width, x, y], ([height, bottom, left, right, top, width, x, y]) => handler({ height, bottom, left, right, top, width, x, y }))
   },
 }
