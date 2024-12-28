1d337647fbc2e11c66d141b6520dc8ffebd5957e
Robin Kehl <robin.kehl@singular-it.de>
2024-12-28 22:55:37 +0100

    test(useAnimation): add basic test

--- packages/core/useAnimate/index.browser.test.ts
+++ packages/core/useAnimate/index.browser.test.ts
@@ -1,11 +1,11 @@
 import { mount } from '@vue/test-utils'
 import { useAnimate } from '@vueuse/core'
-import { describe, expect, it } from 'vitest'
+import { describe, expect, it, vi } from 'vitest'
 import { shallowRef } from 'vue'
 // import { useAnimate } from '.'
 
 describe('useAnimate', () => {
-  it('the test environment does not support animate', () => {
+  it('browser should support useAnimate', () => {
     const wrapper = mount({
       template: '<p ref="el">test</p>',
       setup() {
@@ -18,6 +18,23 @@ describe('useAnimate', () => {
     const vm = wrapper.vm
 
     expect(vm.isSupported).toBe(true)
+    wrapper.unmount()
+  })
+
+  it('should be running', async () => {
+    const wrapper = mount({
+      template: '<p ref="el">test</p>',
+      setup() {
+        const el = shallowRef<HTMLElement>()
+        const animate = useAnimate(el, { transform: 'rotate(360deg)' }, 100)
+
+        return { ...animate, el }
+      },
+    })
+    const vm = wrapper.vm
+    await vi.waitFor(() => {
+      expect(vm.playState).toBe('running')
+    })
     wrapper.unmount()
   })
 })
