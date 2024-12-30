1409524c04df99a218e9a656e41f75c3f9c81fb8
pkc918 <xiaopan2000918@gmail.com>
2024-12-30 21:23:50 +0800

    fix: fix type

--- packages/core/useMouseInElement/directive.ts
+++ packages/core/useMouseInElement/directive.ts
@@ -1,11 +1,11 @@
-import type { ObjectDirective } from 'vue'
+import type { ObjectDirective, Reactive } from 'vue'
 import type { MouseInElementOptions, UseMouseInElementReturn } from '.'
 import { reactiveOmit } from '@vueuse/shared'
-import { watch } from 'vue'
+import { reactive, watch } from 'vue'
 import { useMouseInElement } from '.'
 
 type MouseInElement = Omit<UseMouseInElementReturn, 'stop'>
-type BindingValueFunction = (mouse: MouseInElement) => void
+type BindingValueFunction = (mouse: Reactive<MouseInElement>) => void
 type BindingValueArray = [BindingValueFunction, MouseInElementOptions]
 
 export const vMouseInElement: ObjectDirective<
@@ -15,7 +15,7 @@ export const vMouseInElement: ObjectDirective<
   mounted(el, binding) {
     const [handler, options] = (typeof binding.value === 'function' ? [binding.value, {}] : binding.value) as BindingValueArray
 
-    const state = reactiveOmit(useMouseInElement(el, options), 'stop') as MouseInElement
+    const state = reactiveOmit(reactive(useMouseInElement(el, options)), 'stop')
     watch(state, val => handler(val))
   },
 }
