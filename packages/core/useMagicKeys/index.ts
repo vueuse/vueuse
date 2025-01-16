e4b9d1272aec0de60ee2be03a709fcdfc1a1dd7c
babu-ch <mashirohira@gmail.com>
2025-01-16 23:05:28 +0900

    fix(useMagicKeys): fix key order issue on first use

--- packages/core/useMagicKeys/index.ts
+++ packages/core/useMagicKeys/index.ts
@@ -165,7 +165,7 @@ export function useMagicKeys(options: UseMagicKeysOptions<boolean> = {}): any {
         if (!(prop in refs)) {
           if (/[+_-]/.test(prop)) {
             const keys = prop.split(/[+_-]/g).map(i => i.trim())
-            refs[prop] = computed(() => keys.every(key => toValue(proxy[key])))
+            refs[prop] = computed(() => keys.map(key => toValue(proxy[key])).every(Boolean))
           }
           else {
             refs[prop] = ref(false)
