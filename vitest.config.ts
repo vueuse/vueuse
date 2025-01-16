315d7a765ef76180ae057e2c7b6fa4f9e5575b57
OrbisK <robin.kehl@singular-it.de>
2025-01-16 17:05:52 +0100

    chore(deps): update to vitest@3

--- vitest.config.ts
+++ vitest.config.ts
@@ -21,9 +21,64 @@ export default defineConfig({
     __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: 'true',
   },
   test: {
-    reporters: 'dot',
-    coverage: {
-      exclude: ['./packages/**/demo.vue'],
-    },
+    setupFiles: [resolve(__dirname, 'packages/.test/setup.ts')],
+    workspace: [
+      // matches every folder and file inside the `packages` folder
+      'packages/*/vitest.config.ts',
+      {
+        // add "extends: true" to inherit the options from the root config
+        extends: true,
+        test: {
+          alias: {
+            'vue': 'vue/dist/vue.esm-bundler.js',
+            '@vueuse/shared': resolve(__dirname, 'packages/shared/index.ts'),
+            '@vueuse/core': resolve(__dirname, 'packages/core/index.ts'),
+            '@vueuse/math': resolve(__dirname, 'packages/math/index.ts'),
+            '@vueuse/components': resolve(__dirname, 'packages/components/index.ts'),
+            '@vueuse/docs-utils': resolve(__dirname, 'packages/.vitepress/plugins/utils.ts'),
+          },
+          include: ['packages/**/*.browser.{test,spec}.ts'],
+          name: 'browser',
+          setupFiles: ['vitest-browser-vue'],
+          browser: {
+            enabled: true,
+            provider: 'playwright',
+            headless: true,
+            instances: [
+              { browser: 'chromium' },
+              { browser: 'firefox' },
+              // { browser: 'webkit' },
+            ],
+          },
+        },
+      },
+      {
+        extends: true,
+        test: {
+          name: 'unit',
+          environment: 'jsdom',
+          include: [
+            '!packages/**/*.browser.{test,spec}.ts',
+            'packages/**/*.{test,spec}.ts',
+          ],
+          alias: {
+            'vue': 'vue/dist/vue.esm-bundler.js',
+            '@vueuse/shared': resolve(__dirname, 'packages/shared/index.ts'),
+            '@vueuse/core': resolve(__dirname, 'packages/core/index.ts'),
+            '@vueuse/math': resolve(__dirname, 'packages/math/index.ts'),
+            '@vueuse/components': resolve(__dirname, 'packages/components/index.ts'),
+            '@vueuse/docs-utils': resolve(__dirname, 'packages/.vitepress/plugins/utils.ts'),
+          },
+          server: {
+            deps: {
+              inline: [
+                'vue',
+                'msw',
+              ],
+            },
+          },
+        },
+      },
+    ],
   },
 })
