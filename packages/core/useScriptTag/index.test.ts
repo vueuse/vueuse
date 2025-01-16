ddde136195c511706ed6daf2509137253b8da462
OrbisK <robin.kehl@singular-it.de>
2025-01-16 20:14:29 +0100

    Revert "chore: update `useScriptTag` tests to vitest v3"
    This reverts commit b816fc657d1f86a7a58c2cfd9194b591c2c768e5.

--- packages/core/useScriptTag/index.test.ts
+++ packages/core/useScriptTag/index.test.ts
@@ -1,196 +1,183 @@
-import { describe, expect, it, vi } from 'vitest'
+import { beforeEach, describe, expect, it, vi } from 'vitest'
+import { useScriptTag } from '.'
+import { useSetup } from '../../.test'
 
 describe('useScriptTag', () => {
-  it('a', () => {
-    const spyA = vi.spyOn(console, 'log')
+  const src = 'https://code.jquery.com/jquery-3.5.1.min.js'
 
-    expect(spyA).not.toBeCalled()
-    console.log('a')
-    expect(spyA).toBeCalled()
+  const scriptTagElement = (): HTMLScriptElement | null =>
+    document.head.querySelector(`script[src="${src}"]`)
+
+  beforeEach(() => {
+    document.head.innerHTML = ''
   })
 
-  it('b', () => {
-    const spyB = vi.spyOn(console, 'log')
-    spyB.mockClear()
-    expect(spyB).not.toBeCalled()
-    console.log('a')
-    expect(spyB).toBeCalled()
+  it('should add script tag', async () => {
+    const appendChildListener = vi.spyOn(document.head, 'appendChild')
+
+    expect(appendChildListener).not.toBeCalled()
+
+    expect(scriptTagElement()).toBeNull()
+
+    useSetup(() => {
+      const { scriptTag } = useScriptTag(src, () => {}, { immediate: true })
+
+      return {
+        scriptTag,
+      }
+    })
+
+    expect(appendChildListener).toBeCalled()
+
+    expect(scriptTagElement()).toBeInstanceOf(HTMLScriptElement)
   })
-  // const src = 'https://code.jquery.com/jquery-3.5.1.min.js'
-  //
-  // const scriptTagElement = (): HTMLScriptElement | null =>
-  //   document.head.querySelector(`script[src="${src}"]`)
-  //
-  // beforeEach(() => {
-  //   document.head.innerHTML = ''
-  // })
-  //
-  // it('should add script tag', async () => {
-  //   const appendChildListener = vi.spyOn(document.head, 'appendChild')
-  //
-  //   expect(appendChildListener).not.toBeCalled()
-  //
-  //   expect(scriptTagElement()).toBeNull()
-  //
-  //   useSetup(() => {
-  //     const { scriptTag } = useScriptTag(src, () => {}, { immediate: true })
-  //
-  //     return {
-  //       scriptTag,
-  //     }
-  //   })
-  //
-  //   expect(appendChildListener).toBeCalled()
-  //
-  //   expect(scriptTagElement()).toBeInstanceOf(HTMLScriptElement)
-  // })
-  //
-  // it('should re-use the same src for multiple loads', async () => {
-  //   const addChildListener = vi.spyOn(document.head, 'appendChild')
-  //
-  //   expect(addChildListener).not.toBeCalled()
-  //
-  //   expect(scriptTagElement()).toBeNull()
-  //
-  //   const vm = useSetup(() => {
-  //     const script1 = useScriptTag(src, () => {}, { immediate: false, manual: true })
-  //     const script2 = useScriptTag(src, () => {}, { immediate: false, manual: true })
-  //
-  //     return {
-  //       script1,
-  //       script2,
-  //     }
-  //   })
-  //
-  //   await vm.script1.load(false)
-  //   await vm.script2.load(false)
-  //
-  //   expect(vm.script1.scriptTag.value).not.toBeNull()
-  //   expect(vm.script2.scriptTag.value).not.toBeNull()
-  //
-  //   expect(addChildListener).toBeCalledTimes(1)
-  //   expect(scriptTagElement()).toBeInstanceOf(HTMLScriptElement)
-  // })
-  //
-  // it('should support custom attributes', async () => {
-  //   const appendChildListener = vi.spyOn(document.head, 'appendChild')
-  //
-  //   expect(appendChildListener).not.toBeCalled()
-  //
-  //   expect(scriptTagElement()).toBeNull()
-  //
-  //   useSetup(() => {
-  //     const { scriptTag } = useScriptTag(src, () => {}, {
-  //       attrs: { 'id': 'id-value', 'data-test': 'data-test-value' },
-  //       immediate: true,
-  //     })
-  //
-  //     return {
-  //       scriptTag,
-  //     }
-  //   })
-  //
-  //   expect(appendChildListener).toBeCalled()
-  //
-  //   const element = scriptTagElement()
-  //   expect(element).toBeInstanceOf(HTMLScriptElement)
-  //   expect(element?.getAttribute('id')).toBe('id-value')
-  //   expect(element?.getAttribute('data-test')).toBe('data-test-value')
-  // })
-  //
-  // it('should remove script tag on unmount', async () => {
-  //   const removeChildListener = vi.spyOn(document.head, 'removeChild')
-  //
-  //   expect(removeChildListener).not.toBeCalled()
-  //
-  //   expect(scriptTagElement()).toBeNull()
-  //
-  //   const vm = useSetup(() => {
-  //     const { scriptTag, load, unload } = useScriptTag(src, () => {}, { immediate: false })
-  //
-  //     return {
-  //       scriptTag,
-  //       load,
-  //       unload,
-  //     }
-  //   })
-  //
-  //   await vm.load(false)
-  //
-  //   expect(scriptTagElement()).toBeInstanceOf(HTMLScriptElement)
-  //
-  //   vm.unmount()
-  //
-  //   expect(scriptTagElement()).toBeNull()
-  //
-  //   expect(removeChildListener).toBeCalled()
-  //
-  //   expect(vm.scriptTag).toBeNull()
-  // })
-  //
-  // it('should remove script tag on unload call', async () => {
-  //   const removeChildListener = vi.spyOn(document.head, 'removeChild')
-  //
-  //   expect(removeChildListener).not.toBeCalled()
-  //
-  //   expect(scriptTagElement()).toBeNull()
-  //
-  //   const vm = useSetup(() => {
-  //     const {
-  //       scriptTag,
-  //       load,
-  //       unload,
-  //     } = useScriptTag(src, () => {}, { immediate: false })
-  //
-  //     return {
-  //       scriptTag,
-  //       load,
-  //       unload,
-  //     }
-  //   })
-  //
-  //   await vm.load(false)
-  //
-  //   expect(scriptTagElement()).toBeInstanceOf(HTMLScriptElement)
-  //
-  //   await vm.unload()
-  //
-  //   expect(scriptTagElement()).toBeNull()
-  //
-  //   expect(removeChildListener).toBeCalled()
-  //
-  //   expect(vm.scriptTag).toBeNull()
-  // })
-  //
-  // it('should remove script tag on unload call after multiple loads', async () => {
-  //   const removeChildListener = vi.spyOn(document.head, 'removeChild')
-  //
-  //   expect(removeChildListener).not.toBeCalled()
-  //
-  //   expect(scriptTagElement()).toBeNull()
-  //
-  //   const vm = useSetup(() => {
-  //     const script1 = useScriptTag(src, () => {}, { immediate: false, manual: true })
-  //     const script2 = useScriptTag(src, () => {}, { immediate: false, manual: true })
-  //
-  //     return {
-  //       script1,
-  //       script2,
-  //     }
-  //   })
-  //
-  //   // Multiple Loads
-  //   await vm.script1.load(false)
-  //   await vm.script2.load(false)
-  //
-  //   expect(scriptTagElement()).toBeInstanceOf(HTMLScriptElement)
-  //
-  //   vm.script1.unload()
-  //   vm.script2.unload()
-  //
-  //   expect(vm.script1.scriptTag.value).toBeNull()
-  //   expect(vm.script2.scriptTag.value).toBeNull()
-  //   expect(removeChildListener).toBeCalledTimes(1)
-  //   expect(scriptTagElement()).toBeNull()
-  // })
+
+  it('should re-use the same src for multiple loads', async () => {
+    const addChildListener = vi.spyOn(document.head, 'appendChild')
+
+    expect(addChildListener).not.toBeCalled()
+
+    expect(scriptTagElement()).toBeNull()
+
+    const vm = useSetup(() => {
+      const script1 = useScriptTag(src, () => {}, { immediate: false, manual: true })
+      const script2 = useScriptTag(src, () => {}, { immediate: false, manual: true })
+
+      return {
+        script1,
+        script2,
+      }
+    })
+
+    await vm.script1.load(false)
+    await vm.script2.load(false)
+
+    expect(vm.script1.scriptTag.value).not.toBeNull()
+    expect(vm.script2.scriptTag.value).not.toBeNull()
+
+    expect(addChildListener).toBeCalledTimes(1)
+    expect(scriptTagElement()).toBeInstanceOf(HTMLScriptElement)
+  })
+
+  it('should support custom attributes', async () => {
+    const appendChildListener = vi.spyOn(document.head, 'appendChild')
+
+    expect(appendChildListener).not.toBeCalled()
+
+    expect(scriptTagElement()).toBeNull()
+
+    useSetup(() => {
+      const { scriptTag } = useScriptTag(src, () => {}, {
+        attrs: { 'id': 'id-value', 'data-test': 'data-test-value' },
+        immediate: true,
+      })
+
+      return {
+        scriptTag,
+      }
+    })
+
+    expect(appendChildListener).toBeCalled()
+
+    const element = scriptTagElement()
+    expect(element).toBeInstanceOf(HTMLScriptElement)
+    expect(element?.getAttribute('id')).toBe('id-value')
+    expect(element?.getAttribute('data-test')).toBe('data-test-value')
+  })
+
+  it('should remove script tag on unmount', async () => {
+    const removeChildListener = vi.spyOn(document.head, 'removeChild')
+
+    expect(removeChildListener).not.toBeCalled()
+
+    expect(scriptTagElement()).toBeNull()
+
+    const vm = useSetup(() => {
+      const { scriptTag, load, unload } = useScriptTag(src, () => {}, { immediate: false })
+
+      return {
+        scriptTag,
+        load,
+        unload,
+      }
+    })
+
+    await vm.load(false)
+
+    expect(scriptTagElement()).toBeInstanceOf(HTMLScriptElement)
+
+    vm.unmount()
+
+    expect(scriptTagElement()).toBeNull()
+
+    expect(removeChildListener).toBeCalled()
+
+    expect(vm.scriptTag).toBeNull()
+  })
+
+  it('should remove script tag on unload call', async () => {
+    const removeChildListener = vi.spyOn(document.head, 'removeChild')
+
+    expect(removeChildListener).not.toBeCalled()
+
+    expect(scriptTagElement()).toBeNull()
+
+    const vm = useSetup(() => {
+      const {
+        scriptTag,
+        load,
+        unload,
+      } = useScriptTag(src, () => {}, { immediate: false })
+
+      return {
+        scriptTag,
+        load,
+        unload,
+      }
+    })
+
+    await vm.load(false)
+
+    expect(scriptTagElement()).toBeInstanceOf(HTMLScriptElement)
+
+    await vm.unload()
+
+    expect(scriptTagElement()).toBeNull()
+
+    expect(removeChildListener).toBeCalled()
+
+    expect(vm.scriptTag).toBeNull()
+  })
+
+  it('should remove script tag on unload call after multiple loads', async () => {
+    const removeChildListener = vi.spyOn(document.head, 'removeChild')
+
+    expect(removeChildListener).not.toBeCalled()
+
+    expect(scriptTagElement()).toBeNull()
+
+    const vm = useSetup(() => {
+      const script1 = useScriptTag(src, () => {}, { immediate: false, manual: true })
+      const script2 = useScriptTag(src, () => {}, { immediate: false, manual: true })
+
+      return {
+        script1,
+        script2,
+      }
+    })
+
+    // Multiple Loads
+    await vm.script1.load(false)
+    await vm.script2.load(false)
+
+    expect(scriptTagElement()).toBeInstanceOf(HTMLScriptElement)
+
+    vm.script1.unload()
+    vm.script2.unload()
+
+    expect(vm.script1.scriptTag.value).toBeNull()
+    expect(vm.script2.scriptTag.value).toBeNull()
+    expect(removeChildListener).toBeCalledTimes(1)
+    expect(scriptTagElement()).toBeNull()
+  })
 })
