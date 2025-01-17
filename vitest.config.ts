2bc97511071427265ce4b8f6886f6e7aef7fab21
OrbisK <robin.kehl@singular-it.de>
2025-01-17 10:13:05 +0100

    chore: update vitest filtering and config

--- vitest.config.ts
+++ vitest.config.ts
@@ -21,51 +21,9 @@ export default defineConfig({
     __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: 'true',
   },
   test: {
-    setupFiles: [resolve(__dirname, 'packages/.test/setup.ts')],
-    workspace: [
-      // matches every folder and file inside the `packages` folder
-      'packages/*/vitest.config.ts',
-      {
-        // add "extends: true" to inherit the options from the root config
-        extends: true,
-        test: {
-          alias: {
-            vue: 'vue/dist/vue.esm-bundler.js',
-          },
-          include: ['packages/**/*.browser.{test,spec}.ts'],
-          name: 'browser',
-          setupFiles: ['vitest-browser-vue'],
-          browser: {
-            enabled: true,
-            provider: 'playwright',
-            headless: true,
-            instances: [
-              { browser: 'chromium' },
-              { browser: 'firefox' },
-              // { browser: 'webkit' },
-            ],
-          },
-        },
-      },
-      {
-        extends: true,
-        test: {
-          name: 'unit',
-          environment: 'jsdom',
-          include: [
-            '!packages/**/*.browser.{test,spec}.ts',
-            'packages/**/*.{test,spec}.ts',
-          ],
-          server: {
-            deps: {
-              inline: [
-                'vue',
-                'msw',
-              ],
-            },
-          },
-        },
-      },
-    ],
+    reporters: 'dot',
+    coverage: {
+      exclude: ['./packages/**/demo.vue'],
+    },
   },
 })
