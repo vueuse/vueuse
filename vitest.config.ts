215914ca57b11e4b443b5ce46d1b99e2f6c8d49f
OrbisK <robin.kehl@singular-it.de>
2025-01-17 16:09:42 +0100

    chore: inline workspace config

--- vitest.config.ts
+++ vitest.config.ts
@@ -25,5 +25,52 @@ export default defineConfig({
     coverage: {
       exclude: ['./packages/**/demo.vue'],
     },
+    workspace: [
+      'packages/*/vitest.config.ts',
+      {
+        // add "extends: true" to inherit the options from the root config
+        extends: './vitest.config.ts',
+        resolve: {
+          alias: {
+            vue: 'vue/dist/vue.esm-bundler.js',
+          },
+        },
+        test: {
+          include: ['packages/**/*.browser.{test,spec}.ts'],
+          name: 'browser',
+          setupFiles: ['vitest-browser-vue'],
+          browser: {
+            enabled: true,
+            provider: 'playwright',
+            headless: true,
+            instances: [
+              { browser: 'chromium' },
+              { browser: 'firefox' },
+              // { browser: 'webkit' },
+            ],
+          },
+        },
+      },
+      {
+        extends: './vitest.config.ts',
+        test: {
+          name: 'unit',
+          environment: 'jsdom',
+          setupFiles: [resolve(__dirname, 'packages/.test/setup.ts')],
+          include: [
+            '!packages/**/*.browser.{test,spec}.ts',
+            'packages/**/*.{test,spec}.ts',
+          ],
+          server: {
+            deps: {
+              inline: [
+                'vue',
+                'msw',
+              ],
+            },
+          },
+        },
+      },
+    ],
   },
 })
