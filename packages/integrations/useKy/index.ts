70670e179cf8309bb64cac89eaf792445415b4b0
IlyaL <a25690756@gmail.com>
2025-01-21 19:07:07 +0800

    initial

---
+++ packages/integrations/useKy/index.ts
@@ -0,0 +1,15 @@
+export interface UseKyReturn {
+}
+
+export interface UseKyOptionsBase {
+}
+
+/**
+ * Wrapper for ky.
+ *
+ * @see https://vueuse.org/useKy
+ */
+export function useKy(): UseKyReturn {
+  // TODO: implement
+  return {}
+}
