18b3c7b3b0a0049ddbdab5070aa0df1bcbad828a
Robin <robin.kehl@singular-it.de>
2025-01-28 18:08:31 +0100

    test(useCountdown): replace `promiseTimeout` with `advanceTimersByTime` (#4524)

--- packages/core/useCountdown/index.test.ts
+++ packages/core/useCountdown/index.test.ts
@@ -1,11 +1,11 @@
 import type { Pausable } from '@vueuse/shared'
 import type { UseCountdownOptions } from '.'
-import { promiseTimeout } from '@vueuse/shared'
 import { beforeEach, describe, expect, it, vi } from 'vitest'
 import { effectScope } from 'vue'
 import { useCountdown } from '.'
 
 describe('useCountdown', () => {
+  vi.useFakeTimers()
   let tickCallback = vi.fn()
   let completeCallback = vi.fn()
   let countdown = 3
@@ -33,22 +33,22 @@ describe('useCountdown', () => {
   async function exec({ isActive, pause, resume }: Pausable) {
     expect(isActive.value).toBeTruthy()
     expect(completeCallback).toHaveBeenCalledTimes(0)
-    await promiseTimeout(110)
+    vi.advanceTimersByTime(110)
     expect(tickCallback).toHaveBeenCalledTimes(1)
 
     pause()
     expect(isActive.value).toBeFalsy()
 
-    await promiseTimeout(110)
+    vi.advanceTimersByTime(110)
     expect(tickCallback).toHaveBeenCalledTimes(1)
 
     resume()
     expect(isActive.value).toBeTruthy()
 
-    await promiseTimeout(110)
+    vi.advanceTimersByTime(110)
     expect(tickCallback).toHaveBeenCalledTimes(2)
 
-    await promiseTimeout(110)
+    vi.advanceTimersByTime(110)
     expect(tickCallback).toHaveBeenCalledTimes(3)
     expect(completeCallback).toHaveBeenCalledTimes(1)
   }
@@ -58,14 +58,14 @@ describe('useCountdown', () => {
     expect(isActive.value).toBeTruthy()
     expect(completeCallback).toHaveBeenCalledTimes(0)
 
-    await promiseTimeout(110)
+    vi.advanceTimersByTime(110)
 
     expect(tickCallback).toHaveBeenCalledTimes(1)
     expect(completeCallback).toHaveBeenCalledTimes(0)
 
     stop()
     expect(isActive.value).toBeFalsy()
-    await promiseTimeout(110)
+    vi.advanceTimersByTime(110)
 
     expect(tickCallback).toHaveBeenCalledTimes(1)
     expect(remaining.value).toBe(countdown)
@@ -76,14 +76,14 @@ describe('useCountdown', () => {
     start()
 
     expect(isActive.value).toBeTruthy()
-    await promiseTimeout(210)
+    vi.advanceTimersByTime(210)
 
     expect(tickCallback).toHaveBeenCalledTimes(2)
     expect(completeCallback).toHaveBeenCalledTimes(0)
 
     expect(remaining.value).toBe(1)
 
-    await promiseTimeout(110)
+    vi.advanceTimersByTime(110)
     expect(remaining.value).toBe(0)
     expect(completeCallback).toHaveBeenCalledTimes(1)
   })
@@ -98,8 +98,8 @@ describe('useCountdown', () => {
       await exec(useCountdown(countdown, options))
     })
     tickCallback.mockClear()
-    await scope.stop()
-    await promiseTimeout(300)
+    scope.stop()
+    vi.advanceTimersByTime(300)
     expect(tickCallback).toHaveBeenCalledTimes(0)
   })
 
@@ -107,7 +107,7 @@ describe('useCountdown', () => {
     const { isActive } = useCountdown(5, { interval: -1 })
 
     expect(isActive.value).toBeFalsy()
-    await promiseTimeout(60)
+    vi.advanceTimersByTime(60)
     expect(tickCallback).toHaveBeenCalledTimes(0)
   })
 })
