78889b2cc92f2ba6724f145de0611c4f40f418a7
Isi Mehmeti <49912060+isimehmeti@users.noreply.github.com>
2025-01-31 14:14:43 +0100

    fix(createEventHook): type check for multiple arguments when using tuples

--- packages/shared/createEventHook/index.ts
+++ packages/shared/createEventHook/index.ts
@@ -8,16 +8,18 @@ import { tryOnScopeDispose } from '../tryOnScopeDispose'
 // any extends void = true
 // so we need to check if T is any first
 type Callback<T> = IsAny<T> extends true
-  ? (...param: any) => void
+  ? (...param: any) => void 
   : (
       [T] extends [void]
         ? (...param: unknown[]) => void
+        : T extends any[] 
+        ? (...param: T) => void 
         : (...param: [T, ...unknown[]]) => void
     )
 
 export type EventHookOn<T = any> = (fn: Callback<T>) => { off: () => void }
 export type EventHookOff<T = any> = (fn: Callback<T>) => void
-export type EventHookTrigger<T = any> = (...param: IsAny<T> extends true ? unknown[] : [T, ...unknown[]]) => Promise<unknown[]>
+export type EventHookTrigger<T = any> = (...param: Parameters<Callback<T>>) => Promise<unknown[]>
 
 export interface EventHook<T = any> {
   on: EventHookOn<T>
@@ -54,7 +56,7 @@ export function createEventHook<T = any>(): EventHook<T> {
   }
 
   const trigger: EventHookTrigger<T> = (...args) => {
-    return Promise.all(Array.from(fns).map(fn => fn(...(args as [T, ...unknown[]]))))
+    return Promise.all(Array.from(fns).map(fn => fn(...args)))
   }
 
   return {
