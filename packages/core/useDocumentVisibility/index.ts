a0756740979e65db6788fb1585b633fe8ae45759
James Garbutt <43081j@users.noreply.github.com>
2025-02-15 18:50:20 +0000

    refactor(core/useDocumentVisibility): use `shallowRef` for primitives
    Uses `shallowRef` for `visibilityState` since it is a string.

--- packages/core/useDocumentVisibility/index.ts
+++ packages/core/useDocumentVisibility/index.ts
@@ -1,6 +1,6 @@
 import type { Ref } from 'vue'
 import type { ConfigurableDocument } from '../_configurable'
-import { ref as deepRef, shallowRef } from 'vue'
+import { shallowRef } from 'vue'
 import { defaultDocument } from '../_configurable'
 import { useEventListener } from '../useEventListener'
 
@@ -14,7 +14,7 @@ export function useDocumentVisibility(options: ConfigurableDocument = {}): Ref<D
   if (!document)
     return shallowRef('visible')
 
-  const visibility = deepRef(document.visibilityState)
+  const visibility = shallowRef(document.visibilityState)
 
   useEventListener(document, 'visibilitychange', () => {
     visibility.value = document.visibilityState
