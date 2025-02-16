67a0b39751e61ac26ce6037fd4d48f6dc06cbcb1
IlyaL <a25690756@gmail.com>
2025-02-16 12:51:53 +0800

    refactor(useStorage): rename `ref` usage to `deepRef`

--- packages/core/useStorage/index.ts
+++ packages/core/useStorage/index.ts
@@ -2,7 +2,7 @@ import type { Awaitable, ConfigurableEventFilter, ConfigurableFlush, MaybeRefOrG
 import type { ConfigurableWindow } from '../_configurable'
 import type { StorageLike } from '../ssr-handlers'
 import { pausableWatch, tryOnMounted } from '@vueuse/shared'
-import { computed, nextTick, ref, shallowRef, toValue, watch } from 'vue'
+import { computed, ref as deepRef, nextTick, shallowRef, toValue, watch } from 'vue'
 import { defaultWindow } from '../_configurable'
 import { getSSRHandler } from '../ssr-handlers'
 import { useEventListener } from '../useEventListener'
@@ -153,7 +153,7 @@ export function useStorage<T extends (string | number | boolean | object | null)
     initOnMounted,
   } = options
 
-  const data = (shallow ? shallowRef : ref)(typeof defaults === 'function' ? defaults() : defaults) as RemovableRef<T>
+  const data = (shallow ? shallowRef : deepRef)(typeof defaults === 'function' ? defaults() : defaults) as RemovableRef<T>
   const keyComputed = computed<string>(() => toValue(key))
 
   if (!storage) {
