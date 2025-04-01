41b8c568a5700387a0fc7a75e11bc7105680e898
M.Cozzolino <michaelcozzolino@outlook.it>
2025-04-01 10:11:02 +0200

    fix(useSortable): types

--- packages/integrations/useSortable/index.ts
+++ packages/integrations/useSortable/index.ts
@@ -1,7 +1,7 @@
 import type { ConfigurableDocument, MaybeElementRef } from '@vueuse/core'
 import type { Options } from 'sortablejs'
-import type { ComponentPublicInstance, MaybeRef, MaybeRefOrGetter } from 'vue'
-import { defaultDocument, tryOnMounted, tryOnScopeDispose, unrefElement } from '@vueuse/core'
+import type { MaybeRef, MaybeRefOrGetter } from 'vue'
+import { defaultDocument, tryOnMounted, tryOnScopeDispose } from '@vueuse/core'
 import Sortable from 'sortablejs'
 import { isRef, nextTick, toValue } from 'vue'
 
@@ -27,7 +27,7 @@ export type UseSortableOptions = Options & ConfigurableDocument
 
 export function useSortable<T>(selector: string, list: MaybeRefOrGetter<T[]>,
   options?: UseSortableOptions): UseSortableReturn
-export function useSortable<T>(el: MaybeElementRef<HTMLElement | ComponentPublicInstance | null | undefined>, list: MaybeRefOrGetter<T[]>,
+export function useSortable<T>(el: MaybeElementRef | null | undefined, list: MaybeRefOrGetter<T[]>,
   options?: UseSortableOptions): UseSortableReturn
 
 /**
@@ -37,7 +37,7 @@ export function useSortable<T>(el: MaybeElementRef<HTMLElement | ComponentPublic
  * @param options
  */
 export function useSortable<T>(
-  el: MaybeElementRef<HTMLElement | ComponentPublicInstance | null | undefined> | string,
+  el: MaybeRefOrGetter<MaybeElementRef | null | undefined> | string,
   list: MaybeRefOrGetter<T[]>,
   options: UseSortableOptions = {},
 ): UseSortableReturn {
@@ -52,7 +52,7 @@ export function useSortable<T>(
   }
 
   const start = () => {
-    const target = (typeof el === 'string' ? document?.querySelector(el) : unrefElement(el))
+    const target = (typeof el === 'string' ? document?.querySelector(el) : toValue(el))
     if (!target || sortable !== undefined)
       return
     sortable = new Sortable(target as HTMLElement, { ...defaultOptions, ...resetOptions })
