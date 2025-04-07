5edb516c8138df0afc834760166f8f7b8a2d0013
M.Cozzolino <michaelcozzolino@outlook.it>
2025-04-07 15:18:25 +0200

    fix: typo

--- packages/core/unrefElement/index.ts
+++ packages/core/unrefElement/index.ts
@@ -5,6 +5,7 @@ export type VueInstance = ComponentPublicInstance
 export type MaybeElementRef<T extends MaybeElement = MaybeElement> = MaybeRef<T>
 export type MaybeComputedElementRef<T extends MaybeElement = MaybeElement> = MaybeRefOrGetter<T>
 export type MaybeElement = HTMLElement | SVGElement | VueInstance | undefined | null
+
 export type UnRefElementReturn<T extends MaybeElement = MaybeElement> = T extends VueInstance ? Exclude<MaybeElement, VueInstance> : T | undefined
 
 /**
