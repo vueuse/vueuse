765a266382c19df9a20e6a99cf159c61917041b5
phk422 <penghongkun422@gmail.com>
2025-04-23 17:06:23 +0800

    chore(refManualReset): mirror refAutoReset's API

--- packages/shared/refManualReset/index.ts
+++ packages/shared/refManualReset/index.ts
@@ -1,6 +1,6 @@
 import type { MaybeRefOrGetter, Ref } from 'vue'
 import type { Fn } from '../utils'
-import { ref as deepRef, toValue } from 'vue'
+import { customRef, toValue } from 'vue'
 
 /**
  * Define the shape of a ref that supports manual reset functionality.
@@ -19,12 +19,31 @@ export interface ManualResetRefReturn<T> extends Ref<T> {
  * @param defaultValue The value which will be set.
  */
 export function refManualReset<T>(defaultValue: MaybeRefOrGetter<T>) {
-  const value: T = toValue(defaultValue)
-  const ret = deepRef(value) as unknown as ManualResetRefReturn<T>
-  ret.reset = () => {
-    ret.value = value
+  let value: T = toValue(defaultValue)
+  let trigger: () => void
+
+  const reset = () => {
+    value = toValue(defaultValue)
+    trigger()
   }
-  return ret
+
+  const refValue = customRef<T>((track, _trigger) => {
+    trigger = _trigger
+    return {
+      get() {
+        track()
+        return value
+      },
+      set(newValue) {
+        value = newValue
+        trigger()
+      },
+    }
+  }) as ManualResetRefReturn<T>
+
+  refValue.reset = reset
+
+  return refValue
 }
 
 // alias
