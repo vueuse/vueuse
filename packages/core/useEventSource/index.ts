4ad2bdff20117cee0ac8d1bd615f5e51bf8af671
OrbisK <robin.kehl@singular-it.de>
2025-04-24 08:50:03 +0200

    fix(useEventSource): add missing data generic
    fixes #4725

--- packages/core/useEventSource/index.ts
+++ packages/core/useEventSource/index.ts
@@ -114,7 +114,7 @@ export function useEventSource<Events extends string[], Data = any>(
   url: MaybeRefOrGetter<string | URL | undefined>,
   events: Events = [] as unknown as Events,
   options: UseEventSourceOptions = {},
-): UseEventSourceReturn<Events> {
+): UseEventSourceReturn<Events, Data> {
   const event: ShallowRef<string | null> = shallowRef(null)
   const data: ShallowRef<Data | null> = shallowRef(null)
   const status = shallowRef<EventSourceStatus>('CONNECTING')
