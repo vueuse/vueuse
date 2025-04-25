e7d655eb5569414120c61c935b2fcb375fa7f0ad
lin52025iq <87741822+lin52025iq@users.noreply.github.com>
2025-04-25 15:16:09 +0800

    fix: createSharedComposable dispose

--- packages/shared/createSharedComposable/index.ts
+++ packages/shared/createSharedComposable/index.ts
@@ -30,7 +30,9 @@ export function createSharedComposable<Fn extends AnyFn>(composable: Fn): Shared
       scope = effectScope(true)
       state = scope.run(() => composable(...args))
     }
-    tryOnScopeDispose(dispose)
+    if(!tryOnScopeDispose(dispose)) {
+      dispose()
+    }
     return state
   })
 }
