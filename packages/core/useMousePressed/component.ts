1d4756a4e380438d6c6a2b03954659c11e39c813
IlyaL <ilyaliao324@gmail.com>
2025-05-27 16:53:58 +0800

    refactor: use shallowRef for element target (#4761)

--- packages/core/useMousePressed/component.ts
+++ packages/core/useMousePressed/component.ts
@@ -1,13 +1,13 @@
 import type { MousePressedOptions } from '@vueuse/core'
 import type { RenderableComponent } from '../types'
 import { useMousePressed } from '@vueuse/core'
-import { ref as deepRef, defineComponent, h, reactive } from 'vue'
+import { defineComponent, h, reactive, shallowRef } from 'vue'
 
 export const UseMousePressed = /* #__PURE__ */ defineComponent<Omit<MousePressedOptions, 'target'> & RenderableComponent>({
   name: 'UseMousePressed',
   props: ['touch', 'initialValue', 'as'] as unknown as undefined,
   setup(props, { slots }) {
-    const target = deepRef()
+    const target = shallowRef<HTMLDivElement>()
     const data = reactive(useMousePressed({ ...props, target }))
 
     return () => {
