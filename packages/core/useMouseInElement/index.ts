ccda14f60e4e019d2b6a13b692e8bbab3e8cc022
bgm.cod <bgm.codfish@gmail.com>
2025-05-28 17:38:09 +0800

    feat(useMouseInElement): Use useElementBounding internally, fixing the issue where element movement was not considered.

--- packages/core/useMouseInElement/index.ts
+++ packages/core/useMouseInElement/index.ts
@@ -1,10 +1,9 @@
 import type { MaybeElementRef } from '../unrefElement'
 import type { UseMouseOptions } from '../useMouse'
-import { shallowRef, watch } from 'vue'
-import { defaultWindow } from '../_configurable'
-import { unrefElement } from '../unrefElement'
-import { useEventListener } from '../useEventListener'
+import { computed, reactive, toRefs } from 'vue'
+import { useElementBounding } from '../useElementBounding'
 import { useMouse } from '../useMouse'
+import { useWindowScroll } from '../useWindowScroll'
 
 export interface MouseInElementOptions extends UseMouseOptions {
   handleOutside?: boolean
@@ -21,79 +20,32 @@ export function useMouseInElement(
   target?: MaybeElementRef,
   options: MouseInElementOptions = {},
 ) {
-  const {
-    handleOutside = true,
-    window = defaultWindow,
-  } = options
-  const type = options.type || 'page'
+  const windowScroll = reactive(useWindowScroll())
+  const mouse = reactive(useMouse(options))
+  const elementBounding = reactive(useElementBounding(target))
 
-  const { x, y, sourceType } = useMouse(options)
+  const isOutside = computed(() =>
+    mouse.x < elementBounding.x
+    || mouse.x > elementBounding.x + elementBounding.width
+    || mouse.y < elementBounding.y
+    || mouse.y > elementBounding.y + elementBounding.height,
+  )
 
-  const targetRef = shallowRef(target ?? window?.document.body)
-  const elementX = shallowRef(0)
-  const elementY = shallowRef(0)
-  const elementPositionX = shallowRef(0)
-  const elementPositionY = shallowRef(0)
-  const elementHeight = shallowRef(0)
-  const elementWidth = shallowRef(0)
-  const isOutside = shallowRef(true)
-
-  let stop = () => {}
-
-  if (window) {
-    stop = watch(
-      [targetRef, x, y],
-      () => {
-        const el = unrefElement(targetRef)
-        if (!el || !(el instanceof Element))
-          return
-
-        const {
-          left,
-          top,
-          width,
-          height,
-        } = el.getBoundingClientRect()
-
-        elementPositionX.value = left + (type === 'page' ? window.pageXOffset : 0)
-        elementPositionY.value = top + (type === 'page' ? window.pageYOffset : 0)
-        elementHeight.value = height
-        elementWidth.value = width
-
-        const elX = x.value - elementPositionX.value
-        const elY = y.value - elementPositionY.value
-        isOutside.value = width === 0 || height === 0
-          || elX < 0 || elY < 0
-          || elX > width || elY > height
-
-        if (handleOutside || !isOutside.value) {
-          elementX.value = elX
-          elementY.value = elY
-        }
-      },
-      { immediate: true },
-    )
-
-    useEventListener(
-      document,
-      'mouseleave',
-      () => isOutside.value = true,
-      { passive: true },
-    )
-  }
+  const result = computed(() => ({
+    x: mouse.x,
+    y: mouse.y,
+    sourceType: mouse.sourceType,
+    elementX: mouse.x - elementBounding.left - windowScroll.x,
+    elementY: mouse.y - elementBounding.top - windowScroll.y,
+    elementPositionX: elementBounding.x,
+    elementPositionY: elementBounding.y,
+    elementHeight: elementBounding.height,
+    elementWidth: elementBounding.width,
+    isOutside,
+  }))
 
   return {
-    x,
-    y,
-    sourceType,
-    elementX,
-    elementY,
-    elementPositionX,
-    elementPositionY,
-    elementHeight,
-    elementWidth,
-    isOutside,
-    stop,
+    ...toRefs(result.value),
   }
 }
 
