ccd93cba7fcb492ae94bea36c7f9b3d5beb087e5
Arthur Darkstone <arthurlli@qq.com>
2025-05-30 12:01:20 +0800

    feat: change rollup to rolldown

---
+++ packages/shared/rolldown.config.ts
@@ -0,0 +1,6 @@
+import { packages } from '../../meta/packages'
+import { createRolldownConfig } from '../../rolldown.config'
+
+export default createRolldownConfig(
+  packages.find(pkg => pkg.name === 'shared')!,
+)
