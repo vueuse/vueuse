5999fa3724d993945384db481e97f524cf6dca8b
Arthur Darkstone <arthurlli@qq.com>
2025-07-08 15:54:43 +0800

    fix(demo): remove interval from useTimestamp for simplicity

--- packages/core/useIdle/demo.vue
+++ packages/core/useIdle/demo.vue
@@ -4,10 +4,13 @@ import { computed } from 'vue'
 
 const { idle, lastActive } = useIdle(5000)
 
-const now = useTimestamp()
+const now = useTimestamp({ interval: 1000 })
 
-const idledFor = computed(() =>
-  Math.floor((now.value - lastActive.value) / 1000))
+const idledFor = computed(() => {
+  const active = Math.floor((now.value - lastActive.value) / 1000)
+
+  return active > 0 ? active : 0
+})
 </script>
 
 <template>
