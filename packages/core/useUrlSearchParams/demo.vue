80c7985ef97a5804893361083686b664d3d5cab3
Arthur Darkstone <arthurlli@qq.com>
2025-07-09 15:44:48 +0800

    chore: update demo for useUrlSearchParams example

--- packages/core/useUrlSearchParams/demo.vue
+++ packages/core/useUrlSearchParams/demo.vue
@@ -4,6 +4,12 @@ import { useUrlSearchParams } from '@vueuse/core'
 const params = useUrlSearchParams('history')
 params.foo = 'bar'
 params.vueuse = 'awesome'
+
+function handleAddParams() {
+  if (params.biz)
+    return
+  params.biz = 'biz'
+}
 </script>
 
 <template>
@@ -11,7 +17,12 @@ params.vueuse = 'awesome'
     <ul class="!m-0">
       <li v-for="key in Object.keys(params)" :key="key">
         {{ key }}={{ params[key] }}
+        <input v-model="params[key]" type="text" :placeholder="key">
       </li>
     </ul>
+
+    <button v-if="!params.biz" @click="handleAddParams">
+      Add Param
+    </button>
   </div>
 </template>
