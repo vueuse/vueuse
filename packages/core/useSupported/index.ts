878d00ae9ea05c5e75f6a9e791f3f0bca01a7a19
SerKo <serko.dev@gmail.com>
2025-07-24 22:00:55 +0800

    refactor: add treeshake annotation for `Utilities` category

--- packages/core/useSupported/index.ts
+++ packages/core/useSupported/index.ts
@@ -1,6 +1,7 @@
 import { computed } from 'vue'
 import { useMounted } from '../useMounted'
 
+/* @__NO_SIDE_EFFECTS__ */
 export function useSupported(callback: () => unknown) {
   const isMounted = useMounted()
 
