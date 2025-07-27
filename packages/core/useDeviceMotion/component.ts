5b1a3830cbff13bde9f98f6b0193d12601ced456
IlyaL <ilyaliao324@gmail.com>
2025-07-27 21:32:21 +0800

    refactor(UseDeviceMotion): enhance component with extended props and slots support

--- packages/core/useDeviceMotion/component.ts
+++ packages/core/useDeviceMotion/component.ts
@@ -1,14 +1,33 @@
+import type { DeviceMotionOptions, UseDeviceMotionReturn } from '@vueuse/core'
+import type { SlotsType } from 'vue'
 import { useDeviceMotion } from '@vueuse/core'
 import { defineComponent } from 'vue'
 
-export const UseDeviceMotion = /* #__PURE__ */ defineComponent({
-  name: 'UseDeviceMotion',
-  setup(props, { slots }) {
-    const data = useDeviceMotion()
+export interface UseDeviceMotionProps extends DeviceMotionOptions {}
+interface UseDeviceMotionSlots {
+  default: (data: UseDeviceMotionReturn) => any
+}
+
+export const UseDeviceMotion = /* #__PURE__ */ defineComponent<
+  UseDeviceMotionProps,
+  Record<string, never>,
+  string,
+  SlotsType<UseDeviceMotionSlots>
+>(
+  (props, { slots }) => {
+    const data = useDeviceMotion(props)
 
     return () => {
       if (slots.default)
         return slots.default(data)
     }
   },
-})
+  {
+    name: 'UseDeviceMotion',
+    props: [
+      'eventFilter',
+      'requestPermissions',
+      'window',
+    ],
+  },
+)
