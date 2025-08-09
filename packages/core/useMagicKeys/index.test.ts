61b962776f14d7f38094205951641dfabb3d5907
NoiseMacPro <noisefan@163.com>
2025-08-10 02:20:10 +0800

    test(core): Improve useMagicKeys unit tests
    - feat: alias mapping options
    - feat: use reactive mode
    - issues(#977): After solving the release Command, repeatedly pressing the Command will trigger repeatedly
    - issues(#1350): Target element out-of-focus and refocusing behavior

--- packages/core/useMagicKeys/index.test.ts
+++ packages/core/useMagicKeys/index.test.ts
@@ -22,6 +22,7 @@ describe('useMagicKeys', () => {
     dispatchEvent({ target, key: 'A' })
     expect(A.value).toBe(true)
   })
+
   it('multiple keys', async () => {
     const { Ctrl_Shift_Period } = useMagicKeys({ target })
     expect(Ctrl_Shift_Period.value).toBe(false)
@@ -35,6 +36,29 @@ describe('useMagicKeys', () => {
     dispatchEvent({ target, key: 'Period', ctrlKey: true, shiftKey: true })
     expect(Ctrl_Shift_Period.value).toBe(true)
   })
+
+  it('multiple keys(for MacOS meta won\'t trigger keyup)', async () => {
+    const { command, a } = useMagicKeys({ target })
+    expect(command.value).toBe(false)
+    expect(a.value).toBe(false)
+
+    dispatchEvent({ target, key: 'a' })
+    expect(a.value).toBe(true)
+
+    dispatchEvent({ target, key: 'Meta', metaKey: true })
+    expect(command.value).toBe(true)
+
+    dispatchEvent({ target, eventType: 'keyup', key: 'Meta', metaKey: true })
+    expect(command.value).toBe(false)
+    expect(a.value).toBe(false)
+
+    // #977
+    // After solving the release Command, repeatedly pressing the Command will trigger repeatedly
+    dispatchEvent({ target, key: 'Meta', metaKey: true })
+    expect(command.value).toBe(true)
+    expect(a.value).toBe(false)
+  })
+
   it('multiple keys(in a different order)', async () => {
     const { Ctrl_Shift_Period } = useMagicKeys({ target })
     expect(Ctrl_Shift_Period.value).toBe(false)
@@ -48,6 +72,7 @@ describe('useMagicKeys', () => {
     dispatchEvent({ target, key: 'Period', shiftKey: true, ctrlKey: true })
     expect(Ctrl_Shift_Period.value).toBe(true)
   })
+
   it('prevent incorrect clearing of other keys after releasing shift', async () => {
     const { v, u, e, shift } = useMagicKeys({ target })
 
@@ -68,15 +93,51 @@ describe('useMagicKeys', () => {
     expect(e.value).toBe(false)
     expect(shift.value).toBe(false)
   })
+
   it('current return value', async () => {
     const { v, current } = useMagicKeys({ target })
-    dispatchEvent({ target, key: 'V' })
+    dispatchEvent({ target, key: 'v' })
     expect(v.value).toBe(true)
     expect(current.has('v')).toBe(true)
   })
-  it('aliasMap option', async () => {
+
+  it('alias map option', async () => {
     const { ct } = useMagicKeys({ aliasMap: { ct: 'control' }, target })
     dispatchEvent({ target, key: 'Control', ctrlKey: true })
     expect(ct.value).toBe(true)
+  })
+
+  it('use reactive mode', async () => {
+    const keys = useMagicKeys({ target, reactive: true })
+    expect(keys.a).toBe(false)
+    dispatchEvent({ target, key: 'a' })
+
+    expect(keys.A).toBe(true)
+    expect(keys.current.has('a')).toBe(true)
+  })
+
+  it('target blur', async () => {
+    // #1350
+    const { alt_tab } = useMagicKeys({ target })
+    expect(alt_tab.value).toBe(false)
+
+    dispatchEvent({ target, key: 'Alt', altKey: true })
+    dispatchEvent({ target, key: 'Tab', altKey: true })
+    expect(alt_tab.value).toBe(true)
+
+    window.dispatchEvent(new Event('blur'))
+    expect(alt_tab.value).toBe(false)
+  })
+  it('target focus', async () => {
+    // #1350
+    const { alt_tab } = useMagicKeys({ target })
+    expect(alt_tab.value).toBe(false)
+
+    dispatchEvent({ target, key: 'Alt', altKey: true })
+    dispatchEvent({ target, key: 'Tab', altKey: true })
+    expect(alt_tab.value).toBe(true)
+
+    window.dispatchEvent(new Event('focus'))
+    expect(alt_tab.value).toBe(false)
   })
 })
