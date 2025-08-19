7d158db45ed7fbce91d454cb9271f25b48ed62ce
yosong <2404559603@qq.com>
2025-08-19 13:06:11 +0800

    test(useUrlSearchParams): add update status after pressing the forward or backward button of the browser test

--- packages/core/useUrlSearchParams/index.test.ts
+++ packages/core/useUrlSearchParams/index.test.ts
@@ -10,11 +10,18 @@ describe('useUrlSearchParams', () => {
     writable: true,
   })
 
+  let mockHistory: string[] = ['']
+  let mockCurrentHistoryIndex = 0
   const mockReplaceState = vi.fn()
-  const mockPushState = vi.fn()
+  const mockPushState = vi.fn((_state, _title, url: string) => {
+    mockHistory.push(url)
+    mockCurrentHistoryIndex++
+  })
 
   beforeEach(() => {
     vi.clearAllMocks()
+    mockHistory = ['']
+    mockCurrentHistoryIndex = 0
     window.location.search = ''
     window.location.hash = ''
     window.history.replaceState = mockReplaceState
@@ -31,6 +38,37 @@ describe('useUrlSearchParams', () => {
         hash,
       },
     }))
+  }
+
+  const mockBack = async (search: string, hash: string) => {
+    window.location.search = search
+    window.location.hash = hash
+    window.dispatchEvent(new PopStateEvent('popstate', {
+      state: {
+        ...window.location,
+        search,
+        hash,
+      },
+    }))
+
+    await nextTick()
+    mockHistory.pop()
+    mockCurrentHistoryIndex -= 2
+  }
+
+  const mockForward = async (search: string, hash: string) => {
+    window.location.search = search
+    window.location.hash = hash
+    window.dispatchEvent(new PopStateEvent('popstate', {
+      state: {
+        ...window.location,
+        search,
+        hash,
+      },
+    }))
+
+    await nextTick()
+    mockHistory.pop()
   }
 
   ([
@@ -250,38 +288,89 @@ describe('useUrlSearchParams', () => {
         const params = useUrlSearchParams(mode, { writeMode: 'push' })
         expect(params.foo).toBeUndefined()
         expect(params.bar).toBeUndefined()
+
         params.foo = 'first'
         await nextTick()
         params.bar = 'second'
         await nextTick()
-        window.history.go(-1)
         switch (mode) {
+          case 'history':
+            expect(mockHistory).toEqual(['', '/?foo=first', '/?foo=first&bar=second'])
+            expect(mockCurrentHistoryIndex).toBe(2)
+            break
           case 'hash':
-            mockPopstate('', '#/test/?foo=first')
+            expect(mockHistory).toEqual(['', '/#?foo=first', '/#?foo=first&bar=second'])
+            expect(mockCurrentHistoryIndex).toBe(2)
             break
           case 'hash-params':
-            mockPopstate('', '#foo=first')
+            expect(mockHistory).toEqual(['', '/#foo=first', '/#foo=first&bar=second'])
+            expect(mockCurrentHistoryIndex).toBe(2)
             break
-          case 'history':
-            mockPopstate('?foo=first', '')
         }
-        await nextTick()
-        expect(params.foo).toBe('first')
-        expect(params.bar).toBeUndefined()
-        window.history.go(1)
+
         switch (mode) {
           case 'hash':
-            mockPopstate('', '#/test/?foo=first&bar=second')
+            mockBack('', '#/test/?foo=first')
             break
           case 'hash-params':
-            mockPopstate('', '#foo=first&bar=second')
+            mockBack('', '#foo=first')
             break
           case 'history':
-            mockPopstate('?foo=first&bar=second', '')
+            mockBack('?foo=first', '')
         }
         await nextTick()
-        expect(params.foo).toBe('first')
-        expect(params.bar).toBe('second')
+        switch (mode) {
+          case 'history':
+            expect(mockHistory).toEqual(['', '/?foo=first', '/?foo=first&bar=second'])
+            expect(mockCurrentHistoryIndex).toBe(1)
+            expect(params.foo).toBe('first')
+            expect(params.bar).toBeUndefined()
+            break
+          case 'hash':
+            expect(mockHistory).toEqual(['', '/#?foo=first', '/#?foo=first&bar=second'])
+            expect(mockCurrentHistoryIndex).toBe(1)
+            expect(params.foo).toBe('first')
+            expect(params.bar).toBeUndefined()
+            break
+          case 'hash-params':
+            expect(mockHistory).toEqual(['', '/#foo=first', '/#foo=first&bar=second'])
+            expect(mockCurrentHistoryIndex).toBe(1)
+            expect(params.foo).toBe('first')
+            expect(params.bar).toBeUndefined()
+            break
+        }
+
+        switch (mode) {
+          case 'hash':
+            mockForward('', '#/test/?foo=first&bar=second')
+            break
+          case 'hash-params':
+            mockForward('', '#foo=first&bar=second')
+            break
+          case 'history':
+            mockForward('?foo=first&bar=second', '')
+        }
+        await nextTick()
+        switch (mode) {
+          case 'history':
+            expect(mockHistory).toEqual(['', '/?foo=first', '/?foo=first&bar=second'])
+            expect(mockCurrentHistoryIndex).toBe(2)
+            expect(params.foo).toBe('first')
+            expect(params.bar).toBe('second')
+            break
+          case 'hash':
+            expect(mockHistory).toEqual(['', '/#?foo=first', '/#?foo=first&bar=second'])
+            expect(mockCurrentHistoryIndex).toBe(2)
+            expect(params.foo).toBe('first')
+            expect(params.bar).toBe('second')
+            break
+          case 'hash-params':
+            expect(mockHistory).toEqual(['', '/#foo=first', '/#foo=first&bar=second'])
+            expect(mockCurrentHistoryIndex).toBe(2)
+            expect(params.foo).toBe('first')
+            expect(params.bar).toBe('second')
+            break
+        }
       })
     })
   })
