c7c194e41759bbdefee79fbeda316722afb6d0a1
Vida Xie <vida_2020@163.com>
2025-08-29 13:42:33 +0800

    test(createSharedComposable): add test case

---
+++ packages/shared/createSharedComposable/index.test.ts
@@ -0,0 +1,15 @@
+import { describe, expect, it } from 'vitest'
+import { shallowRef } from 'vue'
+import { createSharedComposable } from '.'
+
+describe('createSharedComposable', () => {
+  it('should return the sharedComposable', () => {
+    const useShared = createSharedComposable(() => shallowRef('data'))
+    const data1 = useShared()
+    const data2 = useShared()
+    data2.value = 'data2'
+
+    expect(data1.value).toBe('data2')
+    expect(data1).toBe(data2)
+  })
+})
