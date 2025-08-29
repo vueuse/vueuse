997ff6516178b88d0e1c8c8865c9f9e7b4815a87
Vida Xie <vida_2020@163.com>
2025-08-29 17:33:11 +0800

    fix: submodules build

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -17,8 +17,7 @@ export function createTsDownConfig(
   if (build === false)
     return []
 
-  const configs: UserConfig[] = []
-
+  const iifeName = 'VueUse'
   const iifeGlobals = {
     'vue': 'Vue',
     '@vueuse/shared': 'VueUse',
@@ -26,12 +25,24 @@ export function createTsDownConfig(
     ...(globals || {}),
   }
 
-  const entry = ['index.ts']
   const format: Format[] = []
+  if (mjs !== false) {
+    format.push('es')
+  }
 
-  const iifeName = 'VueUse'
+  let baseConfig: UserConfig = {
+    target,
+    dts,
+    platform: 'browser',
+    external: [
+      ...externals,
+      ...(external || []),
+    ],
+  }
+
+  const configs: UserConfig[] = []
+
   const functionNames = ['index']
-
   if (submodules) {
     functionNames.push(...globSync(
       '*/index.ts',
@@ -39,39 +50,39 @@ export function createTsDownConfig(
     ).map(i => i.split('/')[0]))
   }
 
-  if (mjs !== false) {
-    format.push('es')
-  }
-
-  configs.push({
-    entry,
-    target,
-    format,
-    dts,
-    copy,
-    external: [
-      ...externals,
-      ...(external || []),
-    ],
-  })
-
-  if (iife !== false) {
-    const baseIIFEConfig: UserConfig = {
-      entry,
-      format: 'iife',
-      name: iifeName,
-      alias: iifeGlobals,
-    }
-    configs.push(
-      baseIIFEConfig,
-      {
-        ...baseIIFEConfig,
-        minify: true,
-        outExtensions: () => ({
-          js: '.min.js',
-        }),
+  for (const fn of functionNames) {
+    baseConfig = {
+      ...baseConfig,
+      entry: {
+        [fn]: fn === 'index' ? 'index.ts' : `${fn}/index.ts`,
       },
-    )
+    }
+
+    configs.push({
+      ...baseConfig,
+      format,
+      copy,
+    })
+
+    if (iife !== false) {
+      const BASE_IIFE_CONFIG: UserConfig = {
+        ...baseConfig,
+        format: 'iife',
+        name: iifeName,
+        alias: iifeGlobals,
+      }
+
+      configs.push(
+        BASE_IIFE_CONFIG,
+        {
+          ...BASE_IIFE_CONFIG,
+          minify: true,
+          outExtensions: () => ({
+            js: '.min.js',
+          }),
+        },
+      )
+    }
   }
 
   return configs
