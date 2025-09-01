4152015e5825744fd39b8c71a47f406a86d1899c
kalu5 <451660550@qq.com>
2025-09-01 11:06:55 +0800

    fix(useMouseInElement): cleaning directives when components are unmounted(#5001)

--- packages/core/useMouseInElement/directive.test.ts
+++ packages/core/useMouseInElement/directive.test.ts
@@ -1,7 +1,7 @@
 import type { VueWrapper } from '@vue/test-utils'
 import { mount } from '@vue/test-utils'
 import { beforeEach, describe, expect, it, vi } from 'vitest'
-import { defineComponent } from 'vue'
+import { defineComponent, nextTick } from 'vue'
 import { vMouseInElement } from './directive'
 
 const App = defineComponent({
@@ -67,6 +67,52 @@ describe('vMouseInElement', () => {
 
     it('should be defined', () => {
       expect(wrapper).toBeDefined()
+    })
+  })
+
+  describe('cleaning directives when components are unmounted', () => {
+    beforeEach(() => {
+      onMouseInElement = vi.fn()
+
+      wrapper = mount(App, {
+        props: {
+          onMouseInElement,
+        },
+        global: {
+          directives: {
+            MouseInElement: vMouseInElement,
+          },
+        },
+      })
+    })
+
+    it('should be defined', () => {
+      expect(wrapper).toBeDefined()
+    })
+
+    it('should call callback when mouse moves into element', async () => {
+      await nextTick()
+      expect(onMouseInElement).toBeCalledTimes(0)
+      // Mock mouse position inside the element
+      const mockMouseEvent = new MouseEvent('mousemove', {
+        clientX: 100,
+        clientY: 100,
+      })
+      // Get the element and trigger mouse event
+      const element = wrapper.element.querySelector('div')
+      if (element) {
+        // Trigger mousemove event
+        window.dispatchEvent(mockMouseEvent)
+        await nextTick()
+      }
+      // Should be called after mouse movement triggers state change
+      expect(onMouseInElement).toBeCalledTimes(1)
+    })
+
+    it('should clean up the directives', async () => {
+      wrapper.unmount()
+      await nextTick()
+      expect(onMouseInElement).toBeCalledTimes(0)
     })
   })
 })
--- packages/core/useMouseInElement/directive.ts
+++ packages/core/useMouseInElement/directive.ts
@@ -1,7 +1,7 @@
-import type { ObjectDirective, Reactive } from 'vue'
+import type { EffectScope, ObjectDirective, Reactive } from 'vue'
 import type { MouseInElementOptions, UseMouseInElementReturn } from './index'
 import { reactiveOmit } from '@vueuse/shared'
-import { reactive, watch } from 'vue'
+import { effectScope, reactive, watch } from 'vue'
 import { useMouseInElement } from './index'
 
 type MouseInElement = Omit<UseMouseInElementReturn, 'stop'>
@@ -9,13 +9,21 @@ type BindingValueFunction = (mouse: Reactive<MouseInElement>) => void
 type BindingValueArray = [BindingValueFunction, MouseInElementOptions]
 
 export const vMouseInElement: ObjectDirective<
-  HTMLElement,
+  HTMLElement & { _scope: EffectScope | null },
   BindingValueFunction | BindingValueArray
 > = {
   mounted(el, binding) {
-    const [handler, options] = (typeof binding.value === 'function' ? [binding.value, {}] : binding.value) as BindingValueArray
+    const scope = effectScope()
+    el._scope = scope
+    el._scope.run(() => {
+      const [handler, options] = (typeof binding.value === 'function' ? [binding.value, {}] : binding.value) as BindingValueArray
 
-    const state = reactiveOmit(reactive(useMouseInElement(el, options)), 'stop')
-    watch(state, val => handler(val))
+      const state = reactiveOmit(reactive(useMouseInElement(el, options)), 'stop')
+      watch(state, val => handler(val))
+    })
+  },
+  unmounted(el) {
+    el._scope?.stop()
+    el._scope = null
   },
 }
