7a25b925fd9432c239e86a5c8bd94a4e3e0b8c12
siavava <amittaijoel@outlook.com>
2025-09-19 00:01:43 -0700

    feat(useMouseInElement): add support for tracking inline elements such as spans

--- packages/core/useMouseInElement/demo.vue
+++ packages/core/useMouseInElement/demo.vue
@@ -19,11 +19,37 @@ const stringify = reactify(
 const target = useTemplateRef<HTMLElement>('target')
 const mouse = reactive(useMouseInElement(target))
 const text = stringify(mouse)
+
+const inlineTarget = useTemplateRef<HTMLElement>('inline-target')
+const inlineMouse = reactive(useMouseInElement(inlineTarget))
+const inlineText = stringify(inlineMouse)
 </script>
 
 <template>
   <div flex="~" gap="4">
     <Area ref="target" />
     <pre lang="yaml">{{ text }}</pre>
+  </div>
+
+  <div flex="~" gap="4">
+    <div
+      class="el"
+      w="40"
+      h="40"
+      bg="gray-400/20"
+      border="rounded"
+      flex="~"
+      place="content-center"
+      select="none"
+      style="text-align: center"
+    >
+      <div m="auto">
+        <span>
+          Hover <span ref="inline-target" style="background: rgb(36, 146, 82, 0.5)">
+            the highlighted text to see the</span> tracking.
+        </span>
+      </div>
+    </div>
+    <pre lang="yaml">{{ inlineText }}</pre>
   </div>
 </template>
