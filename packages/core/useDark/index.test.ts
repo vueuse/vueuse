c7a829072dc68345b861fa9710ebf820edcaa8a7
Scott Bedard <scottbedard90@gmail.com>
2025-09-30 09:21:14 -0500

    use dark coverage

---
+++ packages/core/useDark/index.test.ts
@@ -0,0 +1,74 @@
+import { beforeEach, describe, expect, it } from 'vitest'
+import { nextTick } from 'vue'
+import { useDark } from './index'
+
+describe('useDark', () => {
+  beforeEach(() => {
+    localStorage.clear()
+  })
+
+  it('updates html element by default', async () => {
+    const toggle = useDark({
+      initialValue: 'light',
+    })
+
+    expect(toggle.value).toBe(false)
+    expect(document.documentElement.classList.contains('dark')).toBe(false)
+
+    toggle.value = true
+
+    await nextTick()
+
+    expect(document.documentElement.classList.contains('dark')).toBe(true)
+  })
+
+  it('updates custom element', async () => {
+    const toggle = useDark({
+      initialValue: 'light',
+      selector: 'body',
+    })
+
+    expect(toggle.value).toBe(false)
+    expect(document.body.classList.contains('dark')).toBe(false)
+
+    toggle.value = true
+
+    await nextTick()
+
+    expect(document.body.classList.contains('dark')).toBe(true)
+  })
+
+  it('updates custom storage key', async () => {
+    const toggle = useDark({
+      initialValue: 'light',
+      storageKey: 'custom-key',
+    })
+
+    expect(localStorage.getItem('custom-key')).toBe('light')
+
+    toggle.value = true
+
+    await nextTick()
+
+    expect(localStorage.getItem('custom-key')).toBe('dark')
+  })
+
+  it('sets custom class name', async () => {
+    const toggle = useDark({
+      initialValue: 'light',
+      valueDark: 'custom-dark',
+      valueLight: 'custom-light',
+    })
+
+    expect(toggle.value).toBe(false)
+    expect(document.documentElement.classList.contains('custom-dark')).toBe(false)
+    expect(document.documentElement.classList.contains('custom-light')).toBe(true)
+
+    toggle.value = true
+
+    await nextTick()
+
+    expect(document.documentElement.classList.contains('custom-dark')).toBe(true)
+    expect(document.documentElement.classList.contains('custom-light')).toBe(false)
+  })
+})
