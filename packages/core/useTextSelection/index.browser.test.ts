64b82f307818449c21937b292636e5f6a333757c
kevinluo201 <kevin.luo@hey.com>
2025-10-16 09:31:10 -0400

    test: add tests for initial selection

--- packages/core/useTextSelection/index.browser.test.ts
+++ packages/core/useTextSelection/index.browser.test.ts
@@ -16,27 +16,51 @@ describe('useTextSelection', () => {
     window.getSelection()?.removeAllRanges()
   })
 
-  it('should initialise', async () => {
+  const addNodeContentToRange = (node: HTMLElement) => {
+    const range = document.createRange()
+    range.selectNodeContents(node)
+    const selection = window.getSelection()!
+    selection.removeAllRanges()
+    selection.addRange(range)
+
+    document.dispatchEvent(new Event('selectionchange'))
+  }
+
+  it('should initialize with window.getSelection(), which is always present in browsers', async () => {
     const result = useTextSelection()
     expect(result.text.value).toBe('')
     expect(result.rects.value).toEqual([])
     expect(result.ranges.value).toEqual([])
-    expect(result.selection.value).toBe(null)
+    expect(result.selection.value).toBe(window.getSelection())
   })
 
-  it('should update on selection change', async () => {
-    const result = useTextSelection()
-    const range = document.createRange()
-    range.selectNodeContents(node)
-    const selection = window.getSelection()!
-    selection.addRange(range)
+  it('should initialize with an existing range', async () => {
+    addNodeContentToRange(node)
 
-    const event = new Event('selectionchange')
-    document.dispatchEvent(event)
+    const result = useTextSelection()
 
     expect(result.text.value).toBe('Hello World')
     expect(result.ranges.value.length).toBe(1)
     expect(result.rects.value.length).toBe(1)
-    expect(result.selection.value).toBe(selection)
+    expect(result.selection.value).toBe(window.getSelection())
+  })
+
+  it('should update on selectionchange', async () => {
+    const result = useTextSelection()
+
+    addNodeContentToRange(node)
+
+    expect(result.text.value).toBe('Hello World')
+    expect(result.ranges.value.length).toBe(1)
+    expect(result.rects.value.length).toBe(1)
+    expect(result.selection.value).toBe(window.getSelection())
+
+    window.getSelection()?.removeAllRanges()
+    document.dispatchEvent(new Event('selectionchange'))
+
+    expect(result.text.value).toBe('')
+    expect(result.ranges.value.length).toBe(0)
+    expect(result.rects.value.length).toBe(0)
+    expect(result.selection.value).toBe(window.getSelection())
   })
 })
