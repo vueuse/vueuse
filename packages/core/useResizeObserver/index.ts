753021d58287428d77cd9aa23c91d27547f8a64e
Vida Xie <vida_2020@163.com>
2025-10-30 23:19:39 +0800

    chore(types): remove embeded `ResizeObserverSize` types

--- packages/core/useResizeObserver/index.ts
+++ packages/core/useResizeObserver/index.ts
@@ -7,36 +7,7 @@ import { defaultWindow } from '../_configurable'
 import { unrefElement } from '../unrefElement'
 import { useSupported } from '../useSupported'
 
-export interface ResizeObserverSize {
-  readonly inlineSize: number
-  readonly blockSize: number
-}
-
-export interface ResizeObserverEntry {
-  readonly target: Element
-  readonly contentRect: DOMRectReadOnly
-  readonly borderBoxSize: ReadonlyArray<ResizeObserverSize>
-  readonly contentBoxSize: ReadonlyArray<ResizeObserverSize>
-  readonly devicePixelContentBoxSize: ReadonlyArray<ResizeObserverSize>
-}
-
-export type ResizeObserverCallback = (entries: ReadonlyArray<ResizeObserverEntry>, observer: ResizeObserver) => void
-
-export interface UseResizeObserverOptions extends ConfigurableWindow {
-  /**
-   * Sets which box model the observer will observe changes to. Possible values
-   * are `content-box` (the default), `border-box` and `device-pixel-content-box`.
-   *
-   * @default 'content-box'
-   */
-  box?: ResizeObserverBoxOptions
-}
-
-declare class ResizeObserver {
-  constructor(callback: ResizeObserverCallback)
-  disconnect(): void
-  observe(target: Element, options?: UseResizeObserverOptions): void
-  unobserve(target: Element): void
+export interface UseResizeObserverOptions extends ResizeObserverOptions, ConfigurableWindow {
 }
 
 /**
