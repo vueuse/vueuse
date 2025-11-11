f6986877c9ffe8a4ada999badafcbf0afb904294
Vida Xie <vida_2020@163.com>
2025-11-11 23:34:59 +0800

    chore: update

--- packages/core/useNow/index.ts
+++ packages/core/useNow/index.ts
@@ -55,7 +55,9 @@ export interface UseNowOptions<Controls extends boolean, Legacy = false> extends
 export function useNow(options?: UseNowOptions<false>): Ref<Date>
 export function useNow(options: UseNowOptions<true>): { now: Ref<Date> } & Pausable
 /** @deprecated Please use with `scheduler` option */
-export function useNow(options?: UseNowOptions<boolean, true>): Ref<Date> | { now: Ref<Date> } & Pausable
+export function useNow(options: UseNowOptions<false, true>): Ref<Date>
+/** @deprecated Please use with `scheduler` option */
+export function useNow(options: UseNowOptions<true, true>): { now: Ref<Date> } & Pausable
 
 /**
  * Reactive current Date instance.
