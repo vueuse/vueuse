b06bae1313c2372af0af85298dd63ef60503dada
kalu5 <451660550@qq.com>
2025-11-14 10:11:06 +0800

    fix(useElementBounding): clear directive when component is unmounted(#5001)

--- packages/core/useElementBounding/directive.ts
+++ packages/core/useElementBounding/directive.ts
@@ -1,29 +1,39 @@
 import type { UseElementBoundingOptions, UseElementBoundingReturn } from '@vueuse/core'
-import type { ObjectDirective } from 'vue'
+import type { EffectScope, ObjectDirective } from 'vue'
 import { useElementBounding } from '@vueuse/core'
-import { watch } from 'vue'
+import { effectScope, watch } from 'vue'
 
 type ElementBounding = Omit<UseElementBoundingReturn, 'update'>
 type BindingValueFunction = (bounding: ElementBounding) => void
 type BindingValueArray = [BindingValueFunction, UseElementBoundingOptions]
+
+const vElementBoundingScopes = new WeakMap<HTMLElement, EffectScope>()
 
 export const vElementBounding: ObjectDirective<
   HTMLElement,
   BindingValueFunction | BindingValueArray
 > = {
   mounted(el, binding) {
-    const [handler, options] = (typeof binding.value === 'function' ? [binding.value, {}] : binding.value) as BindingValueArray
+    const scope = vElementBoundingScopes.get(el) ?? effectScope()
+    vElementBoundingScopes.set(el, scope)
+    scope.run(() => {
+      const [handler, options] = (typeof binding.value === 'function' ? [binding.value, {}] : binding.value) as BindingValueArray
 
-    const {
-      height,
-      bottom,
-      left,
-      right,
-      top,
-      width,
-      x,
-      y,
-    } = useElementBounding(el, options)
-    watch([height, bottom, left, right, top, width, x, y], () => handler({ height, bottom, left, right, top, width, x, y }))
+      const {
+        height,
+        bottom,
+        left,
+        right,
+        top,
+        width,
+        x,
+        y,
+      } = useElementBounding(el, options)
+      watch([height, bottom, left, right, top, width, x, y], () => handler({ height, bottom, left, right, top, width, x, y }))
+    })
+  },
+  unmounted(el) {
+    vElementBoundingScopes.get(el)?.stop()
+    vElementBoundingScopes.delete(el)
   },
 }
