f6a99c5514f4c5e347a4312be2dbcaad72a8e971
kalu5 <451660550@qq.com>
2025-11-14 09:52:17 +0800

    fix(useMouseInElement): clear directive when component is unmounted(#5001)

--- packages/core/useMouseInElement/directive.ts
+++ packages/core/useMouseInElement/directive.ts
@@ -1,21 +1,31 @@
 import type { MouseInElementOptions, UseMouseInElementReturn } from '@vueuse/core'
-import type { ObjectDirective, Reactive } from 'vue'
+import type { EffectScope, ObjectDirective, Reactive } from 'vue'
 import { useMouseInElement } from '@vueuse/core'
 import { reactiveOmit } from '@vueuse/shared'
-import { reactive, watch } from 'vue'
+import { effectScope, reactive, watch } from 'vue'
 
 type MouseInElement = Omit<UseMouseInElementReturn, 'stop'>
 type BindingValueFunction = (mouse: Reactive<MouseInElement>) => void
 type BindingValueArray = [BindingValueFunction, MouseInElementOptions]
+
+const vMouseInElementScopes = new WeakMap<HTMLElement, EffectScope>()
 
 export const vMouseInElement: ObjectDirective<
   HTMLElement,
   BindingValueFunction | BindingValueArray
 > = {
   mounted(el, binding) {
-    const [handler, options] = (typeof binding.value === 'function' ? [binding.value, {}] : binding.value) as BindingValueArray
+    const scope = vMouseInElementScopes.get(el) ?? effectScope()
+    vMouseInElementScopes.set(el, scope)
+    scope.run(() => {
+      const [handler, options] = (typeof binding.value === 'function' ? [binding.value, {}] : binding.value) as BindingValueArray
 
-    const state = reactiveOmit(reactive(useMouseInElement(el, options)), 'stop')
-    watch(state, val => handler(val))
+      const state = reactiveOmit(reactive(useMouseInElement(el, options)), 'stop')
+      watch(state, val => handler(val))
+    })
+  },
+  unmounted(el) {
+    vMouseInElementScopes.get(el)?.stop()
+    vMouseInElementScopes.delete(el)
   },
 }
