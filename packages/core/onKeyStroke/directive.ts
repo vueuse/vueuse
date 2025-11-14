aa02db37c9faf74df9e9e86aa2a161b0c8de0565
kalu5 <451660550@qq.com>
2025-11-14 12:39:53 +0800

    fix(onKeyStroke): clear directive when component is unmounted(#5001)

--- packages/core/onKeyStroke/directive.ts
+++ packages/core/onKeyStroke/directive.ts
@@ -1,28 +1,39 @@
 import type { OnKeyStrokeOptions } from '@vueuse/core'
-import type { ObjectDirective } from 'vue'
+import type { EffectScope, ObjectDirective } from 'vue'
 import { onKeyStroke } from '@vueuse/core'
+import { effectScope } from 'vue'
 
 type BindingValueFunction = (event: KeyboardEvent) => void
 
 type BindingValueArray = [BindingValueFunction, OnKeyStrokeOptions]
+
+const vOnKeyStrokeScopes = new WeakMap<HTMLElement, EffectScope>()
 
 export const vOnKeyStroke: ObjectDirective<
   HTMLElement,
   BindingValueFunction | BindingValueArray
 > = {
   mounted(el, binding) {
-    const keys = binding.arg?.split(',') ?? true
-    if (typeof binding.value === 'function') {
-      onKeyStroke(keys, binding.value, {
-        target: el,
-      })
-    }
-    else {
-      const [handler, options] = binding.value
-      onKeyStroke(keys, handler, {
-        target: el,
-        ...options,
-      })
-    }
+    const scope = vOnKeyStrokeScopes.get(el) ?? effectScope()
+    vOnKeyStrokeScopes.set(el, scope)
+    scope.run(() => {
+      const keys = binding.arg?.split(',') ?? true
+      if (typeof binding.value === 'function') {
+        onKeyStroke(keys, binding.value, {
+          target: el,
+        })
+      }
+      else {
+        const [handler, options] = binding.value
+        onKeyStroke(keys, handler, {
+          target: el,
+          ...options,
+        })
+      }
+    })
+  },
+  unmounted(el) {
+    vOnKeyStrokeScopes.get(el)?.stop()
+    vOnKeyStrokeScopes.delete(el)
   },
 }
