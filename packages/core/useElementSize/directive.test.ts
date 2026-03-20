66cfb9bddd67bb7069e9061ad080caee527a305f
yuyuanfei <3334355151@qq.com>
2026-03-20 17:52:19 +0800

    fix:Modify test cases

--- packages/core/useElementSize/directive.test.ts
+++ packages/core/useElementSize/directive.test.ts
@@ -1,8 +1,17 @@
 import type { VueWrapper } from '@vue/test-utils'
 import { mount } from '@vue/test-utils'
 import { beforeEach, describe, expect, it, vi } from 'vitest'
-import { defineComponent } from 'vue'
+import { defineComponent, nextTick, shallowRef } from 'vue'
 import { vElementSize } from './directive'
+import { useElementSize } from './index.ts' // 👈 补上
+
+// 👇 必须加：mock ResizeObserver
+class ResizeObserverMock {
+  observe() {}
+  unobserve() {}
+  disconnect() {}
+}
+vi.stubGlobal('ResizeObserver', ResizeObserverMock)
 
 const App = defineComponent({
   props: {
@@ -68,5 +77,39 @@ describe('vElementSize', () => {
     it('should be defined', () => {
       expect(wrapper).toBeDefined()
     })
+  })
+})
+
+describe('useElementSize - svg branch (component)', () => {
+  it('should use clientWidth/clientHeight when target is SVG', async () => {
+    const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg')
+
+    Object.defineProperty(svg, 'clientWidth', {
+      value: 123,
+      configurable: true,
+    })
+    Object.defineProperty(svg, 'clientHeight', {
+      value: 456,
+      configurable: true,
+    })
+
+    const Comp = defineComponent({
+      setup() {
+        const el = shallowRef<SVGElement | null>(null)
+        const { width, height } = useElementSize(el)
+
+        return { el, width, height }
+      },
+      template: `<svg ref="el"></svg>`,
+    })
+
+    const wrapper = mount(Comp)
+
+    wrapper.vm.el = svg
+
+    await nextTick()
+
+    expect(wrapper.vm.width).toBe(123)
+    expect(wrapper.vm.height).toBe(456)
   })
 })
