73272537db016172cdc4a7015464ecd2946246d1
Edward Sammut Alessi <esammutalessi@gmail.com>
2026-03-27 11:55:40 +0100

    feat(useIDBKeyval): sync changes across tabs
    Add listenToStorageChanges option to useIDBKeyval to sync value changes across tabs like in useStorage. Uses BroadcastChannel and not StorageEvent, but kept the same option name for consistency.

--- packages/integrations/useIDBKeyval/index.ts
+++ packages/integrations/useIDBKeyval/index.ts
@@ -1,15 +1,16 @@
+import type { ConfigurableWindow } from '@vueuse/core'
 import type { ConfigurableFlush, RemovableRef } from '@vueuse/shared'
-import type { MaybeRefOrGetter, Ref, ShallowRef } from 'vue'
-import { watchPausable } from '@vueuse/core'
+import type { ComputedRef, MaybeRefOrGetter, Ref, ShallowRef } from 'vue'
+import { defaultWindow, tryOnScopeDispose, useSupported, watchPausable } from '@vueuse/core'
 import { del, get, set, update } from 'idb-keyval'
-import { ref as deepRef, shallowRef, toRaw, toValue } from 'vue'
+import { ref as deepRef, nextTick, shallowRef, toRaw, toValue } from 'vue'
 
 interface Serializer<T> {
   read: (raw: unknown) => T
   write: (value: T) => unknown
 }
 
-export interface UseIDBOptions<T> extends ConfigurableFlush {
+export interface UseIDBOptions<T> extends ConfigurableFlush, ConfigurableWindow {
   /**
    * Watch for deep changes
    *
@@ -41,11 +42,20 @@ export interface UseIDBOptions<T> extends ConfigurableFlush {
    * Custom data serialization
    */
   serializer?: Serializer<T>
+
+  /**
+   * Listen to storage changes from other tabs via BroadcastChannel,
+   * useful for multiple tabs applications
+   *
+   * @default true
+   */
+  listenToStorageChanges?: boolean
 }
 
 export interface UseIDBKeyvalReturn<T> {
   data: RemovableRef<T>
   isFinished: ShallowRef<boolean>
+  isSupported: ComputedRef<boolean>
   set: (value: T) => Promise<void>
 }
 
@@ -68,11 +78,15 @@ export function useIDBKeyval<T>(
       console.error(e)
     },
     writeDefaults = true,
+    listenToStorageChanges = true,
+    window = defaultWindow,
     serializer = {
       read: (raw: unknown) => raw as T,
       write: (value: T) => value,
     },
   } = options
+
+  const isSupported = useSupported(() => window && 'BroadcastChannel' in window)
 
   const isFinished = shallowRef(false)
   const data = (shallow ? shallowRef : deepRef)(initialValue) as Ref<T>
@@ -100,15 +114,21 @@ export function useIDBKeyval<T>(
 
   read()
 
-  async function write() {
+  let channel: BroadcastChannel | undefined
+
+  async function write(broadcastChange = true) {
     try {
       if (data.value == null) {
         await del(key)
+        if (broadcastChange)
+          channel?.postMessage({ type: 'delete' })
       }
       else {
         const rawValue = toRaw(data.value)
         const serializedValue = serializer.write(rawValue)
         await update(key, () => serializedValue)
+        if (broadcastChange)
+          channel?.postMessage({ type: 'set', value: serializedValue })
       }
     }
     catch (e) {
@@ -121,6 +141,28 @@ export function useIDBKeyval<T>(
     resume: resumeWatch,
   } = watchPausable(data, () => write(), { flush, deep })
 
+  if (listenToStorageChanges && isSupported.value) {
+    channel = new BroadcastChannel(`vueuse-idb-${JSON.stringify(key)}`)
+    tryOnScopeDispose(() => channel?.close())
+
+    channel.addEventListener('message', (event: MessageEvent) => {
+      const { type, value } = event.data ?? {}
+      pauseWatch()
+      try {
+        if (type === 'delete')
+          data.value = rawInit
+        else if (type === 'set')
+          data.value = serializer.read(value)
+      }
+      catch (e) {
+        onError(e)
+      }
+      finally {
+        nextTick(resumeWatch)
+      }
+    })
+  }
+
   async function setData(value: T): Promise<void> {
     pauseWatch()
     data.value = value
@@ -131,6 +173,7 @@ export function useIDBKeyval<T>(
   return {
     set: setData,
     isFinished,
+    isSupported,
     data: data as RemovableRef<T>,
   }
 }
