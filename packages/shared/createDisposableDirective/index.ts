b24246e4eed0477a58200008dd9a1f4ed243d1e7
卢加龙 <451660550@qq.com>
2026-04-15 16:40:21 +0800

    refactor: normalised original value in advance

--- packages/shared/createDisposableDirective/index.ts
+++ packages/shared/createDisposableDirective/index.ts
@@ -10,44 +10,35 @@ type originDirective<H, V, A> = FunctionDirective<H, V, string, A> | ObjectDirec
  *
  * @__NO_SIDE_EFFECTES__
  */
-export function createDisposableDirective<H extends HTMLElement, V, A = any>(origin: originDirective<H, V, A> = {}): ObjectDirective<H, V, string, A> {
+export function createDisposableDirective<H extends HTMLElement, V, A = any>(origin: originDirective<H, V, A> = {}): originDirective<H, V, A> {
   function isFunc(fn: unknown) {
     return typeof fn === 'function'
   }
 
-  function createTargetDirective<H extends HTMLElement, V, A>(origin: ObjectDirective<H, V, string, A>) {
-    const { mounted, unmounted } = origin
+  const normalisedOrigin = isFunc(origin) ? { mounted: origin, updated: origin } : origin
 
-    if (!isFunc(mounted))
-      return origin
+  const { mounted, unmounted } = normalisedOrigin
 
-    const scopeWeakMap = new WeakMap<H, EffectScope>()
+  if (!isFunc(mounted))
+    return origin
 
-    return {
-      ...origin,
-      mounted(el: H, binding: DirectiveBinding<V, string, A>, vNode: VNode<any, H>, prevNode: null) {
-        const scope = scopeWeakMap.get(el) ?? effectScope()
-        scopeWeakMap.set(el, scope)
-        scope.run(() => {
-          mounted?.(el, binding, vNode, prevNode)
-        })
-      },
-      unmounted(el: H, binding: DirectiveBinding<V, string, A>, vNode: VNode<any, H>, prevNode: null) {
-        scopeWeakMap.get(el)?.stop()
-        scopeWeakMap.delete(el)
-        if (isFunc(unmounted)) {
-          unmounted(el, binding, vNode, prevNode)
-        }
-      },
-    }
+  const scopeWeakMap = new WeakMap<H, EffectScope>()
+
+  return {
+    ...normalisedOrigin,
+    mounted(el: H, binding: DirectiveBinding<V, string, A>, vNode: VNode<any, H>, prevNode: null) {
+      const scope = scopeWeakMap.get(el) ?? effectScope()
+      scopeWeakMap.set(el, scope)
+      scope.run(() => {
+        mounted?.(el, binding, vNode, prevNode)
+      })
+    },
+    unmounted(el: H, binding: DirectiveBinding<V, string, A>, vNode: VNode<any, H>, prevNode: null) {
+      scopeWeakMap.get(el)?.stop()
+      scopeWeakMap.delete(el)
+      if (isFunc(unmounted)) {
+        unmounted(el, binding, vNode, prevNode)
+      }
+    },
   }
-
-  if (isFunc(origin)) {
-    return createTargetDirective<H, V, A>({
-      mounted: origin,
-      updated: origin,
-    })
-  }
-
-  return createTargetDirective<H, V, A>(origin)
 }
