305d2fcef2c1f373190383a4644e9c50164cf097
Tobias Braun <tobiasmarkusbraun@gmail.com>
2026-06-01 16:33:34 +0200

    fix(useBrowserLocation): Skip write-back watching on 'load' state (#5480)

--- packages/core/useBrowserLocation/index.ts
+++ packages/core/useBrowserLocation/index.ts
@@ -49,15 +49,7 @@ export function useBrowserLocation(options: UseBrowserLocationOptions = {}): Use
   const { window = defaultWindow } = options
   const refs = Object.fromEntries(
     WRITABLE_PROPERTIES.map(key => [key, deepRef()]),
-  ) as Record<typeof WRITABLE_PROPERTIES[number], Ref<string | undefined>>
-
-  for (const [key, ref] of objectEntries(refs)) {
-    watch(ref, (value) => {
-      if (!window?.location || window.location[key] === value)
-        return
-      window.location[key] = value!
-    })
-  }
+  ) as Record<(typeof WRITABLE_PROPERTIES)[number], Ref<string | undefined>>
 
   const buildState = (trigger: string): BrowserLocationState => {
     const { state, length } = window?.history || {}
@@ -77,6 +69,13 @@ export function useBrowserLocation(options: UseBrowserLocationOptions = {}): Use
 
   const state = deepRef<BrowserLocationState>(buildState('load'))
 
+  for (const [key, ref] of objectEntries(refs)) {
+    watch(ref, (value) => {
+      if (!window?.location || window.location[key] === value)
+        return
+      window.location[key] = value!
+    })
+  }
   if (window) {
     const listenerOptions = { passive: true }
     useEventListener(window, 'popstate', () => state.value = buildState('popstate'), listenerOptions)
