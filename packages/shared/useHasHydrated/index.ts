0d8403857b9d49ba23fd6cfcb289457b0f7292d7
Tofandel <adrien.foulon@tukan.hu>
2026-06-18 01:35:07 +0200

    Avoid double render on server

--- packages/shared/useHasHydrated/index.ts
+++ packages/shared/useHasHydrated/index.ts
@@ -1,4 +1,5 @@
 import { getCurrentInstance, onMounted, onServerPrefetch, shallowReadonly, shallowRef } from 'vue'
+import { isClient } from '../utils'
 
 /* @__NO_SIDE_EFFECTS__ */
 /** Helper to use client-only values safe from hydration mismatch */
@@ -8,8 +9,8 @@ export function useHasHydrated() {
     throw new Error('useHasHydrated must be used in a component')
   }
 
-  const isHydrating = !!(instance.vnode.el && !instance.isMounted)
-  const hasHydrated = shallowRef(!isHydrating)
+  const isHydrating = Boolean(isClient && instance.vnode.el && !instance.isMounted)
+  const hasHydrated = shallowRef(isClient ? !isHydrating : false)
 
   if (isHydrating) {
     onMounted(() => {
